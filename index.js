b8e42ca6b1eb9dfa025ac57419a474d10434f643
Zack Jackson <zackary.l.jackson@gmail.com>
2020-10-24 19:28:59 -0700

    add runtime merging

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const withModuleFederation = require("./withModuleFederation");
 const patchSharing = require("./patchSharing");
-
+const MergeRuntime = require('./merge-runtime')
 module.exports = {
   withModuleFederation,
   patchSharing,
+  MergeRuntime
 };
