acf6c9f9fff8d6ebc4bad56fb23cd48f450540f5
Aurelien MANCA <aurelien.manca@manomano.com>
2020-12-04 10:08:00 +0100

    fix(patch): use with next as external module

--- patchSharing.js
+++ patchSharing.js
@@ -1,8 +1,12 @@
 const path = require("path");
 const fs = require("fs");
+const __require__ =
+  typeof __non_webpack_require__ !== "undefined"
+    ? __non_webpack_require__
+    : require;
 const patchSharing = () => {
   const React = require("react");
-  const reactPath = path.dirname(__non_webpack_require__.resolve("react"));
+  const reactPath = path.dirname(__require__.resolve("react"));
   const umdReact =
     process.env.NODE_ENV === "production"
       ? path.join(reactPath, "umd/react.production.min.js")
