aca8b6f5d34d4f278bd9739c6b42c416e0b1cc4c
Jacob Ebey <jacob.ebey@live.com>
2021-01-25 17:38:56 -0800

    Update merge-runtime.js

--- merge-runtime.js
+++ merge-runtime.js
@@ -1,9 +1,19 @@
 
 const ConcatSource = require('webpack-sources/lib/ConcatSource');
 
-module.exports = class ModuleFedSingleRuntimePlugin {
+const PLUGIN_NAME = "ModuleFedSingleRuntimePlugin";
+
+/**
+ * Merge a runtime with your remote entry.
+ */
+class ModuleFedSingleRuntimePlugin {
+  /**
+   * @param {object} options
+   * @param {string} [options.fileName= remoteEntry.js] The file name to concat the runtime with
+   * @param {string} [options.runtime= runtime.js] The runtime to merge
+   */
   constructor(options) {
-    this._options = {fileName: 'remoteEntry.js', ...options};
+    this._options = {fileName: 'remoteEntry.js', runtime: 'runtime.js', ...options};
   }
   // Define `apply` as its prototype method which is supplied with compiler as its argument
   apply(compiler) {
@@ -11,12 +21,14 @@ module.exports = class ModuleFedSingleRuntimePlugin {
     const options = this._options;
 
     // Specify the event hook to attach to
-    compiler.hooks.emit.tap("EnableSingleRunTimeForFederationPlugin", (compilation) => {
+    compiler.hooks.emit.tap(PLUGIN_NAME, (compilation) => {
       const { assets } = compilation;
-      const runtime  = assets['runtime.js'];
+      const runtime  = assets[this._options.runtime];
       const remoteEntry = assets[this._options.fileName];
       const mergedSource = new ConcatSource(runtime, remoteEntry);
       assets[this._options.fileName] = mergedSource;
     });
   }
 };
+
+module.exports = ModuleFedSingleRuntimePlugin;
