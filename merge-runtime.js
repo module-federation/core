a760f0247d1294bc9ae1c01f18be793c197ead77
Zack Jackson <zackary.l.jackson@gmail.com>
2021-05-05 12:20:31 -0700

    fix: patching the merge runtime plguin

--- merge-runtime.js
+++ merge-runtime.js
@@ -1,5 +1,4 @@
-
-const ConcatSource = require('webpack-sources/lib/ConcatSource');
+const ConcatSource = require("webpack-sources/lib/ConcatSource");
 
 const PLUGIN_NAME = "ModuleFedSingleRuntimePlugin";
 
@@ -10,10 +9,14 @@ class ModuleFedSingleRuntimePlugin {
   /**
    * @param {object} options
    * @param {string} [options.fileName= remoteEntry.js] The file name to concat the runtime with
-   * @param {string} [options.runtime= runtime.js] The runtime to merge
+   * @param {string} [options.runtime= webpack] The runtime to merge
    */
   constructor(options) {
-    this._options = {fileName: 'remoteEntry.js', runtime: 'runtime.js', ...options};
+    this._options = {
+      fileName: "remoteEntry.js",
+      runtime: "webpack",
+      ...options,
+    };
   }
   // Define `apply` as its prototype method which is supplied with compiler as its argument
   apply(compiler) {
@@ -23,12 +26,20 @@ class ModuleFedSingleRuntimePlugin {
     // Specify the event hook to attach to
     compiler.hooks.emit.tap(PLUGIN_NAME, (compilation) => {
       const { assets } = compilation;
-      const runtime  = assets[this._options.runtime];
-      const remoteEntry = assets[this._options.fileName];
+      const assetArray = Object.keys(assets);
+
+      let runtimePath = assetArray.find((asset) => {
+        return asset.includes(this._options.runtime);
+      });
+      const runtime = assets[runtimePath];
+      let remoteEntryPath = assetArray.find((asset) => {
+        return asset.includes(this._options.fileName);
+      });
+      const remoteEntry = assets[remoteEntryPath];
       const mergedSource = new ConcatSource(runtime, remoteEntry);
-      assets[this._options.fileName] = mergedSource;
+      assets[remoteEntryPath] = mergedSource;
     });
   }
-};
+}
 
 module.exports = ModuleFedSingleRuntimePlugin;
