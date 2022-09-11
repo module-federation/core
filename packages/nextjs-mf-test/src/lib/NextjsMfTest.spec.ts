b9ef67a89f595b51c3a7516ad4eaa0e363d7850d
dv <pavan.divi@outlook.com>
2022-09-11 19:18:02 +0530

    refactor: remove test package

--- packages/nextjs-mf-test/src/lib/NextjsMfTest.spec.ts
+++
@@ -1,7 +0,0 @@
-import { nextjsMfTest } from './NextjsMfTest';
-
-describe('nextjsMfTest', () => {
-  it('should work', () => {
-    expect(nextjsMfTest()).toEqual('nextjs-mf-test');
-  });
-});
--- packages/nextjs-mf-test/src/lib/NextjsMfTest.ts
+++
@@ -1,7 +0,0 @@
-export function nextjsMfTest(): string {
-  return 'nextjs-mf-test';
-}
-
-export class TestPlugin {
-  constructor() {}
-}
