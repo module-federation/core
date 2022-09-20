4b4d3c67adc5034167c5145d1c1f3739a876df0b
dv <pavan.divi@outlook.com>
2022-09-20 12:11:53 +0530

    feat: migration shop app

---
+++ apps/shop/pages/shop/products/[...slug].tsx
@@ -0,0 +1,23 @@
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
+
+export default function ProductPage() {
+  const [cnt, setCounter] = useState(0);
+  const router = useRouter();
+  useEffect(() => {
+    setInterval(() => {
+      setCounter((s) => s + 1);
+    }, 1000);
+  }, []);
+  const { query } = useRouter();
+
+  if (cnt === 0) return null;
+
+  return (
+    <div>
+      <h1>Product with id {query?.slug}!!! </h1>
+      <div>{cnt}</div>
+      <pre>{JSON.stringify(router, undefined, 2)}</pre>
+    </div>
+  );
+}
