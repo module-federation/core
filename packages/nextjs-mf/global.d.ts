144cb02339e7ba8b91661f873702882cc7d97af4
Ran Shamay <ranshamay@microsoft.com>
2022-10-13 01:34:30 +0300

    chore: revert global.d.ts

--- packages/nextjs-mf/global.d.ts
+++ packages/nextjs-mf/global.d.ts
@@ -1,9 +1,5 @@
-/* eslint-disable no-var */
 // eslint-disable-next-line @typescript-eslint/triple-slash-reference
 /// <reference path="../../node_modules/webpack/module.d.ts" />
 
 declare module 'webpack/lib/container/options';
 declare module 'webpack/lib/sharing/utils';
-declare global {
-  var __remote_scope__: any;
-}
