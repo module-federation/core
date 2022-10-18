32ebc4743aa97d1d39abd8ae291d4a6ca0cbf5e3
ScriptedAlchemy <zackary.l.jackson@gmail.com>
2022-10-18 15:06:52 -0700

    fix: improve stats performance

--- packages/node/src/plugins/ChunkCorrelationPlugin.js
+++ packages/node/src/plugins/ChunkCorrelationPlugin.js
@@ -387,7 +387,21 @@ class FederationStatsPlugin {
         },
         async () => {
           console.log('getting stats');
-          const stats = compilation.getStats().toJson({});
+          const stats = compilation.getStats().toJson({
+            performance:false,
+            time:false,
+            logging: "none",
+            loggingDebug: false,
+            loggingTrace: false,
+            source: false,
+            children:false,
+            errors: false,
+            warnings: false,
+            errorsCount: false,
+            warningsCount: false,
+            builtAt: false,
+            timings: false
+          });
 
           const federatedModules = federationPlugins.map((federationPlugin) =>
             getFederationStats(stats, federationPlugin)
