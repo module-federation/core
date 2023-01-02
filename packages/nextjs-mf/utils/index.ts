5191aaec88d2fede848b385564029276a97b1ecb
kyletsang <6854874+kyletsang@users.noreply.github.com>
2023-01-01 23:09:50 -0800

    fix(nextjs-mf): fix FlushedChunks TS declaration

--- packages/nextjs-mf/utils/index.ts
+++ packages/nextjs-mf/utils/index.ts
@@ -1,40 +1,61 @@
-import * as React from "react";
-export { extractUrlAndGlobal, injectScript } from '@module-federation/utilities';
+import * as React from 'react';
+export {
+  extractUrlAndGlobal,
+  injectScript,
+} from '@module-federation/utilities';
 // @ts-ignore
-export {flushChunks} from '@module-federation/node/utils';
+export { flushChunks } from '@module-federation/node/utils';
 
 export const revalidate = () => {
-  if(typeof window !== 'undefined') {
+  if (typeof window !== 'undefined') {
     console.error('revalidate should only be called server-side');
     return Promise.resolve(false);
   }
   // @ts-ignore
   return import('@module-federation/node/utils').then((utils) => {
     return utils.revalidate();
-  })
+  });
+};
+
+export interface FlushedChunksProps {
+  chunks: string[];
 }
 
-//@ts-ignore
-export const FlushedChunks = ({ chunks })=>{
-  //@ts-ignore
-  const scripts = chunks.filter((c)=>c.endsWith(".js")).map((chunk)=> {
-    if(!chunk.includes('?') && chunk.includes('remoteEntry')) {
-      chunk = chunk + '?t=' + Date.now();
-    }
-    return React.createElement("script", {
-      src: chunk,
-      async: true
-    }, null)
+export const FlushedChunks = ({ chunks }: FlushedChunksProps) => {
+  const scripts = chunks
+    .filter((c) => c.endsWith('.js'))
+    .map((chunk) => {
+      if (!chunk.includes('?') && chunk.includes('remoteEntry')) {
+        chunk = chunk + '?t=' + Date.now();
+      }
+      return React.createElement(
+        'script',
+        {
+          src: chunk,
+          async: true,
+        },
+        null
+      );
+    });
+
+  const css = chunks
+    .filter((c) => c.endsWith('.css'))
+    .map((chunk) => {
+      return React.createElement(
+        'link',
+        {
+          href: chunk,
+          rel: 'stylesheet',
+        },
+        null
+      );
+    });
+
+  return React.createElement(React.Fragment, {
+    children: [css, scripts],
   });
-  //@ts-ignore
-  const css = chunks.filter((c)=>c.endsWith(".css")).map((chunk)=> {
-    return React.createElement("link", {
-      href: chunk,
-      rel: "stylesheet"
-    }, null)
-  });
-  return [css,scripts];
 };
+
 FlushedChunks.defaultProps = {
-  chunks: []
-};
+  chunks: [],
+} as FlushedChunksProps;
