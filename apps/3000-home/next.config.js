244257c73c0f0c9d2641ab8b3f03e1521c65e947
ScriptedAlchemy <zackary.l.jackson@gmail.com>
2023-01-12 17:43:34 -0800

    feat:  delegate module support

--- apps/3000-home/next.config.js
+++ apps/3000-home/next.config.js
@@ -1,9 +1,6 @@
 const { withNx } = require('@nrwl/next/plugins/with-nx');
 
 const NextFederationPlugin = require('@module-federation/nextjs-mf');
-const {
-  promiseTemplate,
-} = require('@module-federation/nextjs-mf/utils/build-utils');
 
 /**
  * @type {import('@nrwl/next/plugins/with-nx').WithNxOptions}
