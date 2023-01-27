355a1b32ff9a13a0ab2c9498e74d7ca1b6699869
ScriptedAlchemy <zackary.l.jackson@gmail.com>
2023-01-27 11:14:14 -0800

    cleaning up code

--- apps/3000-home/remote-delegate.js
+++ apps/3000-home/remote-delegate.js
@@ -1,7 +1,9 @@
 import { importDelegatedModule } from '@module-federation/utilities';
 
 module.exports = new Promise((resolve, reject) => {
+  //eslint-disable-next-line
   console.log('Delegate being called for', __resourceQuery);
+  //eslint-disable-next-line
   const currentRequest = new URLSearchParams(__resourceQuery).get('remote');
 
   const [global, url] = currentRequest.split('@');
