e64a6582892ca15aa303c83be580ceddb860c750
Yuriy Musienko <musienkoyuriy@gmail.com>
2023-01-30 17:49:06 +0200

    fix nextjs-mf page loading

--- apps/3000-home/pages/home/test-broken-remotes.tsx
+++ apps/3000-home/pages/home/test-broken-remotes.tsx
@@ -7,16 +7,12 @@ export default function TestBrokenRemotes() {
 
       <p>
         Check unresolved host –{' '}
-        <Link href="/unresolved-host">
-          <a>/unresolved-host</a>
-        </Link>{' '}
+        <Link href="/unresolved-host">/unresolved-host</Link>{' '}
         (on http://localhost:<b>3333</b>/_next/static/chunks/remoteEntry.js)
       </p>
       <p>
         Check wrong response for remoteEntry –{' '}
-        <Link href="/wrong-entry">
-          <a>/wrong-entry</a>
-        </Link>{' '}
+        <Link href="/wrong-entry">/wrong-entry</Link>{' '}
         (on http://localhost:3000/_next/static/chunks/remoteEntry<b>Wrong</b>
         .js)
       </p>
