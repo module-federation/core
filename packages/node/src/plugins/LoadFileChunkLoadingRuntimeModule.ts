25623c4e9ad3cd10916112370920a0998df5fe3c
ScriptedAlchemy <zackary.l.jackson@gmail.com>
2023-01-30 16:41:24 -0800

    chore:  add comments

--- packages/node/src/plugins/LoadFileChunkLoadingRuntimeModule.ts
+++ packages/node/src/plugins/LoadFileChunkLoadingRuntimeModule.ts
@@ -89,7 +89,8 @@ class ReadFileChunkLoadingRuntimeModule extends RuntimeModule {
     // so for example, if im in hostA and require(remoteb/module) --> console.log of name in runtime code will return remoteb
 
     const {remotes = {}, name} = this.options;
-
+    // for delegate modules, we need to avoid serializing internal modules, only register primitive configs in the runtime
+    // delegates have their own registration code, so we dont need to handle them here.
     const remotesByType: RemotesByType = Object.values(remotes).reduce((acc:RemotesByType, remote:string) => {
       if (remote.startsWith('promise ') || remote.startsWith('internal ') || remote.startsWith('external ')) {
         acc.functional.push(remote);
