25327f82395553ba52e9ff2bcef83151712bfaa1
ScriptedAlchemy <zackary.l.jackson@gmail.com>
2023-02-01 10:39:21 -0800

    turn off delegates

--- apps/3000-home/next.config.js
+++ apps/3000-home/next.config.js
@@ -16,21 +16,21 @@ const nextConfig = {
     const { isServer } = options;
 ;
     const remotes = {
-      home: createDelegatedModule(require.resolve('./remote-delegate.js'), {
-        remote: `home_app@http://localhost:3000/_next/static/${isServer ? 'ssr' : 'chunks'}/remoteEntry.js`,
-      }),
-      shop: createDelegatedModule(require.resolve('./remote-delegate.js'), {
-        remote: `shop@http://localhost:3001/_next/static/${isServer ? 'ssr' : 'chunks'}/remoteEntry.js`,
-      }),
-      checkout: createDelegatedModule(require.resolve('./remote-delegate.js'), {
-        remote: `checkout@http://localhost:3002/_next/static/${isServer ? 'ssr' : 'chunks'}/remoteEntry.js`,
-      }),
-      // shop: `shop@http://localhost:3001/_next/static/${
-      //   isServer ? 'ssr' : 'chunks'
-      // }/remoteEntry.js`,
-      // checkout: `checkout@http://localhost:3002/_next/static/${
-      //   isServer ? 'ssr' : 'chunks'
-      // }/remoteEntry.js`,
+      // home: createDelegatedModule(require.resolve('./remote-delegate.js'), {
+      //   remote: `home_app@http://localhost:3000/_next/static/${isServer ? 'ssr' : 'chunks'}/remoteEntry.js`,
+      // }),
+      // shop: createDelegatedModule(require.resolve('./remote-delegate.js'), {
+      //   remote: `shop@http://localhost:3001/_next/static/${isServer ? 'ssr' : 'chunks'}/remoteEntry.js`,
+      // }),
+      // checkout: createDelegatedModule(require.resolve('./remote-delegate.js'), {
+      //   remote: `checkout@http://localhost:3002/_next/static/${isServer ? 'ssr' : 'chunks'}/remoteEntry.js`,
+      // }),
+      shop: `shop@http://localhost:3001/_next/static/${
+        isServer ? 'ssr' : 'chunks'
+      }/remoteEntry.js`,
+      checkout: `checkout@http://localhost:3002/_next/static/${
+        isServer ? 'ssr' : 'chunks'
+      }/remoteEntry.js`,
     };
 
     config.plugins.push(
