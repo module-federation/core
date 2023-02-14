1a22d069fb58b119408de277edc71e3772f17224
ScriptedAlchemy <zackary.l.jackson@gmail.com>
2023-02-14 10:36:00 -0800

    Update default-delegate to add support for Next.js

--- apps/3002-checkout/remote-delegate.js
+++ apps/3002-checkout/remote-delegate.js
@@ -3,7 +3,7 @@ import { importDelegatedModule } from '@module-federation/utilities';
 // Delegates are currently not used in this example, but are left here for testing.
 module.exports = new Promise((resolve, reject) => {
   //eslint-disable-next-line
-  console.log('Delegate being called for', __resourceQuery);
+  // console.log('Delegate being called for', __resourceQuery);
   //eslint-disable-next-line
   const currentRequest = new URLSearchParams(__resourceQuery).get('remote');
 
@@ -14,11 +14,11 @@ module.exports = new Promise((resolve, reject) => {
     url: url + '?' + Date.now(),
   })
     .then(async (remote) => {
-      console.log(
-        __resourceQuery,
-        'resolved remote from',
-        __webpack_runtime_id__
-      );
+      // console.log(
+      //   __resourceQuery,
+      //   'resolved remote from',
+      //   __webpack_runtime_id__
+      // );
 
       resolve(remote);
     })
