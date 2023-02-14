e907d040be6b72aaf69fc71970e4497538c387d0
ScriptedAlchemy <zackary.l.jackson@gmail.com>
2023-02-14 10:36:06 -0800

    Update default-delegate to add support for Next.js

--- apps/3001-shop/next.config.js
+++ apps/3001-shop/next.config.js
@@ -21,24 +21,24 @@ const nextConfig = {
         name: 'shop',
         filename: 'static/chunks/remoteEntry.js',
         remotes: {
-          home: createDelegatedModule(require.resolve('./remote-delegate.js'), {
-            remote: `home_app@http://localhost:3000/_next/static/${
-              isServer ? 'ssr' : 'chunks'
-            }/remoteEntry.js`,
-          }),
-          shop: createDelegatedModule(require.resolve('./remote-delegate.js'), {
-            remote: `shop@http://localhost:3001/_next/static/${
-              isServer ? 'ssr' : 'chunks'
-            }/remoteEntry.js`,
-          }),
-          checkout: createDelegatedModule(
-            require.resolve('./remote-delegate.js'),
-            {
-              remote: `checkout@http://localhost:3002/_next/static/${
-                isServer ? 'ssr' : 'chunks'
-              }/remoteEntry.js`,
-            }
-          ),
+          // home: createDelegatedModule(require.resolve('./remote-delegate.js'), {
+          //   remote: `home_app@http://localhost:3000/_next/static/${
+          //     isServer ? 'ssr' : 'chunks'
+          //   }/remoteEntry.js`,
+          // }),
+          // shop: createDelegatedModule(require.resolve('./remote-delegate.js'), {
+          //   remote: `shop@http://localhost:3001/_next/static/${
+          //     isServer ? 'ssr' : 'chunks'
+          //   }/remoteEntry.js`,
+          // }),
+          // checkout: createDelegatedModule(
+          //   require.resolve('./remote-delegate.js'),
+          //   {
+          //     remote: `checkout@http://localhost:3002/_next/static/${
+          //       isServer ? 'ssr' : 'chunks'
+          //     }/remoteEntry.js`,
+          //   }
+          // ),
           home: `home_app@http://localhost:3000/_next/static/${
             isServer ? 'ssr' : 'chunks'
           }/remoteEntry.js`,
--- apps/3002-checkout/next.config.js
+++ apps/3002-checkout/next.config.js
@@ -20,24 +20,24 @@ const nextConfig = {
         name: 'checkout',
         filename: 'static/chunks/remoteEntry.js',
         remotes: {
-          home: createDelegatedModule(require.resolve('./remote-delegate.js'), {
-            remote: `home_app@http://localhost:3000/_next/static/${isServer ? 'ssr' : 'chunks'}/remoteEntry.js`,
-          }),
-          shop: createDelegatedModule(require.resolve('./remote-delegate.js'), {
-            remote: `shop@http://localhost:3001/_next/static/${isServer ? 'ssr' : 'chunks'}/remoteEntry.js`,
-          }),
-          checkout: createDelegatedModule(require.resolve('./remote-delegate.js'), {
-            remote: `checkout@http://localhost:3002/_next/static/${isServer ? 'ssr' : 'chunks'}/remoteEntry.js`,
-          }),
-          // home: `home_app@http://localhost:3000/_next/static/${
-          //   isServer ? 'ssr' : 'chunks'
-          // }/remoteEntry.js`,
-          // shop: `shop@http://localhost:3001/_next/static/${
-          //   isServer ? 'ssr' : 'chunks'
-          // }/remoteEntry.js`,
-          // checkout: `checkout@http://localhost:3002/_next/static/${
-          //   isServer ? 'ssr' : 'chunks'
-          // }/remoteEntry.js`,
+          // home: createDelegatedModule(require.resolve('./remote-delegate.js'), {
+          //   remote: `home_app@http://localhost:3000/_next/static/${isServer ? 'ssr' : 'chunks'}/remoteEntry.js`,
+          // }),
+          // shop: createDelegatedModule(require.resolve('./remote-delegate.js'), {
+          //   remote: `shop@http://localhost:3001/_next/static/${isServer ? 'ssr' : 'chunks'}/remoteEntry.js`,
+          // }),
+          // checkout: createDelegatedModule(require.resolve('./remote-delegate.js'), {
+          //   remote: `checkout@http://localhost:3002/_next/static/${isServer ? 'ssr' : 'chunks'}/remoteEntry.js`,
+          // }),
+          home: `home_app@http://localhost:3000/_next/static/${
+            isServer ? 'ssr' : 'chunks'
+          }/remoteEntry.js`,
+          shop: `shop@http://localhost:3001/_next/static/${
+            isServer ? 'ssr' : 'chunks'
+          }/remoteEntry.js`,
+          checkout: `checkout@http://localhost:3002/_next/static/${
+            isServer ? 'ssr' : 'chunks'
+          }/remoteEntry.js`,
         },
         exposes: {
           './CheckoutTitle': './components/CheckoutTitle',
