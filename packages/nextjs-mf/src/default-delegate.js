011d6e114225d444896f4b45b9b9bef8d306e577
ScriptedAlchemy <zackary.l.jackson@gmail.com>
2023-02-14 10:35:47 -0800

    Update default-delegate to add support for Next.js

---
+++ packages/nextjs-mf/src/default-delegate.js
@@ -0,0 +1,17 @@
+import { importDelegatedModule } from '@module-federation/utilities';
+
+module.exports = new Promise((resolve, reject) => {
+  // eslint-disable-next-line no-undef
+  const currentRequest = new URLSearchParams(__resourceQuery).get('remote');
+
+  const [global, url] = currentRequest.split('@');
+
+  importDelegatedModule({
+    global,
+    url: url + '?' + Date.now(),
+  })
+    .then(async (remote) => {
+      resolve(remote);
+    })
+    .catch((err) => reject(err));
+});
