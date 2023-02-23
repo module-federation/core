d985b195e2919b2d760a36484dc4777681cabb5f
Andrei Fiodorov <andrei.fiodorov@icloud.com>
2023-02-23 16:02:19 +0100

    feat(storybook-plugin): add test for webpack fn

---
+++ packages/storybook-plugin/src/lib/storybook-plugin.spec.ts
@@ -0,0 +1,60 @@
+import { webpack } from './storybook-plugin';
+import { Configuration, container } from 'webpack';
+
+const { ModuleFederationPlugin } = container;
+
+const webpackConfig: Configuration = {
+  mode: 'production',
+  entry: './foo.js',
+  output: {
+    path: './dist',
+    filename: 'foo.bundle.js',
+  },
+};
+
+const moduleFederationConfig = {
+  name: 'host',
+  remotes: {
+    remote: 'remote@http://localhost:3002/remoteEntry.js',
+  },
+  shared: {},
+};
+
+describe('webpack()', () => {
+  it('should return error for webpack 4 version', async () => {
+    const apply = jest.fn().mockResolvedValue(Promise.resolve('4'));
+
+    const presets = {
+      apply,
+    };
+
+    await expect(
+      webpack(webpackConfig, {
+        moduleFederationConfig,
+        presets,
+      })
+    ).rejects.toThrow(
+      'Webpack 5 required: Configure Storybook to use the webpack5 builder'
+    );
+  });
+
+  it('should return config for webpack 5 version', async () => {
+    const apply = jest.fn().mockResolvedValue(Promise.resolve('5'));
+
+    const presets = {
+      apply,
+    };
+
+    const matchObject = {
+      ...webpackConfig,
+      plugins: [new ModuleFederationPlugin(moduleFederationConfig)],
+    };
+
+    await expect(
+      webpack(webpackConfig, {
+        moduleFederationConfig,
+        presets,
+      })
+    ).resolves.toMatchObject(matchObject);
+  });
+});
