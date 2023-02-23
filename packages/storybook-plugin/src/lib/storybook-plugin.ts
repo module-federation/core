df70cd17deb9bd51f60938a0336ef117db455f67
Andrei Fiodorov <andrei.fiodorov@icloud.com>
2023-02-23 16:01:49 +0100

    feat: add package storybook-plugin

---
+++ packages/storybook-plugin/src/lib/storybook-plugin.ts
@@ -0,0 +1,52 @@
+import { container, Configuration } from 'webpack';
+import { ModuleFederationPluginOptions } from '@module-federation/utilities';
+import { logger } from '@storybook/node-logger';
+
+const { ModuleFederationPlugin } = container;
+
+type Preset = string | { name: string };
+
+type Options = {
+  moduleFederationConfig: ModuleFederationPluginOptions;
+  presets: {
+    apply<T>(preset: Preset): Promise<T>;
+  };
+};
+
+export const webpack = async (
+  webpackConfig: Configuration,
+  options: Options
+): Promise<Configuration> => {
+  const { plugins = [] } = webpackConfig;
+  const { moduleFederationConfig, presets } = options;
+
+  // Detect webpack version. More about storybook webpack config https://storybook.js.org/docs/react/addons/writing-presets#webpack
+  const webpackVersion = await presets.apply('webpackVersion');
+  logger.info(`=> Webpack ${webpackVersion} version detected`);
+
+  if (webpackVersion !== '5') {
+    throw new Error(
+      'Webpack 5 required: Configure Storybook to use the webpack5 builder'
+    );
+  }
+
+  logger.info(`=> Push Module Federation plugin`);
+
+  plugins.push(
+    new ModuleFederationPlugin({
+      ...moduleFederationConfig,
+      shared: {}, // TODO: Fix error shared dependencies
+    })
+  );
+
+  return {
+    ...webpackConfig,
+    plugins,
+  };
+};
+
+export const config = (entry: string[] = []): string[] => {
+  logger.info(`=> Load Module Federation plugin preview file`);
+
+  return [...entry, require.resolve('./mf-preview')];
+};
