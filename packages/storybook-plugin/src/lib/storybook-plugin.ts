777f55e23ee844c9e06113b1671f0e0dbb9a3e07
Andrei Fiodorov <andrei.fiodorov@icloud.com>
2023-03-01 13:16:07 +0100

    feat(storybook-plugin): replace VirtualModulePlugin from storybook webpack config with another configuration to bootstrap entry file

--- packages/storybook-plugin/src/lib/storybook-plugin.ts
+++ packages/storybook-plugin/src/lib/storybook-plugin.ts
@@ -1,6 +1,29 @@
+import fs from 'fs';
+import os from 'os';
+import path from 'path';
+import VirtualModulesPlugin from 'webpack-virtual-modules';
 import { container, Configuration } from 'webpack';
 import { ModuleFederationPluginOptions } from '@module-federation/utilities';
 import { logger } from '@storybook/node-logger';
+
+const correctImportPath = (context: string, entryFile: string) => {
+  if (os.platform() !== 'win32') {
+    return entryFile;
+  }
+
+  if (entryFile.match(/^\.?\.\\/) || !entryFile.match(/^[A-Z]:\\\\/i)) {
+    return entryFile.replace(/\\/g, '/');
+  }
+
+  const joint = path.win32.relative(context, entryFile);
+  const relative = joint.replace(/\\/g, '/');
+
+  if (relative.includes('node_modules/')) {
+    return relative.split('node_modules/')[1];
+  }
+
+  return `./${relative}`;
+};
 
 const { ModuleFederationPlugin } = container;
 
@@ -17,12 +40,13 @@ export const webpack = async (
   webpackConfig: Configuration,
   options: Options
 ): Promise<Configuration> => {
-  const { plugins = [] } = webpackConfig;
+  const { plugins = [], entry: webpackEntry, context } = webpackConfig;
   const { moduleFederationConfig, presets } = options;
+  let entry = webpackEntry;
 
   // Detect webpack version. More about storybook webpack config https://storybook.js.org/docs/react/addons/writing-presets#webpack
   const webpackVersion = await presets.apply('webpackVersion');
-  logger.info(`=> Webpack ${webpackVersion} version detected`);
+  logger.info(`=> [MF] Webpack ${webpackVersion} version detected`);
 
   if (webpackVersion !== '5') {
     throw new Error(
@@ -30,17 +54,63 @@ export const webpack = async (
     );
   }
 
-  logger.info(`=> Push Module Federation plugin`);
+  logger.info(`=> [MF] Push Module Federation plugin`);
+  plugins.push(new ModuleFederationPlugin(moduleFederationConfig));
 
-  plugins.push(
-    new ModuleFederationPlugin({
-      ...moduleFederationConfig,
-      shared: {}, // TODO: Fix error shared dependencies
-    })
+  const index = plugins.findIndex(
+    (plugin) => plugin.constructor.name === 'VirtualModulesPlugin'
   );
+
+  if (index !== -1) {
+    logger.info(`=> [MF] Detected plugin VirtualModulesPlugin`);
+
+    /* eslint-disable  @typescript-eslint/no-explicit-any */
+    const plugin = plugins[index] as any;
+    const currenEntryFiles: string[] = Array.isArray(entry) ? entry : []; // TODO: Why if is not array of strings ????
+
+    const virtualModules = plugin._staticModules; // TODO: Exist another way to get virtual modules? Or maybe it's good idea to open a PR adding a method to get modules?
+    const virtualModulePaths: string[] = Object.keys(virtualModules);
+
+    // Get modules that are not virtual
+    const filteredEntry = currenEntryFiles.filter(
+      (path: string) => !virtualModulePaths.includes(path)
+    );
+    const bootstrap: string[] = filteredEntry.map(
+      (entryFile: string) =>
+        `import '${correctImportPath(context || process.cwd(), entryFile)}';`
+    );
+
+    logger.info(`=> [MF] Write files from VirtualModulesPlugin`);
+    virtualModulePaths.forEach((virtualModulePath) => {
+      fs.writeFileSync(virtualModulePath, virtualModules[virtualModulePath]);
+      bootstrap.push(
+        `import '${correctImportPath(
+          context || process.cwd(),
+          virtualModulePath
+        )}';`
+      );
+    });
+
+    /**
+     * Rewrite VirtualModulesPlugin plugin to fix error "Shared module is not available for eager consumption"
+     * Entry file content is moved in bootstrap file. More details in the webpack documentation:
+     * https://webpack.js.org/concepts/module-federation/#uncaught-error-shared-module-is-not-available-for-eager-consumption
+     * */
+    logger.info(
+      `=> [MF] Rewrite plugin VirtualModulesPlugin to bootstrap entry point`
+    );
+    plugins[index] = new VirtualModulesPlugin({
+      './__entry.js': `import('./__bootstrap.js');`,
+      './__bootstrap.js': bootstrap.join('\n'),
+    });
+
+    // replace webpack entry file
+    entry = ['./__entry.js'];
+  }
 
   return {
     ...webpackConfig,
+    entry,
     plugins,
   };
 };
