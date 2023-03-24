397f9cdf21d1139885310fdb9d7025dcc68870fa
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2023-03-24 22:45:27 +0100

    fix: build

--- packages/native-federation-typescript/tsup.config.ts
+++ packages/native-federation-typescript/tsup.config.ts
@@ -1,15 +1,17 @@
 import {defineConfig} from 'tsup'
+import { join } from 'path'
 
 const SUPPORTED_BUNDLERS = ['esbuild', 'rollup', 'vite', 'webpack', 'rspack']
 
 export default defineConfig({
     entry: [
         'src/index.ts',
-        ...SUPPORTED_BUNDLERS.map(bundler => `src/${bundler}.ts`)
+        ...SUPPORTED_BUNDLERS.map(bundler => join(__dirname, 'src', `${bundler}.ts`))
     ],
     dts: true,
     splitting: true,
     clean: true,
     minify: true,
-    format: ['cjs', 'esm']
+    format: ['cjs', 'esm'],
+    outDir: 'dist/packages/native-federation-typescript'
 })
