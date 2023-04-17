4c37801e31f74c4ba28bb22403cbe1b508568416
rcanfield <Rcanfield86@gmail.com>
2023-04-17 10:29:07 -0400

    Typedocs, include other projects

---
+++ apps/react-ts-host/@mf-types/react-ts-remote/_types/apps/react-ts-remote/src/app/App.d.ts
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <title>ReactTsRemote</title>
+    <base href="/">
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="icon" type="image/x-icon" href="favicon.ico" />
+  <link rel="stylesheet" href="styles.css"></head>
+  <body>
+    <div id="root"></div>
+  <script src="polyfills.js" type="module"></script><script src="styles.js" type="module"></script><script src="main.js" type="module"></script></body>
+</html>
