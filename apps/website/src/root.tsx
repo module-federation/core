6c215ff0e038eb1475e5bd3490f1088e7624d308
Caio Rolla <caiioocesar@outlook.com>
2023-04-18 16:29:13 -0300

    feat(website): added google analytics with partytown

--- apps/website/src/root.tsx
+++ apps/website/src/root.tsx
@@ -10,6 +10,7 @@ import { RouterHead } from './components/router-head/router-head';
 
 import globalStyles from './global.css?inline';
 import { config, translationFn } from './speak-config';
+import { QwikPartytown } from './components/partytown/partytown';
 
 export default component$(() => {
   /**
@@ -26,6 +27,12 @@ export default component$(() => {
       <QwikCityProvider>
         <head>
           <meta charSet="utf-8" />
+          <QwikPartytown forward={['dataLayer.push']} />
+          <script
+          async
+          type="text/partytown"
+          src="https://www.googletagmanager.com/gtag/js?id=G-SDV5HRTM4G"
+        />
           <link rel="manifest" href="/manifest.json" />
           <RouterHead />
         </head>
--- apps/website/vite.config.ts
+++ apps/website/vite.config.ts
@@ -1,8 +1,10 @@
 import { qwikVite } from '@builder.io/qwik/optimizer';
 import { qwikCity } from '@builder.io/qwik-city/vite';
+import { partytownVite } from '@builder.io/partytown/utils';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { qwikNxVite } from 'qwik-nx/plugins';
+import { join } from 'path';
 
 export default defineConfig({
   cacheDir: '../../node_modules/.vite/apps/website',
@@ -18,6 +20,7 @@ export default defineConfig({
       },
     }),
     tsconfigPaths({ root: '../../' }),
+    partytownVite({ dest: join(__dirname, 'dist', '~partytown') }),
   ],
   server: {
     fs: {
