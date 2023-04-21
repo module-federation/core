49acbda352d7d5478cb7e05396b582a4a9fc03b6
Dmitriy Shekhovtsov <valorkin@gmail.com>
2023-04-21 21:05:32 +0300

    feat: release to npm with next tag to not ruine latest one (#763) (#772)

---
+++ scripts/update-next-to-latest-at-npm.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+PACKAGES=(@module-federation/native-federation-tests @module-federation/native-federation-typescript @module-federation/nextjs-mf @module-federation/node @module-federation/typescript)
+
+for package in "${PACKAGES[@]}"; do
+  # Check if the package is published with "next" tag
+  if npm view "$package" dist-tags | grep -q next; then
+    echo "Updating $package from 'next' to 'latest'"
+    npm dist-tag add "$package@$(npm dist-tag ls $package | grep next | awk '{print $NF}')" latest
+    npm dist-tag rm "$package" next
+  else
+    echo "$package is not published with 'next' tag"
+  fi
+done
