847c30c92b92993f346884b4ca005ee6c789105a
ScriptedAlchemy <zackary.l.jackson@gmail.com>
2023-04-22 18:37:31 -0700

    refactor: flush chunks

--- packages/node/src/utils/flush-chunks.js
+++ packages/node/src/utils/flush-chunks.js
@@ -1,107 +1,155 @@
+/**
+ * Initialize usedChunks and share it globally.
+ * @type {Set}
+ */
 export const usedChunks = global.usedChunks || new Set();
 global.usedChunks = usedChunks;
-export const flushChunks = async () => {
-  let hostStats = {};
+
+/**
+ * Load hostStats from the JSON file.
+ * @returns {object} hostStats - An object containing host stats data.
+ */
+const loadHostStats = () => {
   try {
-     hostStats = __non_webpack_require__('../host-stats.json');
+    return __non_webpack_require__('../federated-stats.json');
   } catch (e) {
-
+    return {};
   }
+};
 
-  let shareMap = {};
-  if(__webpack_share_scopes__?.default) {
-     shareMap = Object.keys(__webpack_share_scopes__.default).reduce((acc, key) => {
-      const loadedModules = Object.values(__webpack_share_scopes__.default[key]).filter((sharedModule) => {
-        return sharedModule.loaded
-      }).map((sharedModule) => {
-        return sharedModule.from;
-      })
+/**
+ * Create a shareMap based on the loaded modules.
+ * @returns {object} shareMap - An object containing the shareMap data.
+ */
+const createShareMap = () => {
+  // Check if __webpack_share_scopes__ is defined and has a default property
+  if (__webpack_share_scopes__?.default) {
+    // Reduce the keys of the default property to create the share map
+    return Object.keys(__webpack_share_scopes__.default).reduce((acc, key) => {
+      // Get the loaded modules for the current key
+      const loadedModules = Object.values(__webpack_share_scopes__.default[key])
+        // Filter out the modules that are not loaded
+        .filter((sharedModule) => sharedModule.loaded)
+        // Map the filtered modules to their 'from' properties
+        .map((sharedModule) => sharedModule.from);
+
+      // If there are any loaded modules, add them to the accumulator object
       if (loadedModules.length > 0) {
         acc[key] = loadedModules;
       }
-      return acc
+      // Return the accumulator object for the next iteration
+      return acc;
     }, {});
   }
-  const allFlushed = await Promise.all(
-    Array.from(usedChunks).map(async (chunk) => {
-      const chunks = new Set();
-      const [remote, request] = chunk.split('->');
-      if (!global.__remote_scope__._config[remote]) {
-        return;
-      }
-      // fetch the json file
-      try {
-        const remoteName = new URL(global.__remote_scope__._config[remote]).pathname.split('/').pop();
-        const statsFile = global.__remote_scope__._config[remote].replace(
-          remoteName,
-          'federated-stats.json'
-        ).replace('ssr','chunks');
+  // If __webpack_share_scopes__ is not defined or doesn't have a default property, return an empty object
+  return {};
+};
 
-        let stats = {}
-        try {
-          await fetch(global.__remote_scope__._config[remote])
-           stats = await fetch(statsFile).then((res)=>res.json());
-        } catch (e) {
-console.error("flush errer",e);
-        }
 
-        chunks.add(
-          global.__remote_scope__._config[remote].replace('ssr', 'chunks')
-        );
-        const [prefix] =
-          global.__remote_scope__._config[remote].split('static/');
-        if (stats.federatedModules) {
-          // console.log(shareMap)
-          stats.federatedModules.forEach((modules) => {
-            if (modules.exposes?.[request]) {
-              modules.exposes[request].forEach((chunk) => {
-                Object.values(chunk).forEach((chunk) => {
-                  if(chunk.files) chunk.files.forEach((file) => {
-                    console.log('adding', prefix + file)
-                    chunks.add(prefix + file)
-                  });
-                  if(chunk.requiredModules) {
-                    console.log('chunk.requiredModules',request, chunk.requiredModules)
-                    chunk.requiredModules.forEach((module) => {
-                      console.log('shareMap[module]', module, shareMap[module])
+/**
+ * Process a single chunk and return an array of updated chunks.
+ * @param {string} chunk - A chunk string containing remote and request data.
+ * @param {object} shareMap - An object containing the shareMap data.
+ * @param {object} hostStats - An object containing host stats data.
+ * @returns {Promise<Array>} A promise that resolves to an array of updated chunks.
+ */
+const processChunk = async (chunk, shareMap, hostStats) => {
+  // Create a set to store the chunks
+  const chunks = new Set();
 
-                      if (shareMap[module]) {
+  // Split the chunk string into remote and request
+  const [remote, request] = chunk.split('->');
 
-                        if(shareMap[module][0].startsWith('host__')) {
-                          console.log('host',hostStats)
-                        }
-                        console.log('shareMap[module]', module, shareMap[module])
-                        // shareMap[module].forEach((file) => {
-                        //   chunks.add(prefix + file)
-                        // })
-                      }
-                    })
+  // If the remote is not defined in the global config, return
+  if (!global.__remote_scope__._config[remote]) {
+    return;
+  }
+
+  try {
+    // Extract the remote name from the URL
+    const remoteName = new URL(global.__remote_scope__._config[remote]).pathname
+      .split('/')
+      .pop();
+
+    // Construct the stats file URL from the remote config
+    const statsFile = global.__remote_scope__._config[remote]
+      .replace(remoteName, 'federated-stats.json')
+      .replace('ssr', 'chunks');
+
+    let stats = {};
+    try {
+      // Fetch the remote config and stats file
+      await fetch(global.__remote_scope__._config[remote]);
+      stats = await fetch(statsFile).then((res) => res.json());
+    } catch (e) {
+      console.error('flush error', e);
+    }
+
+    // Add the main chunk to the chunks set
+    chunks.add(
+      global.__remote_scope__._config[remote].replace('ssr', 'chunks')
+    );
+
+    // Extract the prefix from the remote config
+    const [prefix] = global.__remote_scope__._config[remote].split('static/');
+
+    // Process federated modules from the stats object
+    if (stats.federatedModules) {
+      stats.federatedModules.forEach((modules) => {
+        // Process exposed modules
+        if (modules.exposes?.[request]) {
+          modules.exposes[request].forEach((chunk) => {
+            Object.values(chunk).forEach((chunk) => {
+              // Add files to the chunks set
+              if (chunk.files) {
+                chunk.files.forEach((file) => {
+                  chunks.add(prefix + file);
+                });
+              }
+              // Process required modules
+              if (chunk.requiredModules) {
+                chunk.requiredModules.forEach((module) => {
+                  // Check if the module is in the shareMap
+                  if (shareMap[module]) {
+                    // If the module is from the host, log the host stats
+                    if (shareMap[module][0].startsWith('host__')) {
+                      console.log('host', hostStats);
+                    }
                   }
                 });
-              });
-            }
-
-            // todo: come back to this later
-            // if(modules.sharedModules?.[0]) {
-            //   const sharedModules = modules.sharedModules.find((sharedModule) => {
-            //     return sharedModule.provides.find((provide) => {return provide.shareKey === request})
-            //   });
-            //   sharedModules.chunks.forEach((chunk) => {
-            //     chunks.add(prefix + chunk);
-            //   });
-            // }
+              }
+            });
           });
         }
+      });
+    }
 
-        return Array.from(chunks);
-      } catch (e) {
-        console.log(e);
-      }
-    })
+    // Return the array of chunks
+    return Array.from(chunks);
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+/**
+ * Flush the chunks and return a deduplicated array of chunks.
+ * @returns {Promise<Array>} A promise that resolves to an array of deduplicated chunks.
+ */
+export const flushChunks = async () => {
+  const hostStats = loadHostStats();
+  const shareMap = createShareMap();
+
+  const allFlushed = await Promise.all(
+    Array.from(usedChunks).map(async (chunk) =>
+      processChunk(chunk, shareMap, hostStats)
+    )
   );
 
+  // Deduplicate the chunks array
   const dedupe = Array.from(new Set([...allFlushed.flat()]));
 
+  // Clear usedChunks
   usedChunks.clear();
+  // Filter out any undefined or null values
   return dedupe.filter(Boolean);
 };
