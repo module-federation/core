04e08e8420b0f68197c686f599e279b658194a46
Zack Jackson <zackary.l.jackson@gmail.com>
2023-04-22 20:03:34 -0700

    Update apps/3000-home/pages/checkout/index.tsx

--- apps/3000-home/pages/checkout/index.tsx
+++ apps/3000-home/pages/checkout/index.tsx
@@ -1,5 +1,5 @@
 //@ts-ignore
-import Page from "checkout/pages/checkout/index";
+import Page from 'checkout/pages/checkout/index';
 
 // the data loading method needs to be here so next can static analyze it properly.
 // eslint-disable-next-line no-self-assign
