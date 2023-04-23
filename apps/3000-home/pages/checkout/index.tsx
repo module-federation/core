1a769623444d825798be36feade627b85b16d192
Zack Jackson <zackary.l.jackson@gmail.com>
2023-04-22 20:05:23 -0700

    refactor: chunk flushing (#788)
    * refactor: flush chunks
    BREAKING CHANGE: chunk flushing implementation was rewritten

--- apps/3000-home/pages/checkout/index.tsx
+++ apps/3000-home/pages/checkout/index.tsx
@@ -2,6 +2,7 @@
 import Page from 'checkout/pages/checkout/index';
 
 // the data loading method needs to be here so next can static analyze it properly.
+// eslint-disable-next-line no-self-assign
 Page.getInitialProps = Page.getInitialProps;
 
 export default Page;
