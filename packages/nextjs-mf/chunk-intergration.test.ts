8ac0d8e857efb7487d5e12d687cdc6db5d3e06c2
ScriptedAlchemy <zackary.l.jackson@gmail.com>
2023-04-24 11:25:19 -0700

    chore: build test fix
    run builds without minify so its easier to see whats happening

--- packages/nextjs-mf/chunk-intergration.test.ts
+++ packages/nextjs-mf/chunk-intergration.test.ts
@@ -4,49 +4,68 @@ import * as fs from 'fs';
 import * as path from 'path';
 
 describe('Next.js build output', () => {
-  const buildOutputDir = path.join(
-    __dirname,
-    '../../',
-    'dist/apps/3000-home/.next'
-  );
-
   beforeAll(() => {
     // Run the build programmatically
     // console.log('buildOutputDir', buildOutputDir);
     // execSync('nx build', { stdio: 'inherit', cwd: '../../' });
     //execSync("cd " + JSON.stringify(__dirname) +'; yarn build', { stdio: 'inherit', cwd: __dirname });
   });
-
-  xit('matches the snapshot', () => {
-    const buildOutput = getBuildOutput(buildOutputDir);
-    expect(buildOutput).toMatchSnapshot();
+  describe('client', () => {
+    const buildOutputDir = path.join(
+      __dirname,
+      '../../',
+      'dist/apps/3000-home/.next/static/chunks'
+    );
+    it('remoteEntry.js', () => {
+      const buildOutput = findFileInDirectory('remoteEntry', buildOutputDir);
+      expect(buildOutput).toMatchSnapshot();
+    });
+    it('partial remote', () => {
+      const buildOutput = findFileInDirectory('home_app', buildOutputDir);
+      expect(buildOutput).toMatchSnapshot();
+    });
+    it('webpack-runtime', () => {
+      const buildOutput = findFileInDirectory('webpack-', buildOutputDir);
+      expect(buildOutput).toMatchSnapshot();
+    });
+  });
+  describe('server', () => {
+    const buildOutputDir = path.join(
+      __dirname,
+      '../../',
+      'dist/apps/3000-home/.next/server'
+    );
+    it('remoteEntry.js', () => {
+      const buildOutput = findFileInDirectory('remoteEntry', buildOutputDir);
+      expect(buildOutput).toMatchSnapshot();
+    });
+    it('partial remote', () => {
+      const buildOutput = findFileInDirectory('home_app', buildOutputDir);
+      expect(buildOutput).toMatchSnapshot();
+    });
+    it('webpack-runtime', () => {
+      const buildOutput = findFileInDirectory('webpack-', buildOutputDir);
+      expect(buildOutput).toMatchSnapshot();
+    });
   });
 });
 
-function getBuildOutput(dir: string): Record<string, string> {
-  const output: Record<string, string> = {};
-
-  const files = fs.readdirSync(dir, { withFileTypes: true });
-
-  files.forEach((file) => {
-    const filePath = path.join(dir, file.name);
-    if (file.isDirectory()) {
-      // @ts-ignore
-      output[file.name] = getBuildOutput(filePath);
-    } else {
-      if (
-        (file.name.startsWith('webpack-') ||
-          file.name.startsWith('home') ||
-          file.name.startsWith('remoteEntry')) &&
-        file.name.endsWith('.js')
-      ) {
-        console.log(file.name);
-      } else {
-        return;
+function findFileInDirectory(
+  filename: string,
+  directory: string
+): string | null {
+  const files = fs.readdirSync(directory);
+  for (const file of files) {
+    const filePath = path.join(directory, file);
+    const fileStat = fs.statSync(filePath);
+    if (fileStat.isDirectory()) {
+      const result = findFileInDirectory(filename, filePath);
+      if (result) {
+        return result;
       }
-      output[file.name] = fs.readFileSync(filePath, 'utf8');
+    } else if (file.startsWith(filename) && file.endsWith('.js')) {
+      return fs.readFileSync(filePath, 'utf-8');
     }
-  });
-
-  return output;
+  }
+  return null;
 }
