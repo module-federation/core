7547b02937fdef2831060d6a7bfd337d2cc3355c
fyodorovandrei <46130932+fyodorovandrei@users.noreply.github.com>
2023-04-25 13:46:44 +0200

    feat: add storybook addon that supports Module Federation remote containers (#598)
    * feat: add package storybook-plugin
    
    * feat(storybook-plugin): add test for webpack fn
    
    * feat: add package storybook-plugin
    
    * feat(storybook-plugin): add test for webpack fn
    
    * feat(storybook-plugin): remove storybook config overwrite
    
    * feat(storybook-plugin): replace VirtualModulePlugin from storybook webpack config with another configuration to bootstrap entry file
    
    * fix(storybook-plugin): update todo comment
    
    * fix(storybook-plugin): remove preview file
    
    * feat(storybook-plugin): move utils in own folder
    
    * feat(storybook-plugin): update logic to replace or push the plugin VirtualModulePlugin
    
    * feat(storybook-plugin): fix package description
    
    * feat(storybook-plugin): update README.md
    
    * feat(storybook-addon): rename from plugin to addon
    
    * chore: add package storybook-addon in release action
    
    * fix(storybook-addon): update package repository url
    
    * fix(storybook-addon): update mock and add virtual module plugin after refactor webpack fn
    
    * feat(storybook-addon): add tests for correctImportPath util
    
    Co-authored-by: Robert Donnelly <dev@robdonn.com>
    
    * feat(storybook-addon): refactor webpack fn
    
    * feat(storybook-addon): write all webpack virtual modules in node_modules directory and fix some imports
    
    * fix(storybook-addon): update test
    
    * fix(storybook-addon): rollback yarn.lock
    
    * feature(storybook-addon): write files recursive when move from virtual modules
    
    * feat(storybook-addon): move util `correctImportPath` in common utils package
    
    * feat(storybook-addon): add 2 mf apps and add support for nx mf config
    
    * fix(storybook-addon): pass webpack config
    
    * fix(storybook-addon): rollback util call
    
    * feat(storybook-addon): implement custom utils `withModuleFederation` for nx Module Federation
    
    * feat(storybook-addon): update package versions
    
    * feat(storybook-addon): update README.md
    
    ---------
    
    Co-authored-by: Zack Jackson <zackary.l.jackson@gmail.com>
    Co-authored-by: Robert Donnelly <dev@robdonn.com>

---
+++ apps/reactRemoteUI/src/Button/Button.tsx
@@ -0,0 +1,26 @@
+import React, { HTMLProps } from 'react';
+import classNames from 'classnames';
+
+import styles from './Button.module.css';
+
+type ButtonProps = {
+  type?: 'button' | 'submit' | 'reset' | undefined;
+  variant?: 'primary' | 'secondary';
+} & HTMLProps<HTMLButtonElement>;
+
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ children, className, variant = 'primary', ...props }, ref) => {
+    const cssClasses = classNames(styles.button, {
+      [styles.primary]: variant === 'primary',
+      [styles.secondary]: variant === 'secondary',
+    });
+
+    return (
+      <button {...props} ref={ref} className={`${cssClasses} ${className}`}>
+        {children}
+      </button>
+    );
+  }
+);
+
+export default Button;
