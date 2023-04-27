159f49838230d6a60b8126de3cc2c7b41c953728
Zack Jackson <zackary.l.jackson@gmail.com>
2023-04-27 15:18:26 -0700

    chore: fix CI installs (#814)

--- apps/react-ts-remote/webpack.config.js
+++ apps/react-ts-remote/webpack.config.js
@@ -1,5 +1,5 @@
 const { withModuleFederation } = require('@nrwl/react/module-federation');
-const { FederatedTypesPlugin } = require('../../dist/packages/typescript');
+const { FederatedTypesPlugin } = require('@module-federation/typescript');
 
 const baseConfig = require('./module-federation.config');
 
