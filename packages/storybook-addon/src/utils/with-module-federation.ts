a9fd3e0522338967e89a283ea45440c0f44052bd
Andrei Fiodorov <andrei.fiodorov@icloud.com>
2023-04-28 15:50:43 +0200

    feat(storybook-addon): upgrade nx and refactor util `withModuleFederation`

--- packages/storybook-addon/src/utils/with-module-federation.ts
+++ packages/storybook-addon/src/utils/with-module-federation.ts
@@ -1,27 +1,8 @@
-import { getModuleFederationConfig } from '@nrwl/react/src/module-federation/utils';
-import { readCachedProjectConfiguration } from 'nx/src/project-graph/project-graph';
+import { getModuleFederationConfig } from '@nx/react/src/module-federation/utils';
 import { container, Configuration } from 'webpack';
 import type { ModuleFederationConfig } from '@nrwl/devkit';
 
 const { ModuleFederationPlugin } = container;
-
-function determineRemoteUrl(remote: string) {
-  const remoteConfiguration = readCachedProjectConfiguration(remote);
-  const serveTarget = remoteConfiguration?.targets?.['serve'];
-
-  if (!serveTarget) {
-    throw new Error(
-      `Cannot automatically determine URL of remote (${remote}). Looked for property "host" in the project's "serve" target.\n
-      You can also use the tuple syntax in your webpack config to configure your remotes. e.g. \`remotes: [['remote1', 'http://localhost:4201']]\``
-    );
-  }
-
-  const host = serveTarget.options?.host ?? 'http://localhost';
-  const port = serveTarget.options?.port ?? 4201;
-  return `${
-    host.endsWith('/') ? host.slice(0, -1) : host
-  }:${port}/remoteEntry.js`;
-}
 
 const updateMappedRemotes = (remotes: Record<string, string>) => {
   const newRemotes: Record<string, string> = {};
@@ -35,8 +16,7 @@ const updateMappedRemotes = (remotes: Record<string, string>) => {
 
 const withModuleFederation = async (options: ModuleFederationConfig) => {
   const { mappedRemotes, sharedDependencies } = await getModuleFederationConfig(
-    options,
-    determineRemoteUrl
+    options
   );
 
   return (config: Configuration) => {
