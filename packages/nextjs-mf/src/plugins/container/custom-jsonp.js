f593725e24672dbf95eae018f2656cab68ec2c4e
ScriptedAlchemy <zackary.l.jackson@gmail.com>
2023-05-01 12:40:40 -0700

    fix: improve eager modularization

--- packages/nextjs-mf/src/plugins/container/custom-jsonp.js
+++ packages/nextjs-mf/src/plugins/container/custom-jsonp.js
@@ -29,167 +29,121 @@
 //   },
 // };
 
-export default `
+export default //language=JS
+`
 var chunkQueue = [];
 var resport = [];
 var cnn = '__APPNAME__'
 var chunkLoadingGlobal = self['webpackChunk' + cnn] || [];
+var asyncQueue = [];
+function setEagerLoading(libraryName, version, fromValue) {
+  var library = __webpack_require__.S.default[libraryName];
+  console.log('setEagerLoading',library, libraryName, version, fromValue);
+library && console.log(library, library[version], library[version] && library[version].from === fromValue)
+  if (library && library[version] && library[version].from === fromValue) {
+  console.log('setting eager', libraryName, version, fromValue);
+    library[version].eager = true;
+  }
+}
+function loadDependencies(libKeys, cnn) {
+  libKeys.forEach(function (libKey) {
+    var lib = __webpack_require__.S.default[libKey];
+    var versionKeys = Object.keys(lib).reverse();
+    versionKeys.forEach(function (versionKey) {
+      if (lib[versionKey].from !== cnn) {
+        console.log('skipping', libKey, versionKey, lib[versionKey].from);
+        return;
+      }
+
+      var version = lib[versionKey];
+      if (!version.loaded) {
+        version.loaded = 1;
+        console.log('loading', libKey, versionKey);
+
+        asyncQueue.push(version
+          .get()
+          .then(function (f) {
+            console.log('loaded', libKey, versionKey);
+            version.get = function () { return f; };
+            version.loaded = 1;
+          }));
+      }
+    });
+  });
+}
+
+function shouldDeferLoading(args) {
+  var matchesOrStartsWith = args[0].some(function (item) {
+    return resport.some(function (resportItem) {
+      console.log('item to test', item);
+      return item === resportItem || item.indexOf(resportItem) === 0;
+    });
+  });
+  return !matchesOrStartsWith;
+}
 
 function asyncOperation() {
   // This is just an example; replace it with your own logic.
-  return new Promise((resolve) => {
-      if(__webpack_require__.S['default']) {
-          resolve()
-      }
-      console.log('init operation completed');
-      resolve(__webpack_require__.I('default', []));
+  return new Promise(function (resolve) {
+    console.log('init operation completed');
+    var prom = __webpack_require__.I('default', []);
+    Object.entries(__webpack_require__.S.default).forEach(function (lib) {
+      setEagerLoading(lib[0], Object.keys(lib[1])[0], cnn);
+    });
+    resolve(prom);
   })
-    .then(async (r) => {
-      const libKeys = Object.keys(__webpack_require__.S.default).reverse();
-      // Load 'react' and 'next' dependencies first
-      for (const libKey of libKeys) {
-        if (!(libKey.startsWith('react') || libKey.startsWith('next'))) continue;
-        const lib = __webpack_require__.S.default[libKey];
-        const versionKeys = [Object.keys(lib).reverse()[0]]
-        for (const versionKey of versionKeys) {
-          const version = lib[versionKey];
-          if (!version.loaded) {
-            // console.log('loading', libKey, versionKey, r);
-           version.loaded = 1;
+    .then(function () {
+      var libKeys = Object.keys(__webpack_require__.S.default).reverse();
+      var reactAndNextLibKeys = libKeys.filter(function (libKey) {
+        return libKey.startsWith('react') || libKey.startsWith('next');
+      });
+      var otherLibKeys = libKeys.filter(function (libKey) {
+        return !libKey.startsWith('react') && !libKey.startsWith('next');
+      });
 
-            await version
-              .get()
-              .then((f) => {
-              //  console.log('loaded', libKey, versionKey, r, f());
-                version.get = () => f;
-                version.loaded = true;
-              });
-          }
-        }
-      }
+      loadDependencies(reactAndNextLibKeys, cnn);
+      loadDependencies(otherLibKeys, cnn);
 
-      // Load the rest of the dependencies
-      for (const libKey of libKeys) {
-        if (libKey.startsWith('react') || libKey.startsWith('next')) continue;
-        const lib = __webpack_require__.S.default[libKey];
-        const versionKeys = [Object.keys(lib).reverse()[0]]
-        for (const versionKey of versionKeys) {
-        console.log(libKey, lib[versionKey],lib[versionKey].from)
-
-          const version = lib[versionKey];
-          console.log(version)
-          if (!version.loaded) {
-           version.loaded = 1;
-            console.log('loading', libKey, versionKey, r);
-            await version
-              .get()
-              .then((f) => {
-                console.log('loaded', libKey, versionKey, r);
-                version.get = () => f;
-                version.loaded = 1;
-              });
-          }
-        }
-      }
+      return Promise.all(asyncQueue);
     })
-    .then((r) => {
+    .then(function () {
       console.log('webpack is done negotiating dependency trees', cnn);
-      console.log('number of entry points to inveert startup',chunkQueue.length);
+      console.log('number of entry points to invert startup', chunkQueue.length);
       console.log('startup inversion in progress', chunkQueue);
-while (chunkQueue.length > 0) {
-      chunkLoadingGlobal.push(chunkQueue.shift());
 
-  //var item = chunkQueue.shift(); // Remove and get the first item from the queue
-  // console.log('calling deferred entry queue item:', item);
-  // webpackJsonpCallback.apply(null, [null].concat([item]));
- // console.log('deferred push call:', item[0]);
-  // webpackJsonpCallback.apply(chunkLoadingGlobal, item);
- //    webpackJsonpCallback.call(chunkLoadingGlobal,0, item);
-
-      //      webpackJsonpCallback.apply(
-      //   null,
-      //   [null].concat(Array.prototype.slice.call(item))
-      // );
-      //
-      // return webpackJsonpCallback.apply(chunkLoadingGlobal, item);
-}
+      while (chunkQueue.length > 0) {
+        chunkLoadingGlobal.push(chunkQueue.shift());
+      }
     });
 }
-
-chunkLoadingGlobal.forEach(function (item, index) {
-  asyncOperation().then(() => {
-  // console.log('forEach:', index, item);
-    webpackJsonpCallback.call(null, 0, item);
-    chunkLoadingGlobal.push.bind(chunkLoadingGlobal)(item);
-  });
-});
+var kickstart = asyncOperation()
 
 
 chunkLoadingGlobal.push = (function (originalPush) {
   return function () {
+    console.log('push catcher called push:', arguments[0][0]);
 
-  console.log('push catcher called push:', arguments[0][0]);
-  console.log('deffered queue', chunkQueue);
-  console.log('deffered report', resport);
-  var matchesOrStartsWith = arguments[0][0].some(function (item) {
-    return resport.some(function (resportItem) {
-    console.log('matcher', item, resportItem);
-      return item === resportItem || item.indexOf(resportItem) === 0;
-    });
-  });
-
-console.log('matchesOrStartsWith', matchesOrStartsWith);
-
-console.log('queue push hook: currently processing', arguments[0][0]);
-
-    if(!matchesOrStartsWith && arguments[0][0].includes('main')){
-    console.log('thinks its main');
-    resport = Array.prototype.concat.apply(resport, arguments[0][0]);
-    return Array.prototype.push.apply(chunkQueue, arguments);
-
-    return chunkQueue.push(arguments);
-    //   return chunkQueue.push(()=>{
-    // if(cnn === 'home_app'){
-    //
-    //   console.log('push queue main', arguments[0][0]);
-    //   }
-    //    return webpackJsonpCallback.apply(
-    //       null,
-    //       [null].concat(Array.prototype.slice.call(arguments))
-    //     );
-    //   })
-    }
-     if(!matchesOrStartsWith && arguments[0][0].some(item => item.startsWith('pages/'))){
-      resport = Array.prototype.concat.apply(resport, arguments[0][0]);
-    return Array.prototype.push.apply(chunkQueue, arguments);
-
-    return chunkQueue.push(arguments);
-    //   return chunkQueue.push(()=>{
-    // if(cnn === 'home_app'){
-    //
-    //   console.log('push queue page', arguments);
-    //   }
-    //    return webpackJsonpCallback.apply(
-    //       null,
-    //       [null].concat(Array.prototype.slice.call(arguments))
-    //     );
-    //   })
-    }
-    if(cnn === 'home_app'){
-    console.log('arguments', arguments[0][0]);
-    console.log('whole queue', chunkQueue);
-    console.log('report queue', resport);
+    if (shouldDeferLoading(arguments[0])) {
+      if (arguments[0][0].includes('main') || arguments[0][0].some(function (item) { return item.startsWith('pages/'); })) {
+        console.log('thinks its main or starts with pages/');
+        resport = Array.prototype.concat.apply(resport, arguments[0][0]);
+        return Array.prototype.push.apply(chunkQueue, arguments);
+      }
     }
 
+    if (cnn === 'home_app') {
+      console.log('arguments', arguments[0][0]);
+      console.log('whole queue', chunkQueue);
+      console.log('report queue', resport);
+    }
 
-      webpackJsonpCallback.apply(
-        null,
-        [null].concat(Array.prototype.slice.call(arguments))
-      );
-console.log('else push call:', arguments[0][0]);
+    webpackJsonpCallback.apply(
+      null,
+      [null].concat(Array.prototype.slice.call(arguments))
+    );
+    console.log('else push call:', arguments[0][0]);
 
-      return originalPush.apply(chunkLoadingGlobal, arguments);
-
+    return originalPush.apply(chunkLoadingGlobal, arguments);
   };
 })(chunkLoadingGlobal.push.bind(chunkLoadingGlobal));
 `;
