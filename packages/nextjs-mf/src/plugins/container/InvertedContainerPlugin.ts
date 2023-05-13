0cf5b9fc0ac776108ad2aa3d7f455609e5fa9f89
ScriptedAlchemy <zackary.l.jackson@gmail.com>
2023-05-13 15:46:49 -0700

    feat: RemoveEagerModulesFromRuntimePlugin

--- packages/nextjs-mf/src/plugins/container/InvertedContainerPlugin.ts
+++ packages/nextjs-mf/src/plugins/container/InvertedContainerPlugin.ts
@@ -5,6 +5,7 @@ import type { ModuleFederationPluginOptions } from '../types';
 import InvertedContainerRuntimeModule from './InvertedContainerRuntimeModule';
 import { RuntimeGlobals } from 'webpack';
 import Template from '../../../utils/Template';
+import RemoveEagerModulesFromRuntimePlugin from './RemoveEagerModulesFromRuntimePlugin';
 /**
  * Interface for InvertedContainerOptions, extending ModuleFederationPluginOptions.
  * This interface includes additional fields specific to the plugin's behavior.
@@ -52,6 +53,11 @@ class InvertedContainerPlugin {
    * @param {Compiler} compiler - Webpack compiler instance.
    */
   apply(compiler: Compiler) {
+    new RemoveEagerModulesFromRuntimePlugin({
+      container: this.options.container,
+      debug: this.options.debug,
+    }).apply(compiler);
+
     // Hook into the compilation process.
     compiler.hooks.thisCompilation.tap(
       'InvertedContainerPlugin',
@@ -105,120 +111,6 @@ class InvertedContainerPlugin {
           );
         }
 
-        compilation.hooks.optimizeChunkModules.tap(
-          'InvertedContainerPlugin',
-          (chunks, modules) => {
-            // Create a Set to store dependent modules
-            const dependentModules: Set<Module> = new Set();
-
-            // Create a Set to track visited modules during traversal
-            const visitedModules: Set<Module> = new Set();
-
-            // Function to traverse the module graph recursively
-            function traverseModuleGraph(module: Module) {
-              // Check if module has been visited before
-              if (visitedModules.has(module)) {
-                return; // Skip traversal if module has been visited
-              }
-
-              visitedModules.add(module); // Mark module as visited
-
-              // Skip traversal for certain module types
-              if (
-                module.type === 'provide-module' ||
-                module.type === 'consume-shared-module'
-              ) {
-                return;
-              }
-
-              dependentModules.add(module); // Add module to dependent modules set
-
-              module.dependencies.forEach((dependency) => {
-                // Get the dependent module using moduleGraph
-                const dependentModule =
-                  compilation.moduleGraph.getModule(dependency);
-
-                // If dependent module exists and is not already in dependentModules set, traverse it
-                if (dependentModule && !dependentModules.has(dependentModule)) {
-                  traverseModuleGraph(dependentModule);
-                }
-              });
-            }
-
-            // Iterate over chunks and modules
-            for (const chunk of chunks) {
-              // Check if the chunk has a runtime and matches the specified container name
-              if (
-                chunk.hasRuntime() &&
-                chunk.name === this.options?.container
-              ) {
-                // get all provide-shared modules
-                const eagerModulesInRemote =
-                  compilation.chunkGraph.getChunkModulesIterableBySourceType(
-                    chunk,
-                    'share-init'
-                  ) || [];
-
-                // Loop through modules in the chunk
-                for (const module of eagerModulesInRemote) {
-                  // If the module is eager, add it to eagerModulesInRemote set
-                  //@ts-ignore
-                  if (module._eager) {
-                    //@ts-ignore
-                    eagerModulesInRemote.add(module._request);
-                  }
-
-                  // If the module is eager or has a name starting with 'next', disconnect it from the chunk
-                  //@ts-ignore
-                  if (module?._eager || module?._name?.startsWith('next')) {
-                    compilation.chunkGraph.disconnectChunkAndModule(
-                      chunk,
-                      module
-                    );
-                  }
-                }
-
-                // Loop through modules again
-                for (const module of modules) {
-                  // Skip module if it is not in the chunk
-                  if (!compilation.chunkGraph.isModuleInChunk(module, chunk)) {
-                    continue;
-                  }
-
-                  // Process module if it is a NormalModule and its resource is in eagerModulesInRemote set
-                  if (module.constructor.name === 'NormalModule') {
-                    //@ts-ignore
-                    if (eagerModulesInRemote.has(module.resource)) {
-                      // Traverse the module graph recursively
-                      traverseModuleGraph(module);
-                    }
-                  }
-                }
-
-                // Loop through dependentModules set and disconnect them from the chunk
-                for (const moduleToRemove of dependentModules) {
-                  if (this.options.debug) {
-                    //@ts-ignore
-                    console.log('removing', moduleToRemove?.resource);
-                  }
-
-                  if (
-                    compilation.chunkGraph.isModuleInChunk(
-                      moduleToRemove,
-                      chunk
-                    )
-                  ) {
-                    compilation.chunkGraph.disconnectChunkAndModule(
-                      chunk,
-                      moduleToRemove
-                    );
-                  }
-                }
-              }
-            }
-          }
-        );
-
         compilation.hooks.optimizeChunks.tap(
           'InvertedContainerPlugin',
           (chunks) => {
@@ -226,21 +118,6 @@ class InvertedContainerPlugin {
               this.resolveContainerModule(compilation);
             if (!containerEntryModule) return;
             for (const chunk of chunks) {
-              if (
-                chunk.hasRuntime() &&
-                chunk.name === this.options?.container
-              ) {
-                const modules = chunk.getModules();
-                for (const module of modules) {
-                  if (module.constructor.name === 'NormalModule') {
-                    // compilation.chunkGraph.disconnectChunkAndModule(
-                    //   chunk,
-                    //   module
-                    // );
-                  }
-                }
-              }
-
               if (
                 !compilation.chunkGraph.isModuleInChunk(
                   containerEntryModule,
@@ -265,11 +142,6 @@ class InvertedContainerPlugin {
           'InvertedContainerPlugin',
           (chunks) => {
             chunks.forEach((chunk) => {
-              const chunkModules =
-                compilation.chunkGraph.getChunkRuntimeModulesIterable(chunk);
-              const runtimeRequirementsInChunk =
-                compilation.chunkGraph.getChunkRuntimeRequirements(chunk);
-
               chunk.files.forEach((file) => {
                 const asset = compilation.getAsset(file);
                 if (asset) {
@@ -323,12 +195,12 @@ class InvertedContainerPlugin {
               originalExec + 1,
               replaceSource.length
             );
-            // Push renamed exec pack into new source
-            newSource.push(
-              firstHalf.join('\n').replace(searchString, replaceString)
-            );
 
-            newSource.push(`
+            const part1 = firstHalf
+              .join('\n')
+              .replace(searchString, replaceString);
+
+            const part2 = `
             var ${searchString} = function(moduleId) {
 return __webpack_require__.own_remote.then(function(thing){
 return Promise.all(__webpack_require__.initRemotes);
@@ -338,7 +210,7 @@ return Promise.all(__webpack_require__.initConsumes);
 return ${replaceString}(moduleId);
 })
              };
-              `);
+              `;
 
             return Template.asString([
               '',
@@ -349,7 +221,8 @@ return ${replaceString}(moduleId);
                 `__webpack_require__.getEagerRemotesForChunkId(currentChunkId,__webpack_require__.initConsumes)`,
               ]),
               '}',
-              ...newSource,
+              part1,
+              part2,
               ...secondHalf,
               '',
             ]);
