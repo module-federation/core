54e839e276c4a8ae782b71ec46cdeb68b6514770
ScriptedAlchemy <zackary.l.jackson@gmail.com>
2023-05-12 18:27:42 -0700

    refactor: improve module hooks for eager loading and search

--- packages/nextjs-mf/src/plugins/container/InvertedContainerPlugin.ts
+++ packages/nextjs-mf/src/plugins/container/InvertedContainerPlugin.ts
@@ -82,7 +82,6 @@ class InvertedContainerPlugin {
           }
         };
 
-        // Hook the handler function into the compilation process.
         compilation.hooks.additionalChunkRuntimeRequirements.tap(
           'InvertedContainerPlugin',
           handler
@@ -92,28 +91,29 @@ class InvertedContainerPlugin {
           'InvertedContainerPlugin',
           (chunks) => {
             for (const chunk of chunks) {
-              if (chunk.hasRuntime()) {
-                //@ts-ignore
-                if (chunk.name === this.options?.container) {
-                  const eagerModulesInRemote =
-                    compilation.chunkGraph.getChunkModulesIterableBySourceType(
+              if (
+                chunk.hasRuntime() &&
+                chunk.name === this.options?.container
+              ) {
+                // const eagerModulesInRemote =
+                //   compilation.chunkGraph.getChunkModulesIterableBySourceType(
+                //     chunk,
+                //     'provide-module'
+                //   );
+                const modules = chunk.getModules();
+                for (const module of modules) {
+                  if (module.type === 'provide-module') {
+                    compilation.chunkGraph.disconnectChunkAndModule(
                       chunk,
-                      'provide-module'
+                      module
                     );
-                  if (eagerModulesInRemote) {
-                    for (const mod of eagerModulesInRemote) {
-                      console.log('found type', mod.type);
-                      compilation.chunkGraph.disconnectChunkAndModule(
-                        chunk,
-                        mod
-                      );
-                    }
                   }
                 }
               }
             }
           }
         );
+
         compilation.hooks.optimizeChunks.tap(
           'InvertedContainerPlugin',
           (chunks) => {
@@ -121,65 +121,54 @@ class InvertedContainerPlugin {
               this.resolveContainerModule(compilation);
             if (!containerEntryModule) return;
             for (const chunk of chunks) {
-              if (chunk.hasRuntime()) {
-                //@ts-ignore
-
-                if (chunk.name === this.options?.container) {
-                  const eagerModulesInRemote =
-                    compilation.chunkGraph.getChunkModulesIterableBySourceType(
+              if (
+                chunk.hasRuntime() &&
+                chunk.name === this.options?.container
+              ) {
+                const modules = chunk.getModules();
+                for (const module of modules) {
+                  if (module.type === 'provide-module') {
+                    compilation.chunkGraph.disconnectChunkAndModule(
                       chunk,
-                      'provide-module'
+                      module
                     );
-
-                  const moduels = chunk.getModules();
-                  for (const module of moduels) {
-                    if (module.type === 'provide-module') {
-                      compilation.chunkGraph.disconnectChunkAndModule(
-                        chunk,
-                        module
-                      );
-                    }
                   }
                 }
-                console.log(
-                  'chunk',
-                  chunk.name,
-                  !compilation.chunkGraph.isModuleInChunk(
-                    containerEntryModule,
-                    chunk
-                  )
+              }
+              // console.log(
+              //   'chunk',
+              //   chunk.name || chunk.id || chunk.debugId,
+              //   !compilation.chunkGraph.isModuleInChunk(
+              //     containerEntryModule,
+              //     chunk
+              //   )
+              // );
+              if (
+                !compilation.chunkGraph.isModuleInChunk(
+                  containerEntryModule,
+                  chunk
+                )
+              ) {
+                compilation.chunkGraph.connectChunkAndModule(
+                  chunk,
+                  containerEntryModule
                 );
-                if (
-                  !compilation.chunkGraph.isModuleInChunk(
-                    containerEntryModule,
-                    chunk
-                  )
-                ) {
-                  // if its the browser runtime, inject the container module into the host runtime
-                  // TODO: try and do the same on the server,
-                  // if (this.options.runtime === 'webpack-runtime') {
-                  compilation.chunkGraph.connectChunkAndModule(
-                    chunk,
-                    containerEntryModule
-                  );
-                  // }
-                }
               }
             }
           }
         );
-        // if (compiler.options.name === 'client') return;
+
         const hooks =
           compiler.webpack.javascript.JavascriptModulesPlugin.getCompilationHooks(
             compilation
           );
+
         compilation.hooks.afterOptimizeChunkAssets.tap(
-          'ChunkIdPlugin',
+          'InvertedContainerPlugin',
           (chunks) => {
             chunks.forEach((chunk) => {
               const chunkModules =
                 compilation.chunkGraph.getChunkRuntimeModulesIterable(chunk);
-
               const runtimeRequirementsInChunk =
                 compilation.chunkGraph.getChunkRuntimeRequirements(chunk);
 
@@ -194,12 +183,11 @@ class InvertedContainerPlugin {
                     //@ts-ignore
                     .replace('__INSERT_CH_ID__MF__', chunk.id);
                   const sourceBuffer = Buffer.from(source, 'utf-8');
-                  //@ts-ignore
                   const sourceObj = {
                     source: () => sourceBuffer,
                     size: () => sourceBuffer.length,
                   };
-                  // @ts-ignore
+                  //@ts-ignore
                   compilation.updateAsset(file, sourceObj);
                 }
               });
@@ -218,13 +206,12 @@ class InvertedContainerPlugin {
               return source;
             }
 
-            const newSource = [];
             const replaceSource = source.source().toString().split('\n');
 
             const searchString = '__webpack_exec__';
             const replaceString = '__webpack_exec_proxy__';
 
-            const originalExec = replaceSource.findIndex((s: string) =>
+            const originalExec = replaceSource.findIndex((s) =>
               s.includes(searchString)
             );
 
@@ -237,7 +224,7 @@ class InvertedContainerPlugin {
               originalExec + 1,
               replaceSource.length
             );
-            let currentChunkID: string | number | null = null;
+            let currentChunkID = null;
             for (const chunk of compilation.chunks) {
               if (
                 chunk.isOnlyInitial() &&
@@ -246,42 +233,38 @@ class InvertedContainerPlugin {
                 currentChunkID = chunk.id;
               }
             }
-            // Push renamed exec pack into new source
-            newSource.push(
-              firstHalf.join('\n').replace(searchString, replaceString)
-            );
-
-            newSource.push(`
-            var ${searchString} = function(moduleId) {
-return __webpack_require__.own_remote.then((thing)=>{
-// console.log('loaded pages remote if exists:',currentChunkId);
-return Promise.all(__webpack_require__.initRemotes);
-}).then(()=>{
-// console.log('loaded pages remote if exists:',currentChunkId);
-return Promise.all(__webpack_require__.initConsumes);
-}).then(()=>{
-// console.log('async startup for entrypoint done');
-// console.log('SUOULD REQUIRE PAged,m', moduleId);
-// console.log('SCOPE MEMORY CHECK',__webpack_require__.S === globalThis.backupScope);
-// console.log('SCOPE MEMORY CHECK',Object.keys(__webpack_require__.S), Object.keys(globalThis.backupScope))
-return ${replaceString}(moduleId);
-})
-             };
-              `);
-
-            return Template.asString([
+            const newSource = [
+              ...firstHalf,
               '',
-              'var currentChunkId = "__INSERT_CH_ID__MF__";',
-              `if(currentChunkId) {`,
+              `var currentChunkId = "${currentChunkID}";`,
+              'if (currentChunkId) {',
               Template.indent([
-                `__webpack_require__.getEagerSharedForChunkId(currentChunkId,__webpack_require__.initRemotes);`,
-                `__webpack_require__.getEagerRemotesForChunkId(currentChunkId,__webpack_require__.initConsumes)`,
+                '__webpack_require__.getEagerSharedForChunkId(currentChunkId, __webpack_require__.initRemotes);',
+                '__webpack_require__.getEagerRemotesForChunkId(currentChunkId, __webpack_require__.initConsumes);',
               ]),
               '}',
-              ...newSource,
-              ...secondHalf,
               '',
-            ]);
+              `var ${searchString} = function(moduleId) {`,
+              Template.indent([
+                'return __webpack_require__.own_remote.then((thing) => {',
+                Template.indent([
+                  'return Promise.all(__webpack_require__.initRemotes);',
+                ]),
+                '}).then(function() {',
+                Template.indent([
+                  '// console.log("loaded pages remote if exists:", currentChunkId);',
+                  'return Promise.all(__webpack_require__.initConsumes);',
+                ]),
+                '}).then(function() {',
+                Template.indent([`return ${replaceString}(moduleId);`]),
+                '})',
+              ]),
+              '};',
+              '',
+              ...secondHalf,
+            ];
+
+            return Template.asString(newSource);
           }
         );
       }
