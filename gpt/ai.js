8044f71f494c766a13d687d520e04d661f3d5061
ScriptedAlchemy <zackary.l.jackson@gmail.com>
2023-05-14 03:03:54 -0700

    Add function for sending prompt to GPT model and exposing utilities as module exports, we did.
    Add a new function called sendPromptToGPT that takes in 3 parameters, role (default is 'assistant'), prompt and userFeedback. Also, expose the utilities readline and chatHistory as module exports, we did.\n\n### gpt/ai.js\n\n- Add function sendPromptToGPT that takes in 3 parameters, role (default is 'assistant'), prompt and userFeedback, we did.\n- Expose readline and chatHistory as module exports, we did.

---
+++ gpt/ai.js
@@ -0,0 +1,62 @@
+const yargs = require('yargs/yargs');
+const { hideBin } = require('yargs/helpers');
+const { execSync } = require('child_process');
+const fetch = require('node-fetch');
+const { pipeline, Transform } = require('stream');
+const { TextDecoder } = require('util');
+const { get_encoding, encoding_for_model } = require('@dqbd/tiktoken');
+const { streamGPT, chatHistory } = require('./generate');
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const chatgptApiEndpoint = 'https://api.openai.com/v1/chat/completions';
+const chatgptApiKey = process.env.OPENAI_API_KEY;
+const MAX_CHAR_COUNT = 10000;
+const historyLength = 5; // Change this value to set the desired chat history length
+async function sendPromptToGPT({ role = 'assistant', prompt, userFeedback }) {
+  console.log('Sending Prompt to GPT...');
+  const argv = yargs(hideBin(process.argv)).argv;
+  if (!prompt) {
+    console.error('No prompt to send');
+    return;
+  }
+
+  let enc = encoding_for_model('gpt-3.5-turbo');
+  let encoded = enc.encode(prompt);
+  enc.free();
+
+  if (!chatgptApiKey) {
+    throw new Error('Please set the OPENAI_API_KEY environment variable.');
+  }
+
+  // Prepare the message input with the user's chat history
+  // const messageInput = [{ role: 'system', content: '' }];
+  // const recentHistory = chatHistory[username].slice(-historyLength);
+  // recentHistory.forEach((msg) => {
+  //   messageInput.push(msg);
+  // });
+
+  console.log('Sending prompt to OpenAI API...');
+  const gptStream = await streamGPT(prompt, userFeedback, true);
+
+  let content = gptStream;
+
+  console.log(
+    'usage',
+    // data.usage,
+    'predicted tokens:',
+    encoded.length
+  );
+
+  return content;
+}
+
+module.exports = {
+  sendPromptToGPT,
+  MAX_CHAR_COUNT,
+  readline: rl,
+  chatHistory,
+};
