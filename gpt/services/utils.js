afcddfd0ee269837d7aef5bb3a654f9379f29cba
ScriptedAlchemy <zackary.l.jackson@gmail.com>
2023-05-14 16:47:50 -0700

    Add code generation & utility functions
    This commit adds new code generation and utility functions to generate unit test code for a given Python function and description, search for the most semantically similar code chunks, and parse GPT responses.
    
    ### gpt/generateUnitTest.js
    
    - Create generateUnitTest function to generate unit test code for given Python code and description
    
    - Export generateUnitTest function
    
    ### gpt/services/utils.js
    
    - Add searchFileChunks function to search semantically similar code chunks for given search query and files
    
    - Add isFileNameInString function to check if a file name is part of a given string
    
    - Add parseGptResponse function to parse GPT response text into separate code blocks
    
    - Export all utility functions
    
    ### gpt/tests.js
    
    - Add script to test generateUnitTest function using command line arguments

---
+++ gpt/services/utils.js
@@ -0,0 +1,83 @@
+const { embedding } = require('./openai');
+// This is the minimum cosine similarity score that a file must have with the search query to be considered relevant
+// This is an arbitrary value, and you should vary/ remove this depending on the diversity of your dataset
+const COSINE_SIM_THRESHOLD = 0.72;
+// This function takes a search query and a list of files, and returns the chunks of text that are most semantically similar to the query
+async function searchFileChunks({ searchQuery, files, maxResults }) {
+  // Get the search query embedding
+  const searchQueryEmbeddingResponse = await embedding({
+    input: searchQuery,
+  });
+  // Get the first element in the embedding array
+  const searchQueryEmbedding =
+    searchQueryEmbeddingResponse.length > 0
+      ? searchQueryEmbeddingResponse[0]
+      : [];
+  // Rank the chunks by their cosine similarity to the search query (using dot product since the embeddings are normalized) and return this
+  const rankedChunks = files
+    // Map each file to an array of chunks with the file name and score
+    .flatMap((file) =>
+      file.chunks
+        ? file.chunks.map((chunk) => {
+            // Calculate the dot product between the chunk embedding and the search query embedding
+            const dotProduct = chunk.embedding.reduce(
+              (sum, val, i) => sum + val * searchQueryEmbedding[i],
+              0
+            );
+            // Assign the dot product as the score for the chunk
+            return { ...chunk, filename: file.name, score: dotProduct };
+          })
+        : []
+    )
+    // Sort the chunks by their scores in descending order
+    .sort((a, b) => b.score - a.score)
+    // Filter the chunks by their score above the threshold
+    .filter((chunk) => chunk.score > COSINE_SIM_THRESHOLD)
+    // Take the first maxResults chunks
+    .slice(0, maxResults);
+  return rankedChunks;
+}
+// A function that takes a file name and a string and returns true if the file name is contained in the string
+// after removing punctuation and whitespace from both
+const isFileNameInString = (fileName, str) => {
+  // Check if the input string is null or undefined
+  if (!str) {
+    return false;
+  }
+  // Convert both to lowercase and remove punctuation and whitespace
+  const normalizedFileName = fileName
+    .toLowerCase()
+    .replace(/[.,/#!$%^&*;:{}=-_~()\s]/g, '');
+  const normalizedStr = str
+    .toLowerCase()
+    .replace(/[.,/#!$%^&*;:{}=-_~()\s]/g, '');
+  // Return true if the normalized file name is included in the normalized string
+  return normalizedStr.includes(normalizedFileName);
+};
+const fs = require('fs');
+
+function parseGptResponse(response) {
+  console.log('Parsing GPT response...');
+  const blocks = response.split('__BLOCK_START__').slice(1); // Ignore everything before the first block
+  const parsed = {};
+  blocks.forEach((block) => {
+    const [filePath, code] = block.split('__CODE_START__');
+
+    const cleanedFilePath = filePath.trim();
+    console.log('Parsing block:', cleanedFilePath);
+
+    const cleanedCode = code.split('__BLOCK_END__')[0].trim(); // Ignore everything after the block end
+    parsed[cleanedFilePath] = cleanedCode;
+  });
+  Object.entries(parsed).forEach(([filePath, code]) => {
+    fs.writeFileSync(filePath + '.new', code, 'utf8');
+  });
+  console.log(Object.keys(parsed).length, 'files parsed');
+
+  return parsed;
+}
+module.exports = {
+  parseGptResponse,
+  isFileNameInString,
+  searchFileChunks,
+};
