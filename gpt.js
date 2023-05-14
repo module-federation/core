a8e448221b1434225c1e3a66e8ca883ef97539ba
ScriptedAlchemy <zackary.l.jackson@gmail.com>
2023-05-13 20:24:37 -0700

    Add functionality to generate and commit markdown-formatted commit messages
    This commit adds a new functionality to generate and commit markdown-formatted commit messages. The commit message generation is handled by OpenAI's GPT-3 and the markdown formatting is handled by the  library. The commit message generator prompts the user for a title and description of the changes made, as well as a list of files and their changes. The user is then given the option to accept or reject the generated commit message. If accepted, the commit is automatically made with the generated message.
    
    ### gpt.js
    
    - Added  and  functions from  to encode the commit message prompt for OpenAI's GPT-3
    - Added the  module from  to render markdown in the terminal
    - Added a function  to generate the commit message prompt and display the changes made to each file
    - Refactored the  function to use the  function and encode the prompt using  and
    - Added error handling for missing  environment variable
    - Refactored the  function to create a more readable markdown message
    - Added a function  to commit the changes using
    - Modified the  function to check if there are any changes to commit before running
    - Modified the  function to use the  function to commit the changes if the user accepts the generated commit message
    - Added a call to  to render the generated commit message in the terminal

--- gpt.js
+++ gpt.js
@@ -6,7 +6,10 @@ const { execSync } = require('child_process');
 const yargs = require('yargs/yargs');
 const { hideBin } = require('yargs/helpers');
 const readline = require('readline');
-
+const { get_encoding, encoding_for_model } = require('@dqbd/tiktoken');
+const { marked } = require('marked');
+const TerminalRenderer = require('marked-terminal');
+const chalk = require('chalk');
 dotenv.config();
 const MAX_CHAR_COUNT = 10000;
 
@@ -15,27 +18,18 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-async function generateCommitMsg() {
-  const argv = yargs(hideBin(process.argv)).argv;
-  let diff = execSync('git diff -U10 --staged').toString();
+// Configure marked to use `marked-terminal` as the renderer
+marked.setOptions({
+  // Define custom renderer
+  mangle: false,
+  headerIds: false,
+  renderer: new TerminalRenderer({
+    firstHeading: chalk.magenta.underline.bold,
+    heading: chalk.green.underline,
+  }),
+});
 
-  console.log(diff);
-
-  if (diff.length > MAX_CHAR_COUNT) {
-    const diffStat = execSync('git diff --staged --stat').toString();
-    const diffContext = execSync('git diff -U3 --staged').toString();
-    diff = diffStat + '\n' + diffContext;
-  }
-
-  const chatgptApiEndpoint = 'https://api.openai.com/v1/chat/completions';
-  const chatgptApiKey = process.env.OPENAI_API_KEY;
-  if (!chatgptApiKey) {
-    throw new Error('Please set the  OPENAI_API_KEY environment variable.');
-  }
-
-  const userCommitMsg = argv.message ? argv.message : '';
-
-  let prompt = `
+let createPrompt = (diff) => `
 Please suggest one detailed commit message for the code changes.\n
 Each commit message should be composed of a title and a description.\n
 In the description list overall summary of changes, and list changes for each file and the change sets for them.\n
@@ -51,10 +45,40 @@ Return it as a JSON object with the following format:\n
 Here are the changes: ${diff.slice(0, MAX_CHAR_COUNT)}
     `;
 
+async function generateCommitMsg() {
+  console.log('Generating commit message for...');
+  const argv = yargs(hideBin(process.argv)).argv;
+  let diff = execSync('git diff -U10 --staged').toString();
+  let files = execSync('git diff --name-only --cached').toString().trim();
+  console.log(files);
+  if (!files.length > 0) {
+    console.error('No files to commit');
+    return;
+  }
+  let prompt = createPrompt(diff);
+  const enc = encoding_for_model('gpt-3.5-turbo');
+  const encoded = enc.encode(prompt);
+  enc.free();
+
+  if (diff.length > MAX_CHAR_COUNT) {
+    console.warn('over max char count, truncating diff', diff.length);
+    const diffStat = execSync('git diff --staged --stat').toString();
+    const diffContext = execSync('git diff -U3 --staged').toString();
+    diff = diffStat + '\n' + diffContext;
+  }
+
+  const chatgptApiEndpoint = 'https://api.openai.com/v1/chat/completions';
+  const chatgptApiKey = process.env.OPENAI_API_KEY;
+  if (!chatgptApiKey) {
+    throw new Error('Please set the  OPENAI_API_KEY environment variable.');
+  }
+
+  const userCommitMsg = argv.message ? argv.message : '';
+
   if (userCommitMsg) {
     prompt += `\nThe user has already specified this commit message: ${userCommitMsg}\n`;
   }
-
+  console.log('Sending prompt to OpenAI API...');
   const response = await fetch(chatgptApiEndpoint, {
     method: 'POST',
     headers: {
@@ -65,9 +89,9 @@ Here are the changes: ${diff.slice(0, MAX_CHAR_COUNT)}
       model: 'gpt-3.5-turbo',
       messages: [{ role: 'assistant', content: prompt }],
       temperature: 0.7,
-      max_tokens: 4097,
     }),
   });
+  console.log('got response');
   const data = await response.json();
   let commitMsg = '';
 
@@ -84,6 +108,9 @@ Here are the changes: ${diff.slice(0, MAX_CHAR_COUNT)}
     .filter((line) => line.includes('Commit message:'))
     .map((line) => line.split(':')[1].trim());
 
+  console.log('usage', data.usage, 'predicted tokens:', encoded.length);
+  // console.log(require('util').inspect(data, { depth: null }));
+
   try {
     commitMsg = JSON.parse(data.choices[0].message.content);
   } catch (error) {
@@ -95,7 +122,7 @@ Here are the changes: ${diff.slice(0, MAX_CHAR_COUNT)}
   return commitMsg;
 }
 function createMarkdownCommit(commitMsg = {}) {
-  const markdownCommitMsg = `## ${commitMsg.Title}\n\n${
+  const markdownCommitMsg = `# ${commitMsg.Title}\n\n${
     commitMsg.Description
   }\n\n${commitMsg.Changes.map(
     (change) =>
@@ -107,17 +134,31 @@ function createMarkdownCommit(commitMsg = {}) {
   return markdownCommitMsg;
 }
 
+function gitCommit(title, body) {
+  execSync(`git commit -m "${title}" -m "${body}"`, { stdio: 'inherit' });
+}
+
 const run = async () => {
+  let files = execSync('git diff --name-only --cached').toString().trim();
+  console.log(files.length);
+  if (!files.length > 0) {
+    rl.write('No files to commit');
+    rl.write('\n');
+    rl.close();
+    return;
+  }
   const commitMsg = await generateCommitMsg();
 
-  console.log(require('util').inspect(commitMsg, { depth: null }));
   const markdown = createMarkdownCommit(commitMsg);
 
-  console.log(markdown);
+  const title = commitMsg.Title;
+  const body = markdown.replace(`# ${title}\n\n`, '');
 
-  rl.question('Accept this suggestion? (yes/no) ', (answer) => {
-    if (answer.toLowerCase() === 'yes') {
-      console.log('DONE');
+  console.log(marked(markdown));
+
+  rl.question('Accept this suggestion? (y/n) ', (answer) => {
+    if (answer.toLowerCase() === 'y') {
+      gitCommit(title, body);
       rl.close();
     } else {
       console.log('Generating a new suggestion...');
