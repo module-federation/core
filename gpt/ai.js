4efd24cc918dd53e80a78d2bab9c6e1cd43fb77c
ScriptedAlchemy <zackary.l.jackson@gmail.com>
2023-05-14 12:55:10 -0700

    Refactor ai.js and update dependencies
    This commit refactors the ai.js file, removing unnecessary imports and updating the way GPT-3 completions are fetched. It also updates the constants imported to reflect the changes.
    
    ### gpt/ai.js
    
    - Remove unnecessary imports such as yargs, child_process, node-fetch, and others
    
    - Update the import of completionStream from the services/openai module
    
    - Replace the usage of streamGPT with the new completionStream function
    
    - Remove the chatgptApiEndpoint and chatgptApiKey variables as they are no longer needed
    
    - Update the import of MAX_TOKENS from the constants module
    
    - Refactor the sendPromptToGPT function to use the new completionStream function
    
    - Update the module.exports to only export sendPromptToGPT and readline

--- gpt/ai.js
+++ gpt/ai.js
@@ -1,24 +1,14 @@
-const yargs = require('yargs/yargs');
-const { hideBin } = require('yargs/helpers');
-const { execSync } = require('child_process');
-const fetch = require('node-fetch');
-const { pipeline, Transform } = require('stream');
-const { TextDecoder } = require('util');
+const { completionStream } = require('./services/openai');
 const { get_encoding, encoding_for_model } = require('@dqbd/tiktoken');
-const { chatHistory, MAX_CHAR_COUNT, model } = require('./constants');
-const { streamGPT } = require('./generate');
+const { MAX_TOKENS, model } = require('./constants'); // make sure to import completionStream
 const readline = require('readline');
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-const chatgptApiEndpoint = 'https://api.openai.com/v1/chat/completions';
-const chatgptApiKey = process.env.OPENAI_API_KEY;
-const historyLength = 5; // Change this value to set the desired chat history length
 async function sendPromptToGPT({ role = 'assistant', prompt, userFeedback }) {
   console.log('Sending Prompt to GPT...');
-  const argv = yargs(hideBin(process.argv)).argv;
   if (!prompt && !userFeedback) {
     console.error('No prompt to send');
     return;
@@ -28,33 +18,22 @@ async function sendPromptToGPT({ role = 'assistant', prompt, userFeedback }) {
   let encoded = enc.encode(prompt);
   enc.free();
 
-  if (!chatgptApiKey) {
-    throw new Error('Please set the OPENAI_API_KEY environment variable.');
-  }
-
-  // Prepare the message input with the user's chat history
-  // const messageInput = [{ role: 'system', content: '' }];
-  // const recentHistory = chatHistory[username].slice(-historyLength);
-  // recentHistory.forEach((msg) => {
-  //   messageInput.push(msg);
-  // });
-
   console.log('Sending prompt to OpenAI API...');
-  console.log(
-    'usage',
-    // data.usage,
-    'predicted tokens:',
-    encoded && encoded.length
-  );
-  const gptStream = await streamGPT(prompt, userFeedback, true);
+  console.log('usage', 'predicted tokens:', encoded && encoded.length);
 
-  let content = gptStream;
+  const gptStream = await completionStream({
+    prompt,
+    temperature: 0.5,
+    max_tokens: MAX_TOKENS,
+  });
+
+  let content = '';
+  for await (const data of gptStream) {
+    content += data;
+  }
 
   return content;
 }
 
-module.exports = {
-  sendPromptToGPT,
-  readline: rl,
-  chatHistory,
-};
+module.exports.sendPromptToGPT = sendPromptToGPT;
+module.exports.readline = rl;
