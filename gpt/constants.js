73a4d77cd7f24afcf410c1e804697103fbb05e1f
Zack Jackson <zackary.l.jackson@gmail.com>
2023-05-15 16:56:49 -0700

    Gpt helper to main (#879)
    * DX: add GPT helper
    
    * DX: add GPT helper

---
+++ gpt/constants.js
@@ -0,0 +1,16 @@
+module.exports.pre_prompt = ``;
+module.exports.chatHistory = new Set();
+// module.exports.model = 'gpt-3.5-turbo';
+module.exports.model = 'gpt-4';
+module.exports.MAX_FILES_LENGTH = 2000 * 3;
+module.exports.MAX_TOKENS = Infinity; // 4000 if gpt3
+module.exports.showProgress = true;
+module.exports.MAX_CHAR_COUNT = 10000;
+module.exports.MAIN_BRANCH = 'kill_child_compilers';
+module.exports.filterStopwords = false;
+module.exports.response = {
+  end: ['_', 'END', 'OF', 'RESPONSE', '_'].join('_'),
+};
+// This is the minimum cosine similarity score that a file must have with the search query to be considered relevant
+// This is an arbitrary value, and you should vary/ remove this depending on the diversity of your dataset
+module.exports.COSINE_SIM_THRESHOLD = 0.72;
