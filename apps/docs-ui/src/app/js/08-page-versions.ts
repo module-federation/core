9c8d963c97b7c2cf7b3861ae2608e037599a440b
Caio Rolla <caiioocesar@outlook.com>
2023-05-18 14:59:43 -0300

    fix(docs-ui): design changes to docs

--- apps/docs-ui/src/app/js/08-page-versions.ts
+++ apps/docs-ui/src/app/js/08-page-versions.ts
@@ -1,15 +1,19 @@
 export default function () {
-  const toggles = document.querySelectorAll('.version-toggle');
+  const wrappers = document.querySelectorAll('.version-wrapper');
+  
 
-  if (!toggles) return;
+  if (!wrappers) return;
 
-  toggles.forEach((toggle) => {
-    const dropdown = toggle.querySelector('.version-dropdown');
+  wrappers.forEach((wrapper) => {
+    const toggle = wrapper.querySelector('.version-toggle');
+    const dropdown = wrapper.querySelector('.version-dropdown');
 
-    if (!dropdown) return;
+    if (!dropdown || !toggle) return;
 
     toggle.addEventListener('click', (e) => {
       dropdown.classList.toggle('hidden');
+      toggle.classList.toggle('bg-deep-purple-100');
+      toggle.classList.toggle('bg-blue-gray-200');
       e.stopPropagation();
     });
   });
