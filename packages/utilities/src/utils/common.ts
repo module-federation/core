faea7abe491d3ead632c3bdcbbd72b945d4dd48e
ScriptedAlchemy <zackary.l.jackson@gmail.com>
2023-05-17 22:46:15 -0700

    lint: handle fail remote

--- packages/utilities/src/utils/common.ts
+++ packages/utilities/src/utils/common.ts
@@ -238,6 +238,7 @@ export const loadScript = (keyOrRuntimeRemoteItem: string | RuntimeRemote) => {
             };
           });
         },
+        //eslint-disable-next-line
         init: () => {},
       };
     });
