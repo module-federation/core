d4cb8c8847ac4b3fc9c989949d5d3afe6a828296
Caio Rolla <caiioocesar@outlook.com>
2023-05-19 09:56:50 -0300

    fix(website): fix tailwind version mismatch

--- apps/website/src/components/line/line.tsx
+++ apps/website/src/components/line/line.tsx
@@ -12,7 +12,7 @@ export default component$((props: LineProps) => {
   useStylesScoped$(styles);
 
   return (
-    <div class={`flex items-center w-full ${props.class || ''}`}>
+    <div class={`flex items-center ${props.class || ''}`}>
       {props.showStart !== false && <div class="w-[3px] h-[3px] rounded-full bg-blue-gray-400"></div>}
       <div class="flex-1 w-full h-[1px] bg-blue-gray-400"></div>
       {props.showEnd !== false &&<div class="w-[3px] h-[3px] rounded-full bg-blue-gray-400"></div>}
