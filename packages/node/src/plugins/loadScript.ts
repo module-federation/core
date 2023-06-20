b703381da72c580f1f5536e328e14c3fed10b68b
Ran Shamay <ranshamay@microsoft.com>
2023-06-20 13:27:53 +0300

    debugger

--- packages/node/src/plugins/loadScript.ts
+++ packages/node/src/plugins/loadScript.ts
@@ -10,6 +10,7 @@ export default `
     console.log('ani torro, megfuzaz bedinero');
     console.log('ani torro, megfuzaz bedinero');
     console.log('ani torro, megfuzaz bedinero');
+    debugger;
     console.log('ani torro, megfuzaz bedinero');
     if (global.webpackChunkLoad) {
       global.webpackChunkLoad(url).then(function (resp) {
