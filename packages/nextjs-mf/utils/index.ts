9c41f4c8a2cdbb4b4b49ecdb03227492c05369c5
rcanfield <Rcanfield86@gmail.com>
2023-06-21 17:49:51 -0400

    Revert nextjs-mf changes

--- packages/nextjs-mf/utils/index.ts
+++ packages/nextjs-mf/utils/index.ts
@@ -1,7 +1,7 @@
 import * as React from 'react';
 export {
   extractUrlAndGlobal,
-  loadAndInitializeRemote,
+  injectScript,
 } from '@module-federation/utilities';
 // @ts-ignore
 export { flushChunks } from '@module-federation/node/utils';
