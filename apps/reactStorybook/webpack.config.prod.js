0378b6bf4697fd8a3c9ddd2472cd34bd4ff244a7
Zack Jackson <zackary.l.jackson@gmail.com>
2023-06-30 00:34:26 -0700

    chore: nrwl/storybook to nx/storybook (#1071)
    * chore: nrwl/storybook to nx/storybook
    
    * chore: nrwl/webpack to nx/webpack

--- apps/reactStorybook/webpack.config.prod.js
+++ apps/reactStorybook/webpack.config.prod.js
@@ -1,4 +1,4 @@
-const { composePlugins, withNx } = require('@nrwl/webpack');
+const { composePlugins, withNx } = require('@nx/webpack');
 const { withReact } = require('@nx/react');
 const { withModuleFederation } = require('@nx/react/module-federation');
 
