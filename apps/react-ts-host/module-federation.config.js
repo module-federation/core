0d25754dadcf15ed2184624ceccca5e7e643372c
rcanfield <Rcanfield86@gmail.com>
2023-07-01 20:33:56 -0400

    Updates

--- apps/react-ts-host/module-federation.config.js
+++ apps/react-ts-host/module-federation.config.js
@@ -5,7 +5,10 @@
  **/
 const moduleFederationConfig = {
   name: 'react_ts_host',
-  remotes: [['react_ts_remote', 'http://localhost:3004/remoteEntry.js']],
+  library: { type: 'var', name: 'react_ts_host' },
+  remotes: [
+    ['react_ts_remote', 'react_ts_remote@http://localhost:3004/remoteEntry.js'],
+  ],
   exposes: {
     './Component': './src/app/nx-welcome.tsx',
     './ab/c/Component': './src/app/App.tsx',
