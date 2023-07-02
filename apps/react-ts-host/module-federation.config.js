c6aba7332e23c7f36f3403a043916fd007d1bf59
rcanfield <Rcanfield86@gmail.com>
2023-07-01 20:31:24 -0400

    ESM changes

--- apps/react-ts-host/module-federation.config.js
+++ apps/react-ts-host/module-federation.config.js
@@ -4,7 +4,7 @@
  * @type {import('@nrwl/devkit').ModuleFederationConfig}
  **/
 const moduleFederationConfig = {
-  name: 'react-ts-host',
+  name: 'react_ts_host',
   library: { type: 'var', name: 'react_ts_host' },
   remotes: [['react_ts_remote', 'http://localhost:3004/remoteEntry.js']],
   exposes: {
