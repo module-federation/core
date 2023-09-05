40b40e7c4e74cf867533e65cbc316cbf8466ea48
ScriptedAlchemy <zackary.l.jackson@gmail.com>
2023-09-04 18:00:35 -0700

    refactor: improve code modularization in LoadFileChunkLoadingRuntimeModule.ts
    Improved the code modularization by creating methods for the generation of `ExternalInstallChunkCode` and `HmrCode`. These pieces of logic were previously in the `generate` method, but long stretches of code like these can make a method hard to read and understand. Now the `generate` method simply calls the new, succinctly named methods. The actual logic inside these chunks of code has not been altered during this refactoring; only their placement within the file has changed.
    
    ### packages/node/src/plugins/LoadFileChunkLoadingRuntimeModule.ts
    
    - Created `generateExternalInstallChunkCode` method
    
    - Created `generateHmrCode` method
    
    - Refactored `generate` method to call the two new methods

--- packages/node/src/plugins/LoadFileChunkLoadingRuntimeModule.ts
+++ packages/node/src/plugins/LoadFileChunkLoadingRuntimeModule.ts
@@ -263,6 +263,87 @@ class ReadFileChunkLoadingRuntimeModule extends RuntimeModule {
       '};',
     ]);
   }
+  generateExternalInstallChunkCode(withExternalInstallChunk: boolean): string {
+    if (!withExternalInstallChunk) {
+      return '// no external install chunk';
+    }
+
+    return Template.asString([
+      'module.exports = __webpack_require__;',
+      `${RuntimeGlobals.externalInstallChunk} = function(){`,
+      this.options.debug
+        ? `console.debug('node: webpack installing to install chunk id:', arguments['0'].id);`
+        : '',
+      `return installChunk.apply(this, arguments)};`,
+    ]);
+  }
+
+  generateHmrCode(withHmr: boolean, rootOutputDir: string): string {
+    if (!withHmr) {
+      return '// no HMR';
+    }
+
+    return Template.asString([
+      'function loadUpdateChunk(chunkId, updatedModulesList) {',
+      Template.indent([
+        'return new Promise(function(resolve, reject) {',
+        Template.indent([
+          `var filename = require('path').join(__dirname, ${JSON.stringify(
+            rootOutputDir
+          )} + ${RuntimeGlobals.getChunkUpdateScriptFilename}(chunkId));`,
+          "require('fs').readFile(filename, 'utf-8', function(err, content) {",
+          Template.indent([
+            'if(err) return reject(err);',
+            'var update = {};',
+            "require('vm').runInThisContext('(function(exports, require, __dirname, __filename) {' + content + '\\n})', filename)" +
+              "(update, require, require('path').dirname(filename), filename);",
+            'var updatedModules = update.modules;',
+            'var runtime = update.runtime;',
+            'for(var moduleId in updatedModules) {',
+            Template.indent([
+              `if(${RuntimeGlobals.hasOwnProperty}(updatedModules, moduleId)) {`,
+              Template.indent([
+                `currentUpdate[moduleId] = updatedModules[moduleId];`,
+                'if(updatedModulesList) updatedModulesList.push(moduleId);',
+              ]),
+              '}',
+            ]),
+            '}',
+            'if(runtime) currentUpdateRuntime.push(runtime);',
+            'resolve();',
+          ]),
+          '});',
+        ]),
+        '});',
+      ]),
+      '}',
+      '',
+      Template.getFunctionContent(
+        // eslint-disable-next-line @typescript-eslint/no-var-requires
+        require('webpack/lib/hmr/JavascriptHotModuleReplacement.runtime.js')
+      )
+        .replace(/\$key\$/g, 'readFileVm')
+        .replace(/\$installedChunks\$/g, 'installedChunks')
+        .replace(/\$loadUpdateChunk\$/g, 'loadUpdateChunk')
+        .replace(/\$moduleCache\$/g, RuntimeGlobals.moduleCache)
+        .replace(/\$moduleFactories\$/g, RuntimeGlobals.moduleFactories)
+        .replace(
+          /\$ensureChunkHandlers\$/g,
+          RuntimeGlobals.ensureChunkHandlers
+        )
+        .replace(/\$hasOwnProperty\$/g, RuntimeGlobals.hasOwnProperty)
+        .replace(/\$hmrModuleData\$/g, RuntimeGlobals.hmrModuleData)
+        .replace(
+          /\$hmrDownloadUpdateHandlers\$/g,
+          RuntimeGlobals.hmrDownloadUpdateHandlers
+        )
+        .replace(
+          /\$hmrInvalidateModuleHandlers\$/g,
+          RuntimeGlobals.hmrInvalidateModuleHandlers
+        ),
+    ]);
+  }
+
   /**
    * @returns {string} runtime code
    */
@@ -358,78 +439,9 @@ class ReadFileChunkLoadingRuntimeModule extends RuntimeModule {
         : '// no chunk loading',
 
       '',
-      withExternalInstallChunk
-        ? Template.asString([
-            'module.exports = __webpack_require__;',
-            `${RuntimeGlobals.externalInstallChunk} = function(){`,
-            this.options.debug
-              ? `console.debug('node: webpack installing to install chunk id:', arguments['0'].id);`
-              : '',
-            `return installChunk.apply(this, arguments)};`,
-          ])
-        : '// no external install chunk',
+      this.generateExternalInstallChunkCode(withExternalInstallChunk),
       '',
-      withHmr
-        ? Template.asString([
-            'function loadUpdateChunk(chunkId, updatedModulesList) {',
-            Template.indent([
-              'return new Promise(function(resolve, reject) {',
-              Template.indent([
-                `var filename = require('path').join(__dirname, ${JSON.stringify(
-                  rootOutputDir
-                )} + ${RuntimeGlobals.getChunkUpdateScriptFilename}(chunkId));`,
-                "require('fs').readFile(filename, 'utf-8', function(err, content) {",
-                Template.indent([
-                  'if(err) return reject(err);',
-                  'var update = {};',
-                  "require('vm').runInThisContext('(function(exports, require, __dirname, __filename) {' + content + '\\n})', filename)" +
-                    "(update, require, require('path').dirname(filename), filename);",
-                  'var updatedModules = update.modules;',
-                  'var runtime = update.runtime;',
-                  'for(var moduleId in updatedModules) {',
-                  Template.indent([
-                    `if(${RuntimeGlobals.hasOwnProperty}(updatedModules, moduleId)) {`,
-                    Template.indent([
-                      `currentUpdate[moduleId] = updatedModules[moduleId];`,
-                      'if(updatedModulesList) updatedModulesList.push(moduleId);',
-                    ]),
-                    '}',
-                  ]),
-                  '}',
-                  'if(runtime) currentUpdateRuntime.push(runtime);',
-                  'resolve();',
-                ]),
-                '});',
-              ]),
-              '});',
-            ]),
-            '}',
-            '',
-            Template.getFunctionContent(
-              // eslint-disable-next-line @typescript-eslint/no-var-requires
-              require('webpack/lib/hmr/JavascriptHotModuleReplacement.runtime.js')
-            )
-              .replace(/\$key\$/g, 'readFileVm')
-              .replace(/\$installedChunks\$/g, 'installedChunks')
-              .replace(/\$loadUpdateChunk\$/g, 'loadUpdateChunk')
-              .replace(/\$moduleCache\$/g, RuntimeGlobals.moduleCache)
-              .replace(/\$moduleFactories\$/g, RuntimeGlobals.moduleFactories)
-              .replace(
-                /\$ensureChunkHandlers\$/g,
-                RuntimeGlobals.ensureChunkHandlers
-              )
-              .replace(/\$hasOwnProperty\$/g, RuntimeGlobals.hasOwnProperty)
-              .replace(/\$hmrModuleData\$/g, RuntimeGlobals.hmrModuleData)
-              .replace(
-                /\$hmrDownloadUpdateHandlers\$/g,
-                RuntimeGlobals.hmrDownloadUpdateHandlers
-              )
-              .replace(
-                /\$hmrInvalidateModuleHandlers\$/g,
-                RuntimeGlobals.hmrInvalidateModuleHandlers
-              ),
-          ])
-        : '// no HMR',
+      this.generateHmrCode(withHmr, rootOutputDir),
       '',
       withHmrManifest
         ? Template.asString([
