d420ad94b7515123254af45c61704abcc0971511
ScriptedAlchemy <zackary.l.jackson@gmail.com>
2023-09-08 00:18:29 -0700

    fix(node): use ES6 imports and override method in FederationModuleInfoRuntimeModule (#bytedance)

--- packages/node/src/plugins/FederationModuleInfoRuntimeModule.ts
+++ packages/node/src/plugins/FederationModuleInfoRuntimeModule.ts
@@ -1,8 +1,7 @@
 'use strict';
 
-const { RuntimeGlobals } = require('webpack');
-const { RuntimeModule } = require('webpack');
-const { Template } = require('webpack');
+import { RuntimeGlobals, RuntimeModule } from 'webpack';
+import { Template } from 'webpack';
 
 class FederationModuleInfoRuntimeModule extends RuntimeModule {
   constructor() {
@@ -13,7 +12,7 @@ class FederationModuleInfoRuntimeModule extends RuntimeModule {
    * @returns {string} runtime code
    */
   // eslint-disable-next-line max-lines-per-function
-  generate() {
+  override generate() {
     return Template.asString([
       `${RuntimeGlobals.require}.federation = {`,
       Template.indent([`cache: {},`, `remotes: {},`, `moduleInfo: {},`]),
