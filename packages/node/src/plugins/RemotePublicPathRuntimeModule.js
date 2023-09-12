061285e0b6210baa1dd502dc94fd57fd9a8af822
ScriptedAlchemy <zackary.l.jackson@gmail.com>
2023-09-12 12:32:00 -0700

    feat(node): enhance error handling and remote container registry in RemotePublicPathRuntimeModule (#bytedance)
    This commit improves error handling for automatic publicPath calculation in server environments. It also adds a new way to get URLs from remote container registry. This may improve the robustness of the code and its ability to handle different server environments.

--- packages/node/src/plugins/RemotePublicPathRuntimeModule.js
+++ packages/node/src/plugins/RemotePublicPathRuntimeModule.js
@@ -12,7 +12,7 @@ class AutoPublicPathRuntimeModule extends RuntimeModule {
    */
   generate() {
     const { compilation } = this;
-    const { scriptType, path, publicPath } = compilation.outputOptions;
+    const { scriptType, path, publicPath, importMetaName, uniqueName } = compilation.outputOptions;
 
     // If publicPath is not "auto", return the static value
     if (publicPath !== "auto") {
@@ -34,34 +34,56 @@ class AutoPublicPathRuntimeModule extends RuntimeModule {
       }
     );
     const undoPath = getUndoPath(chunkName, path, false);
+    const ident = Template.toIdentifier(uniqueName);
 
     return Template.asString([
       "var scriptUrl;",
-      'var remoteContainerRegistry = globalThis.__remote_scope__._config;',
+      // its an esproxy so nesting into _config directly is not possible
+      "var remoteReg = globalThis.__remote_scope__._config;",
+      `
+      let remoteContainerRegistry = {
+        get url() {
+          return remoteReg[${JSON.stringify(ident)}];
+        }
+      };
+      `,
+      
       scriptType === "module"
-        ? `if (typeof remoteContainerRegistry[${JSON.stringify(this.options.name)}] === "string") scriptUrl = remoteContainerRegistry[${JSON.stringify(this.options.name)}]`
-        :  Template.asString([
-          'scriptUrl = __dirname',
+        ? Template.asString([
+          'try {',           
+            Template.indent([
+              `scriptUrl = new Function('return typeof ${importMetaName}.url === "string" ? ${importMetaName}.url : undefined;')();`,
+            ]),
+            '} catch (e) {',
+              Template.indent([
+              `if (typeof remoteContainerRegistry.url === "string") {`,
+              Template.indent('scriptUrl = remoteContainerRegistry.url;'),
+              '} else {',
+              Template.indent('scriptUrl = __filename;'),
+              '}',
+            ]),
+          '}'
+          ])
+        : Template.asString([
+          `if (${RuntimeGlobals.global}.importScripts) scriptUrl = ${RuntimeGlobals.global}.location + "";`,
+          `var document = ${RuntimeGlobals.global}.document;`,
+          "if (!scriptUrl && document) {",
+          Template.indent([
+            `if (document.currentScript)`,
+            Template.indent(`scriptUrl = document.currentScript.src`),
+            "if (!scriptUrl) {",
+            Template.indent([
+              'var scripts = document.getElementsByTagName("script");',
+              "if(scripts.length) scriptUrl = scripts[scripts.length - 1].src"
+            ]),
+            "}"
+          ]),
+          "}",
         ]),
-        // Template.asString([
-        //   `if (${RuntimeGlobals.global}.importScripts) scriptUrl = ${RuntimeGlobals.global}.location + "";`,
-        //   `var document = ${RuntimeGlobals.global}.document;`,
-        //   "if (!scriptUrl && document) {",
-        //   Template.indent([
-        //     `if (document.currentScript)`,
-        //     Template.indent(`scriptUrl = document.currentScript.src`),
-        //     "if (!scriptUrl) {",
-        //     Template.indent([
-        //       'var scripts = document.getElementsByTagName("script");',
-        //       "if(scripts.length) scriptUrl = scripts[scripts.length - 1].src"
-        //     ]),
-        //     "}"
-        //   ]),
-        //   "}"
-        // ]),
-      "// When supporting browsers where an automatic publicPath is not supported you must specify an output.publicPath manually via configuration",
+      "console.log('scriptUrl', scriptUrl);",
+      "// When supporting server environments where an automatic publicPath is not supported, you must specify an output.publicPath manually via configuration",
       '// or pass an empty string ("") and set the __webpack_public_path__ variable from your code to use your own logic.',
-      'if (!scriptUrl) throw new Error("Automatic publicPath is not supported in this browser");',
+      'if (!scriptUrl) throw new Error("Unable to calculate automatic public path");',
       'scriptUrl = scriptUrl.replace(/#.*$/, "").replace(/\\?.*$/, "").replace(/\\/[^\\/]+$/, "/");',
       !undoPath
         ? `${RuntimeGlobals.publicPath} = scriptUrl;`
