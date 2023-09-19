28f737e9d93b09e13988acc5154a6f917f359fc8
ScriptedAlchemy <zackary.l.jackson@gmail.com>
2023-09-18 18:07:18 -0700

    add migration / sync steps

--- packages/enhanced/types/lib/container/RemoteToExternalDependency.d.ts
+++
@@ -1,3 +0,0 @@
-export = RemoteToExternalDependency;
-declare class RemoteToExternalDependency extends ModuleDependency {}
-import ModuleDependency = require('webpack/lib/dependencies/ModuleDependency');
