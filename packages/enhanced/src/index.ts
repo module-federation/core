92130e2162c7822f81822ea1ce80b6b1e0aa537e
ScriptedAlchemy <zackary.l.jackson@gmail.com>
2023-09-23 21:45:56 -0700

    export contaainr plugins

--- packages/enhanced/src/index.ts
+++ packages/enhanced/src/index.ts
@@ -3,3 +3,4 @@ export { default as ModuleInfoRuntimePlugin } from './runtime/ModuleInfoRuntimeP
 export { default as ModuleFederationPlugin } from './lib/container/ModuleFederationPlugin';
 export { default as ContainerReferencePlugin } from './lib/container/ContainerReferencePlugin';
 export { default as ContainerPlugin } from './lib/container/ContainerPlugin';
+export { default as ContainerEntryModule } from './lib/container/ContainerEntryModule';
