12ff71c673be95381ea583f94117527a31fc53a6
ScriptedAlchemy <zackary.l.jackson@gmail.com>
2023-09-26 16:35:42 -0700

    chore: patching changes from next branch

--- apps/3000-home/next.config.js
+++ apps/3000-home/next.config.js
@@ -96,4 +96,3 @@ const nextConfig = {
 };
 
 module.exports = withNx(nextConfig);
-
--- apps/3000-home/pages/dynamic-remote.js
+++ apps/3000-home/pages/dynamic-remote.js
@@ -1,8 +1,8 @@
 import React from 'react';
 //eslint-disable-next-line
-// import { injectScript } from '@module-federation/nextjs-mf/utilities';
+import { injectScript } from '@module-federation/nextjs-mf/utilities';
 // example of dynamic remote import on server and client
-// const isServer = typeof window === 'undefined';
+const isServer = typeof window === 'undefined';
 //could also use
 // getModule({
 //   remoteContainer: {
@@ -13,25 +13,25 @@ import React from 'react';
 // }).then((sample) => {
 //   console.log(sample)
 // });
-// const dynamicContainer = injectScript({
-//   global: 'checkout',
-//   url: `http://localhost:3002/_next/static/${
-//     isServer ? 'ssr' : 'chunks'
-//   }/remoteEntry.js`,
-// }).then((container) => {
-//   return container.get('./CheckoutTitle').then((factory) => {
-//     return factory();
-//   });
-// });
-// const DynamicComponent = React.lazy(() => dynamicContainer);
+const dynamicContainer = injectScript({
+  global: 'checkout',
+  url: `http://localhost:3002/_next/static/${
+    isServer ? 'ssr' : 'chunks'
+  }/remoteEntry.js`,
+}).then((container) => {
+  return container.get('./CheckoutTitle').then((factory) => {
+    return factory();
+  });
+});
+const DynamicComponent = React.lazy(() => dynamicContainer);
 
 // eslint-disable-next-line react/display-name
 export default (props) => {
   return (
     <>
-      {/*<React.Suspense>*/}
-      {/*  <DynamicComponent />*/}
-      {/*</React.Suspense>*/}
+      <React.Suspense>
+        <DynamicComponent />
+      </React.Suspense>
       <p>Code from GSSP:</p>
       <pre>{props.code}</pre>
     </>
@@ -41,7 +41,7 @@ export default (props) => {
 export async function getServerSideProps() {
   return {
     props: {
-      // code: (await dynamicContainer).default.toString(),
+      code: (await dynamicContainer).default.toString(),
     },
   };
 }
