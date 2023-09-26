12ed54c87ba539bc2a79cdee86058f0a2776653e
ScriptedAlchemy <zackary.l.jackson@gmail.com>
2023-09-25 20:26:28 -0700

    fix: export parseRemotes

--- packages/node/src/index.ts
+++ packages/node/src/index.ts
@@ -4,3 +4,4 @@ export { default as UniversalFederationPlugin } from './plugins/UniversalFederat
 export { default as ChunkCorrelationPlugin } from './plugins/ChunkCorrelationPlugin';
 export { default as RemotePublicPathPlugin } from './plugins/RemotePublicPathRuntimeModule';
 export { default as NodeContainerPlugin } from './plugins/NodeContainerPlugin';
+export { parseRemotes } from './plugins/NodeFederationPlugin';
