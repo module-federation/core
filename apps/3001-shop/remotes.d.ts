2bee3649cac603551a0b368d9120d05265b1a6e5
rcanfield <Rcanfield86@gmail.com>
2023-09-26 18:20:44 -0400

    Add TS modules for sample Next apps

--- apps/3001-shop/remotes.d.ts
+++ apps/3001-shop/remotes.d.ts
@@ -1,1 +1,5 @@
 declare module 'home/SharedNav';
+declare module 'checkout/pages/checkout/exposed-pages';
+declare module 'checkout/pages/checkout/index';
+declare module 'checkout/pages/checkout/test-check-button';
+declare module 'checkout/pages/checkout/test-title';
--- apps/3002-checkout/pages/home/exposed-pages.tsx
+++ apps/3002-checkout/pages/home/exposed-pages.tsx
@@ -1,4 +1,3 @@
-//@ts-ignore
 import ExposedPages from 'home/pages/home/exposed-pages';
 
 export default ExposedPages;
