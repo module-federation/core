347e4c96e87ff4f28dce319fc6b2fe40f1cbabee
ScriptedAlchemy <zackary.l.jackson@gmail.com>
2023-09-27 20:45:51 -0700

    fix: legacy scope duplication on recreation

--- packages/enhanced/src/runtime/ModuleInfoRuntimeModule.ts
+++ packages/enhanced/src/runtime/ModuleInfoRuntimeModule.ts
@@ -1,6 +1,6 @@
-'use strict';
+"use strict";
 
-import { RuntimeGlobals, RuntimeModule, Template } from 'webpack';
+import { RuntimeGlobals, RuntimeModule, Template } from "webpack";
 
 /**
  * Class representing a runtime module for federation module info.
@@ -11,7 +11,7 @@ export class ModuleInfoRuntimeModule extends RuntimeModule {
    * Create a ModuleInfoRuntimeModule.
    */
   constructor() {
-    super('federation module info runtime', RuntimeModule.STAGE_BASIC);
+    super("federation module info runtime", RuntimeModule.STAGE_BASIC);
   }
 
   /**
@@ -24,39 +24,39 @@ export class ModuleInfoRuntimeModule extends RuntimeModule {
       Template.indent([`cache: {},`, `remotes: {},`, `moduleInfo: {}, `]),
       `};`,
       Template.indent([
-        '// backward compatible global proxy',
+        "// backward compatible global proxy",
         `let oldScope = globalThis.__remote_scope__ || {};`,
-             //TODO: move this elsewhere
+        //TODO: move this elsewhere
         `if(${RuntimeGlobals.runtimeId} === 'webpack-runtime' && globalThis.__remote_scope__) {`,
-          // during lazy compilations, webpack will push updated runtime modules ito webpack runtime
-          // this causes sharing to reinitialize, but remotes in the cache are already initialized
-          // when this happens, we will clear the cache so that the runtime re-initilizes remotes as well
-          Template.indent([
-            'globalThis.__remote_scope__ = {}',
-          ]),
-          `}`,
+        // during lazy compilations, webpack will push updated runtime modules ito webpack runtime
+        // this causes sharing to reinitialize, but remotes in the cache are already initialized
+        // when this happens, we will clear the cache so that the runtime re-initilizes remotes as well
+        Template.indent([
+          "globalThis.__remote_scope__ = {}"
+        ]),
+        `}`,
         `if (!globalThis.__remote_scope__ || !globalThis.__remote_scope__.moduleInfo) {
          console.log("create proxy",${RuntimeGlobals.runtimeId},!globalThis.__remote_scope__, globalThis.__remote_scope__ && !globalThis.__remote_scope__.moduleInfo);
          `,
         `globalThis.__remote_scope__ = new Proxy(${RuntimeGlobals.require}.federation, {`,
         Template.indent([
           `get: function(target, prop, receiver) {`,
-          'var result;',
+          "var result;",
           Template.indent([
             `if (prop === '_config') {`,
             Template.indent([
-              `result = ${RuntimeGlobals.require}.federation.remotes;`,
+              `result = ${RuntimeGlobals.require}.federation.remotes;`
             ]),
             `} else if(prop === 'moduleInfo') {`,
             Template.indent([
-              `result = ${RuntimeGlobals.require}.federation[prop];`,
+              `result = ${RuntimeGlobals.require}.federation[prop];`
             ]),
             `} else {`,
             Template.indent([
-              `result = ${RuntimeGlobals.require}.federation.cache[prop];`,
+              `result = ${RuntimeGlobals.require}.federation.cache[prop];`
             ]),
             `}`,
-            'return result;',
+            "return result;"
           ]),
           `},`,
 
@@ -65,35 +65,36 @@ export class ModuleInfoRuntimeModule extends RuntimeModule {
           Template.indent([
             `if (prop === '_config') {`,
             Template.indent([
-              `${RuntimeGlobals.require}.federation.remotes = value;`,
+              `${RuntimeGlobals.require}.federation.remotes = value;`
             ]),
             `} else {`,
             Template.indent([
-              `${RuntimeGlobals.require}.federation.cache[prop] = value;`,
+              `${RuntimeGlobals.require}.federation.cache[prop] = value;`
             ]),
             `}`,
-            `return true;`,
+            `return true;`
           ]),
-          `}`,
+          `}`
         ]),
         `});`,
 
-
+        "if(!oldScope.moduleInfo) {",
         Template.indent([
           `for (let key in oldScope._config) {`,
           Template.indent([
-            `globalThis.__remote_scope__._config[key] = oldScope[key];`,
+            `globalThis.__remote_scope__._config[key] = oldScope[key];`
           ]),
           `}`,
           `for (let key in oldScope) {`,
           Template.indent([
-            'if(key === "_config") continue;',
+            "if(key === \"_config\" || !oldScope[key]) continue;",
             `globalThis.__remote_scope__[key] = oldScope[key];`,
           ]),
-          `}`,
+          "}",
+          `}`
         ]),
-        `}`,
-      ]),
+        `}`
+      ])
     ]);
   }
 }
