8eea1a84ae6a12f69dbb16d00f52ec902efbdda6
ScriptedAlchemy <zackary.l.jackson@gmail.com>
2023-09-28 11:58:23 -0700

    fix: improve logic in runtime module info proxy

--- packages/enhanced/src/runtime/ModuleInfoRuntimeModule.ts
+++ packages/enhanced/src/runtime/ModuleInfoRuntimeModule.ts
@@ -1,6 +1,6 @@
-"use strict";
+'use strict';
 
-import { RuntimeGlobals, RuntimeModule, Template } from "webpack";
+import { RuntimeGlobals, RuntimeModule, Template } from 'webpack';
 
 /**
  * Class representing a runtime module for federation module info.
@@ -11,7 +11,7 @@ export class ModuleInfoRuntimeModule extends RuntimeModule {
    * Create a ModuleInfoRuntimeModule.
    */
   constructor() {
-    super("federation module info runtime", RuntimeModule.STAGE_BASIC);
+    super('federation module info runtime', RuntimeModule.STAGE_BASIC);
   }
 
   /**
@@ -23,56 +23,56 @@ export class ModuleInfoRuntimeModule extends RuntimeModule {
       `${RuntimeGlobals.require}.federation = {`,
       Template.indent([`cache: {},`, `remotes: {},`, `moduleInfo: {}, `]),
       `};`,
-      "var createProxyGetter = function() {",
+      'var createProxyGetter = function() {',
       Template.indent([
-        "return function(target, prop, receiver) {",
+        'return function(target, prop, receiver) {',
         Template.indent([
-          "var result;",
+          'var result;',
           "if (prop === '_config') {",
-          Template.indent("result = target.remotes;"),
+          Template.indent('result = target.remotes;'),
           "} else if (prop === 'moduleInfo') {",
-          Template.indent("result = target[prop];"),
-          "} else {",
-          Template.indent("result = target.cache[prop];"),
-          "}",
-          "return result;",
+          Template.indent('result = target[prop];'),
+          '} else {',
+          Template.indent('result = target.cache[prop];'),
+          '}',
+          'return result;',
         ]),
-        "};"
+        '};',
       ]),
-      "};",
-      "var createProxySetter = function() {",
+      '};',
+      'var createProxySetter = function() {',
       Template.indent([
-        "return function(target, prop, value) {",
+        'return function(target, prop, value) {',
         Template.indent([
           "if (prop === '_config') {",
-          Template.indent(["target.remotes = value;"]),
-          "} else {",
-          Template.indent(["target.cache[prop] = value;"]),
-          "}",
-          "return true;",
+          Template.indent(['target.remotes = value;']),
+          '} else {',
+          Template.indent(['target.cache[prop] = value;']),
+          '}',
+          'return true;',
         ]),
-        "};"
+        '};',
       ]),
-      "};",
-      "let oldScope = globalThis.__remote_scope__ || {};",
+      '};',
+      'let oldScope = globalThis.__remote_scope__ || {};',
       `if(${RuntimeGlobals.runtimeId} === 'webpack-runtime' && globalThis.__remote_scope__) {`,
-        "globalThis.__remote_scope__ = {}",
+      'globalThis.__remote_scope__ = {}',
       `}`,
-      "if (!globalThis.__remote_scope__ || !globalThis.__remote_scope__.moduleInfo) {",
-        `globalThis.__remote_scope__ = new Proxy(${RuntimeGlobals.require}.federation, {`,
-          "get: createProxyGetter(),",
-          "set: createProxySetter()",
-        `});`,
-        "if(!oldScope.moduleInfo) {",
-          "for (let key in oldScope._config) {",
-            "globalThis.__remote_scope__._config[key] = oldScope[key];",
-          "}",
-          "for (let key in oldScope) {",
-            "if(key === '_config' || !oldScope[key]) continue;",
-            "globalThis.__remote_scope__[key] = oldScope[key];",
-          "}",
-        "}",
-      "}",
+      'if (!globalThis.__remote_scope__ || !globalThis.__remote_scope__.moduleInfo) {',
+      `globalThis.__remote_scope__ = new Proxy(${RuntimeGlobals.require}.federation, {`,
+      'get: createProxyGetter(),',
+      'set: createProxySetter()',
+      `});`,
+      'if(!oldScope.moduleInfo) {',
+      'for (let key in oldScope._config) {',
+      'globalThis.__remote_scope__._config[key] = oldScope[key];',
+      '}',
+      'for (let key in oldScope) {',
+      "if(key === '_config' || !oldScope[key]) continue;",
+      'globalThis.__remote_scope__[key] = oldScope[key];',
+      '}',
+      '}',
+      '}',
     ]);
   }
 }
