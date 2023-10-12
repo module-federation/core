619b4985bd95269d890ff3dfd276ac21ec2186ae
Bruno Silva <bruno3dcontato@gmail.com>
2023-10-12 20:08:49 -0300

    docs: add link to use options

--- nxrelease.config.js
+++ nxrelease.config.js
@@ -1,3 +1,15 @@
+/*
+This project uses the nx-semantic-release plugin to automate the release process with semantic-release and nx.
+The original plugin has some issues with ESM packages, so this we opted to use a forked version of the plugin.
+- https://github.com/TheUnderScorer/nx-semantic-release   (original version)
+- https://github.com/goestav/nx-semantic-release          (forked version with ESM support)
+*/
+
+// Here you can find the available options for the nx-semantic-release plugin.
+// options                https://github.com/goestav/nx-semantic-release#available-options
+// options.branches       https://github.com/semantic-release/semantic-release/blob/master/docs/usage/configuration.md#branches
+// options.parserOpts     https://github.com/conventional-changelog/conventional-changelog/tree/master/packages/conventional-commits-parser#parseroptions
+// options.releaseRules   https://github.com/semantic-release/commit-analyzer#releaserules
 module.exports = {
   changelog: true,
   git: true,
