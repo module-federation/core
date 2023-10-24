951a41b63881e111c1c1e0467e5a937a4c5080b9
ScriptedAlchemy <zackary.l.jackson@gmail.com>
2023-10-23 21:49:25 -0700

    feat(enhanced): Forking webpack semver tools
    webpack semver parser is light but has a few bugs we will fix here

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var jest_1 = require("@nx/jest");
+exports.default = {
+    projects: (0, jest_1.getJestProjects)(),
+};
