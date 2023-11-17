a6349a0dd326886945d47f1464497c2d5a301dce
ScriptedAlchemy <zackary.l.jackson@gmail.com>
2023-11-17 13:34:33 -0800

    feat(enhanced): ✨ Reference Hoisting
    Refactoring Hoist container references plugin

--- packages/enhanced/src/lib/container/HoistContainerReferencesPlugin.ts
+++ packages/enhanced/src/lib/container/HoistContainerReferencesPlugin.ts
@@ -3,6 +3,7 @@ import Module from 'webpack/lib/Module';
 import Chunk from 'webpack/lib/Chunk';
 import Compilation from 'webpack/lib/Compilation';
 import ChunkGroup from 'webpack/lib/ChunkGroup';
+import Dependency from 'webpack/lib/Dependency';
 
 /**
  * @typedef {import("webpack").Compiler} Compiler
@@ -15,52 +16,64 @@ import ChunkGroup from 'webpack/lib/ChunkGroup';
  * This class is used to hoist container references in the code.
  * @constructor
  */
-class HoistContainerReferences {
+export class HoistContainerReferences {
   /**
    * @function apply
    * @param {Compiler} compiler The webpack compiler object
    */
   apply(compiler: Compiler): void {
+    // Hook into the compilation process
     compiler.hooks.thisCompilation.tap(
       'HoistContainerReferences',
       (compilation: Compilation) => {
+        // After chunks are optimized, perform the hoisting
         compilation.hooks.afterOptimizeChunks.tap(
           'HoistContainerReferences',
           (chunks: Iterable<Chunk>, chunkGroups: ChunkGroup[]) => {
+            // Create a map to store chunks by their id or name
             /** @type {Map<(string|number), Chunk>} */
             const chunkSet = new Map<string | number, Chunk>();
+            // Create a set to store external module requests
             /** @type {Set<Module>} */
             const externalRequests = new Set<Module>();
+            // Populate the chunkSet with chunks
             for (const chunk of chunks) {
               const ident = chunk.id || chunk.name;
               if (ident) {
                 chunkSet.set(ident, chunk);
               }
             }
+            // Iterate over chunks again to handle remote modules
             for (const chunk of chunks) {
+              // Get iterable of remote modules for the chunk
               const remoteModules =
                 compilation.chunkGraph.getChunkModulesIterableBySourceType(
                   chunk,
                   'remote',
                 );
               if (!remoteModules) continue;
+              // Iterate over remote modules
               for (const remoteModule of remoteModules) {
-                remoteModule.dependencies.forEach((dep: any) => {
+                // Iterate over dependencies of the remote module
+                for (const dep of remoteModule.dependencies) {
+                  // Get the module associated with the dependency
                   const mod = compilation.moduleGraph.getModule(dep);
+                  // If the module exists and the chunk has a runtime, add the module to externalRequests
                   if (mod !== null && chunk.runtime) {
                     externalRequests.add(mod);
-                    const runtimeChunk = (chunk.runtime typeof === 'string' || chunk.runtime typeof === 'number') ? Array.from(chunk.runtime) : [chunk.runtime];
-                    runtimeChunk.forEach((runtimeChunkId) => {
+                    // Get the runtime chunk(s) associated with the chunk
+                    const runtimeChunk = typeof chunk.runtime === 'string' || typeof chunk.runtime === 'number' ? [chunk.runtime] : [...chunk.runtime];
+                    // Iterate over runtime chunks
+                    for (const runtimeChunkId of runtimeChunk) {
+                      // Get the runtime chunk from the chunkSet
                       const runtimeChunk = chunkSet.get(runtimeChunkId);
+                      // If the runtime chunk exists, connect it with the module in the chunk graph
                       if (runtimeChunk) {
-                        compilation.chunkGraph.connectChunkAndModule(
-                          runtimeChunk,
-                          mod,
-                        );
+                        compilation.chunkGraph.connectChunkAndModule(runtimeChunk, mod);
                       }
-                    });
+                    }
                   }
-                });
+                }
               }
             }
           },
@@ -69,5 +82,4 @@ class HoistContainerReferences {
     );
   }
 }
-
 export default HoistContainerReferences;
