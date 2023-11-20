583fd5ea13b0f4f7ae92e41451fcf18e4c01e6f1
ScriptedAlchemy <zackary.l.jackson@gmail.com>
2023-11-19 18:17:02 -0800

    chore: e2e across all apps

--- apps/3002-checkout/cypress/e2e/app.cy.ts
+++ apps/3002-checkout/cypress/e2e/app.cy.ts
@@ -1,13 +1,118 @@
-import { getGreeting } from '../support/app.po';
+import { getH1, getH3 } from '../support/app.po';
 
-describe('3002-checkout', () => {
+describe('3000-home/', () => {
   beforeEach(() => cy.visit('/'));
 
-  it('should display welcome message', () => {
-    // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
+  describe('Welcome message', () => {
+    it('should display welcome message', () => {
+      getH1().contains('This is SPA combined');
+    });
+  });
 
-    // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains(/Welcome/);
+  describe('Image checks', () => {
+    it('should check that the home-webpack-png and shop-webpack-png images are not 404', () => {
+      // Get the src attribute of the home-webpack-png image
+      cy.debug()
+        .get('img.home-webpack-png')
+        .invoke('attr', 'src')
+        .then((src) => {
+          cy.log(src);
+          cy.request(src).its('status').should('eq', 200);
+        });
+
+      // Get the src attribute of the shop-webpack-png image
+      cy.get('img.shop-webpack-png')
+        .invoke('attr', 'src')
+        .then((src) => {
+          // Send a GET request to the src URL
+          cy.request(src).its('status').should('eq', 200);
+        });
+    });
+  });
+
+  describe('Routing checks', () => {
+    it('check that clicking back and forwards in client side routeing still renders the content correctly', () => {
+      cy.visit('/checkout');
+      cy.url().should('include', '/checkout');
+      getH1().contains('checkout page');
+      cy.get('.home-menu-link').click();
+      cy.wait(2999);
+      cy.url().should('include', '/');
+      getH1().contains('This is SPA combined');
+    });
+  });
+
+  describe('3000-home/checkout', () => {
+    beforeEach(() => cy.visit('/checkout'));
+
+    describe('Welcome message', () => {
+      it('should display welcome message', () => {
+        getH1().contains('checkout page');
+      });
+    });
+
+    describe('Tag checks', () => {
+      it('should check that a .description + pre tag exists', () => {
+        cy.get('.description').should('exist');
+        cy.get('main pre').should('exist');
+      });
+    });
+
+    describe('3000-home/checkout/test-title', () => {
+      beforeEach(() => cy.visit('/checkout/test-title'));
+
+      it('should display welcome message', () => {
+        getH3().contains('This title came');
+      });
+    });
+
+    describe('3000-home/checkout/test-check-button', () => {
+      beforeEach(() => cy.visit('/checkout/test-check-button'));
+
+      it('should display welcome message', () => {
+        cy.get('button').contains('Button');
+      });
+    });
+  });
+
+  describe('3000-home/shop', () => {
+    beforeEach(() => cy.visit('/shop'));
+
+    describe('Welcome message', () => {
+      it('should display welcome message', () => {
+        getH1().contains('Shop Page');
+      });
+    });
+
+    describe('Image checks', () => {
+      it('should check that shop-webpack-png images are not 404', () => {
+        // Get the src attribute of the shop-webpack-png image
+        cy.get('img.shop-webpack-png')
+          .invoke('attr', 'src')
+          .then((src) => {
+            // Send a GET request to the src URL
+            cy.request(src).its('status').should('eq', 200);
+          });
+      });
+      xit('should check that shop-webpack-png images are not 404 between route clicks', () => {
+        cy.visit('/shop');
+        cy.url().should('include', '/shop');
+        getH1().contains('Shop Page');
+        cy.get('.home-menu-link').click();
+        cy.wait(2999);
+        cy.get('img.shop-webpack-png')
+          .invoke('attr', 'src')
+          .then((src) => {
+            // Send a GET request to the src URL
+            cy.request(src).its('status').should('eq', 200);
+          });
+      });
+    });
+
+    describe('Tag checks', () => {
+      it('should check that a .description + pre tag exists', () => {
+        cy.get('.description + pre').should('exist');
+      });
+    });
   });
 });
