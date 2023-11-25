ed499986342fc3e9ce3d851078f50cb1c2147a80
ScriptedAlchemy <zackary.l.jackson@gmail.com>
2023-11-24 17:19:24 -0800

    refactor(sdk): consolidate import to root webpack package when possible

--- packages/sdk/__tests__/decodeName.spec.js
+++ packages/sdk/__tests__/decodeName.spec.js
@@ -1,12 +1,12 @@
-'use strict';
-Object.defineProperty(exports, '__esModule', { value: true });
-var constant_1 = require('./resources/constant');
-var src_1 = require('../src');
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var constant_1 = require("./resources/constant");
+var src_1 = require("../src");
 describe('decodeName', function () {
-  it('should correct decode transformed name', function () {
-    Object.keys(constant_1.nameMap).forEach(function (name) {
-      var transformedName = constant_1.nameMap[name];
-      expect((0, src_1.decodeName)(transformedName)).toBe(name);
+    it('should correct decode transformed name', function () {
+        Object.keys(constant_1.nameMap).forEach(function (name) {
+            var transformedName = constant_1.nameMap[name];
+            expect((0, src_1.decodeName)(transformedName)).toBe(name);
+        });
     });
-  });
 });
