adaff1cb27b02fac4c373ee08c5afbc740a3bab0
ScriptedAlchemy <zackary.l.jackson@gmail.com>
2023-11-25 17:08:48 -0800

    chore: partial backmerge canary to runtime branch

--- packages/nextjs-mf/src/plugins/CopyFederationPlugin.ts
+++ packages/nextjs-mf/src/plugins/CopyFederationPlugin.ts
@@ -1,6 +1,6 @@
 import { promises as fs } from 'fs';
 import path from 'path';
-import { Compilation, Compiler } from 'webpack';
+import type { Compilation, Compiler } from 'webpack';
 
 /**
  * Plugin to copy build output files.
