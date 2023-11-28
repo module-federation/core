71535752847579e257162e530c7cb944c904b52d
ScriptedAlchemy <zackary.l.jackson@gmail.com>
2023-11-27 22:39:15 -0800

    style:reduce ts-ignore use

--- apps/runtime-demo/host/src/runtimePlugin.ts
+++ apps/runtime-demo/host/src/runtimePlugin.ts
@@ -12,44 +12,34 @@ export default function (): FederationRuntimePlugin {
     },
     //@ts-ignore
     async createScript(args) {
-      //@ts-ignore
       // anything can be script loader
       console.log('createScript', args);
-      await fetch(args.url).then((res) => {
+      return fetch(args.url).then((res) => {
         res.text().then((text) => {
           eval(text);
         });
       });
     },
-    //@ts-ignore
-    loadRemoteMatch(things: any) {
-      console.log('loadRemoteMatch', things);
+    loadRemoteMatch(args) {
+      console.log('loadRemoteMatch', args);
       // randomly switch between different modules
       if (Math.random() > 0.5) {
-        things.expose = './Button1';
-        return things;
+        args.expose = './Button1';
+        return args;
       }
 
-      return things;
+      return args;
     },
     loadRemote(args) {
       console.log('loadRemote: ', args);
       return args;
     },
-    //@ts-ignore
     async loadShare(args) {
       console.log('loadShare:', args);
-      return args;
     },
-    //@ts-ignore
     async beforeLoadShare(args) {
       console.log('beforeloadShare:', args);
       return args;
-    },
-    //@ts-ignore
-    async generatePreloadAssets(args) {
-      console.log('generatePreloadAssets: ', args);
-      return Promise.resolve(args);
     },
   };
 }
