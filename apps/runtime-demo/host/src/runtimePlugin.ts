5c6e21dce66b36398172767efdbabfba7ad886e4
ScriptedAlchemy <zackary.l.jackson@gmail.com>
2023-11-27 16:30:58 -0800

    chore: add to runtime demo

--- apps/runtime-demo/host/src/runtimePlugin.ts
+++ apps/runtime-demo/host/src/runtimePlugin.ts
@@ -1,5 +1,4 @@
 import { FederationRuntimePlugin } from '@module-federation/runtime/type';
-
 export default function (): FederationRuntimePlugin {
   return {
     name: 'custom-plugin',
@@ -7,10 +6,50 @@ export default function (): FederationRuntimePlugin {
       console.log('beforeInit: ', args);
       return args;
     },
-    beforeLoadShare(args) {
-      console.log('beforeLoadShare: ', args);
-
+    beforeLoadRemote(args) {
+      console.log('beforeLoadRemote: ', args);
       return args;
+    },
+    //@ts-ignore
+    async createScript(args) {
+      //@ts-ignore
+      // anything can be script loader
+      console.log('createScript', args);
+      await fetch(args.url).then((res) => {
+        res.text().then((text) => {
+          eval(text);
+        });
+      });
+    },
+    //@ts-ignore
+    loadRemoteMatch(things: any) {
+      console.log('loadRemoteMatch', things);
+      // randomly switch between different modules
+      if (Math.random() > 0.5) {
+        things.expose = './Button1';
+        return things;
+      }
+
+      return things;
+    },
+    loadRemote(args) {
+      console.log('loadRemote: ', args);
+      return args;
+    },
+    //@ts-ignore
+    async loadShare(args) {
+      console.log('loadShare:', args);
+      return args;
+    },
+    //@ts-ignore
+    async beforeLoadShare(args) {
+      console.log('beforeloadShare:', args);
+      return args;
+    },
+    //@ts-ignore
+    async generatePreloadAssets(args) {
+      console.log('generatePreloadAssets: ', args);
+      return Promise.resolve(args);
     },
   };
 }
