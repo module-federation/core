f4ed6f0d40cb2de1cbea4189f2deb1ff2d8cc17d
ScriptedAlchemy <zackary.l.jackson@gmail.com>
2023-12-03 19:09:40 -0800

    feat(sdk): fix version missmatch

--- packages/sdk/src/logger.ts
+++ packages/sdk/src/logger.ts
@@ -1,5 +1,8 @@
 import { BROWSER_LOG_KEY, BROWSER_LOG_VALUE } from './constant';
 import { isBrowserEnv, isDebugMode } from './env';
+
+const DEBUG_LOG = '[ FEDERATION DEBUG ]';
+
 function safeToString(info: any): string {
   try {
     return JSON.stringify(info, null, 2);
@@ -8,7 +11,6 @@ function safeToString(info: any): string {
   }
 }
 
-const DEBUG_LOG = '[ FEDERATION DEBUG ]';
 class Logger {
   enable = false;
   identifier: string;
