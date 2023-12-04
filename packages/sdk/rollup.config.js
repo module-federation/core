729d10c5feba09cdf83e3a12c6aa480b2d190325
ScriptedAlchemy <zackary.l.jackson@gmail.com>
2023-12-03 20:09:17 -0800

    style: lint

--- packages/sdk/rollup.config.js
+++ packages/sdk/rollup.config.js
@@ -1,13 +1,10 @@
 const copy = require('rollup-plugin-copy');
 
-
 module.exports = (rollupConfig, _projectOptions) => {
   rollupConfig.plugins.push(
     copy({
-      targets: [
-        { src: 'packages/sdk/LICENSE', dest: 'dist/packages/sdk' }
-      ]
-    })
+      targets: [{ src: 'packages/sdk/LICENSE', dest: 'dist/packages/sdk' }],
+    }),
   );
 
   return rollupConfig;
