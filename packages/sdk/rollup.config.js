964e333c6d9ade74f1fc4344e40dc2931468d5b4
2heal1 <TwoHeal@163.com>
2023-12-04 11:43:43 +0800

    feat(sdk): add sdk license

---
+++ packages/sdk/rollup.config.js
@@ -0,0 +1,14 @@
+const copy = require('rollup-plugin-copy');
+
+
+module.exports = (rollupConfig, _projectOptions) => {
+  rollupConfig.plugins.push(
+    copy({
+      targets: [
+        { src: 'packages/sdk/LICENSE', dest: 'dist/packages/sdk' }
+      ]
+    })
+  );
+
+  return rollupConfig;
+};
