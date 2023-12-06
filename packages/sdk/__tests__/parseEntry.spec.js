8c32914d4a7cf95533564ecad6b73671f9da16ee
2heal1 <TwoHeal@163.com>
2023-12-06 17:23:09 +0800

    feat(sdk): add test cases

---
+++ packages/sdk/__tests__/parseEntry.spec.js
@@ -0,0 +1,29 @@
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
+var src_1 = require('../src');
+describe('parseEntry', function () {
+  it('get correct entryInfo by parsing normal entry', function () {
+    var entry = '@byted/app1:1.2.0';
+    var entryInfo = (0, src_1.parseEntry)(entry);
+    expect(entryInfo).toMatchObject({
+      version: '1.2.0',
+      name: '@byted/app1',
+    });
+  });
+  it('get correct entryInfo by parsing none version entry', function () {
+    var entry = '@byted/app1';
+    var entryInfo = (0, src_1.parseEntry)(entry);
+    expect(entryInfo).toMatchObject({
+      version: '*',
+      name: '@byted/app1',
+    });
+  });
+  it('get correct entryInfo by parsing local version entry', function () {
+    var entry = '@byted/app1:http://localhost:8080/vmok-manifest.json';
+    var entryInfo = (0, src_1.parseEntry)(entry);
+    expect(entryInfo).toMatchObject({
+      name: '@byted/app1',
+      entry: 'http://localhost:8080/vmok-manifest.json',
+    });
+  });
+});
