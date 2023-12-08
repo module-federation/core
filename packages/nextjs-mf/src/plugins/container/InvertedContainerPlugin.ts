2edcf06c84d5515d3e2fe5d0999f0a51a68aaa1b
ScriptedAlchemy <zackary.l.jackson@gmail.com>
2023-12-07 17:56:39 -0800

    fix(nextjs-mf): V1.5 async boundary adjustments

--- packages/nextjs-mf/src/plugins/container/InvertedContainerPlugin.ts
+++ packages/nextjs-mf/src/plugins/container/InvertedContainerPlugin.ts
@@ -2,18 +2,7 @@ import type { Compiler } from 'webpack';
 import { ModuleFederationPluginOptions } from './types';
 import EmbeddedContainerPlugin from './EmbeddedContainerPlugin';
 import { AsyncBoundaryPlugin } from '@module-federation/enhanced';
-import HoistPseudoEager from './HoistPseudoEagerModules';
 
-/**
- * This interface extends the ModuleFederationPluginOptions interface and includes additional fields
- * specific to the InvertedContainerPlugin's behavior.
- * @interface InvertedContainerOptions
- * @property {string} [container] - The name of the container.
- * @property {Record<string, string>} remotes - A map of remote modules to their URLs.
- * @property {string} runtime - The name of the current module.
- * @property {boolean} [debug] - A flag to enable debug logging.
- * @property {string} chunkToEmbed - The name of the chunk to embed.
- */
 interface InvertedContainerOptions extends ModuleFederationPluginOptions {
   container?: string;
   remotes: Record<string, string>;
@@ -22,25 +11,13 @@ interface InvertedContainerOptions extends ModuleFederationPluginOptions {
   chunkToEmbed: string;
 }
 
-/**
- * The InvertedContainerPlugin is a Webpack plugin that manages the loading of chunks in a federated module.
- * @class
- */
 class InvertedContainerPlugin {
   private options: InvertedContainerOptions;
 
-  /**
-   * Constructs an instance of the InvertedContainerPlugin.
-   * @param {InvertedContainerOptions} options - The configuration options for the plugin.
-   */
   constructor(options: InvertedContainerOptions) {
     this.options = options;
   }
 
-  /**
-   * This method applies the plugin logic and hooks to the Webpack compiler instance.
-   * @param {Compiler} compiler - The Webpack compiler instance.
-   */
   public apply(compiler: Compiler): void {
     new EmbeddedContainerPlugin({
       runtime: this.options.runtime,
@@ -48,30 +25,12 @@ class InvertedContainerPlugin {
       chunkToEmbed: this.options.chunkToEmbed,
     }).apply(compiler);
 
-    //@ts-ignore
-    const asyncBoundaryPlugin = new AsyncBoundaryPlugin().apply(compiler);
-    new HoistPseudoEager().apply(compiler);
-    // The following code is commented out for future reference.
-    // asyncBoundaryPlugin.hooks.checkInvalidContext.tap(
-    //   'InvertedContainerPlugin',
-    //   (renderContext, compilation) => {
-    //     return (
-    //       !renderContext ||
-    //       //@ts-ignore
-    //       renderContext?._name ||
-    //       //@ts-ignore
-    //       !renderContext?.debugId ||
-    //       //@ts-ignore
-    //       !compilation.chunkGraph.isEntryModule(renderContext) ||
-    //       //@ts-ignore
-    //       renderContext?.rawRequest?.includes('pages/api') ||
-    //       //@ts-ignore
-    //       renderContext?.layer === 'api'
-    //     );
-    //   },
-    // );
-    // asyncBoundaryPlugin.apply(compiler);
+    new AsyncBoundaryPlugin({
+      eager: (module) => /.federation/.test(module.identifier())
+      //@ts-ignore
+    }).apply(compiler);
   }
 }
 
 export default InvertedContainerPlugin;
+
