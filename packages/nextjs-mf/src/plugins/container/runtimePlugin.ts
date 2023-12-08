9f2f655c8420e0bc76dddab6dbefa6d0a4ab926d
ScriptedAlchemy <zackary.l.jackson@gmail.com>
2023-12-07 18:19:06 -0800

    feat(nextjs-mf): Move to Federation 1.5 standard plugin

---
+++ packages/nextjs-mf/src/plugins/container/runtimePlugin.ts
@@ -0,0 +1,47 @@
+//@ts-nocheck
+import { FederationRuntimePlugin } from '@module-federation/runtime/type';
+export default function (): FederationRuntimePlugin {
+  return {
+    name: 'custom-plugin',
+    beforeInit(args) {
+      // console.log('beforeInit: ', args);
+      return args;
+    },
+    init(args) {
+      // console.log('init: ', args);
+      return args;
+    },
+    beforeLoadRemote(args) {
+      // console.log('beforeLoadRemote: ', args);
+      return args;
+    },
+    createScript(args) {
+      // anything can be script loader
+      // console.log('createScript', args);
+      // console.log(args);
+      // return fetch(args.url).then((res) => {
+      //   res.text().then((text) => {
+      //     eval(text);
+      //   });
+      // });
+      return args;
+    },
+    loadRemoteMatch(args) {
+      // console.log('loadRemoteMatch', args);
+      // randomly switch between different modules
+
+      return args;
+    },
+    loadRemote(args) {
+      // console.log('loadRemote: ', args);
+      return args;
+    },
+    async loadShare(args) {
+      // console.log('loadShare:', args);
+    },
+    async beforeLoadShare(args) {
+      // console.log('beforeloadShare:', args);
+      return args;
+    },
+  };
+}
