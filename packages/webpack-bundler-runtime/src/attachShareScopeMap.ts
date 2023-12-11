201b417cd71e5919de5a4216d5b002e98e805557
ScriptedAlchemy <zackary.l.jackson@gmail.com>
2023-12-11 14:22:48 -0800

    style: format codebase

--- packages/webpack-bundler-runtime/src/attachShareScopeMap.ts
+++ packages/webpack-bundler-runtime/src/attachShareScopeMap.ts
@@ -4,13 +4,13 @@ export function attachShareScopeMap(webpackRequire: WebpackRequire) {
   if (
     !webpackRequire.S ||
     webpackRequire.federation.hasAttachShareScopeMap ||
-    !webpackRequire.federation.instance
-    || !webpackRequire.federation.instance.shareScopeMap
+    !webpackRequire.federation.instance ||
+    !webpackRequire.federation.instance.shareScopeMap
   ) {
     return;
   }
 
-  webpackRequire.S = webpackRequire.federation.instance.shareScopeMap
+  webpackRequire.S = webpackRequire.federation.instance.shareScopeMap;
 
   webpackRequire.federation.hasAttachShareScopeMap = true;
 }
