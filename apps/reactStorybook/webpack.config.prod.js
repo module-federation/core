f4fd149c5141eaff800d63fa1c13066f6e60c8a9
zhouxiao.shaw <zhouxiao.shaw@bytdance.com>
2023-12-18 22:33:55 +0800

    chore: optimize example

--- apps/reactStorybook/webpack.config.prod.js
+++ apps/reactStorybook/webpack.config.prod.js
@@ -23,9 +23,9 @@ const prodConfig = {
    *   ['app2', 'http://example.com/path/to/app2/remoteEntry.js'],
    * ]
    */
-  remotes: {
-    reactRemoteUI: 'reactRemoteUI@http://localhost:4201/remoteEntry.js',
-  },
+  // remotes: {
+  //   reactRemoteUI: 'reactRemoteUI@http://localhost:4201/remoteEntry.js',
+  // },
 };
 
 // Nx plugins for webpack to build config object from Nx options and context.
