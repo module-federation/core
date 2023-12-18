7ef5ab9d7c23c293033cef13f65b0463ad51450a
zhouxiao.shaw <zhouxiao.shaw@bytdance.com>
2023-12-18 10:25:19 +0800

    chore: modify nextjs-mf package.json

--- apps/3000-home/remote-delegate.js
+++ apps/3000-home/remote-delegate.js
@@ -1,4 +1,4 @@
-import { importDelegatedModule } from '@module-federation/utilities/src/utils/importDelegatedModule';
+import { importDelegatedModule } from '@module-federation/utilities';
 /* eslint-disable no-undef */
 // eslint-disable-next-line no-async-promise-executor
 module.exports = new Promise((resolve, reject) => {
