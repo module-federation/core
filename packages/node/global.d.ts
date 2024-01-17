3c8a9dac3ad501ec06f7aca78a707b8d3f000d5c
ScriptedAlchemy <zackaryjackson@bytedance.com>
2024-01-16 22:16:10 -0800

    chore: fix types

--- packages/node/global.d.ts
+++ packages/node/global.d.ts
@@ -1,3 +1,17 @@
+/* eslint-disable @typescript-eslint/triple-slash-reference */
+
+/// <reference path="../../node_modules/webpack/module.d.ts" />
+/// <reference path="../../node_modules/webpack/types.d.ts" />
+
+declare module 'webpack/lib/RuntimeGlobals';
+declare module 'webpack/lib/Compilation';
+declare module 'webpack/lib/Compiler';
+declare module 'webpack/lib/runtime/StartupChunkDependenciesPlugin';
+declare module 'webpack/lib/RuntimeModule';
+declare module 'webpack/lib/Template';
+declare module 'webpack/lib/util/compileBooleanMatcher';
+declare module 'webpack/lib/util/identifier';
+
 declare global {
   namespace NodeJS {
     interface Global {
