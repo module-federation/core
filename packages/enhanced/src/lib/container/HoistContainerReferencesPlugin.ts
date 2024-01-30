dc83c2bf120034a45b3216a17324bb5a2b41ebc9
ScriptedAlchemy <zackary.l.jackson@gmail.com>
2024-01-29 18:41:12 -0800

    fix(enhanced): refactor FederationInitModule.ts module wrapping logic

--- packages/enhanced/src/lib/container/HoistContainerReferencesPlugin.ts
+++ packages/enhanced/src/lib/container/HoistContainerReferencesPlugin.ts
@@ -7,6 +7,16 @@ import type {
 } from 'webpack';
 import ContainerEntryModule from './ContainerEntryModule';
 
+const getRuntime = (chunk: Chunk) => {
+  if (typeof chunk.runtime === 'string') {
+    return [chunk.runtime];
+  } else if (chunk.runtime === undefined) {
+    return [];
+  } else {
+    return Array.from(chunk.runtime);
+  }
+};
+
 /**
  * This class is used to hoist container references in the code.
  * @constructor
@@ -35,27 +45,30 @@ export class HoistContainerReferences implements WebpackPluginInstance {
                 realContainers.set(chunk.name, chunk);
               }
             }
+
             for (const chunk of chunks) {
               if (realContainers.has(chunk.name)) continue;
               this.clearEntry(chunk, compilation, realContainers);
             }
           },
         );
-        compilation.hooks.afterOptimizeChunks.tap(
-          'HoistContainerReferences',
-          (chunks: Iterable<Chunk>) => {
-            const realContainers: Map<Chunk['name'], Chunk> = new Map();
-            for (const chunk of chunks) {
-              if (this.chunkContainsContainerEntryModule(chunk, compilation)) {
-                realContainers.set(chunk.name, chunk);
-              }
-            }
-            for (const chunk of chunks) {
-              if (realContainers.has(chunk.name)) continue;
-              this.hoistModulesInChunk(chunk, compilation, realContainers);
-            }
-          },
-        );
+        // compilation.hooks.afterOptimizeChunks.tap(
+        //   'HoistContainerReferences',
+        //   (chunks: Iterable<Chunk>) => {
+        //     const realContainers: Map<Chunk['name'], Chunk> = new Map();
+        //     for (const chunk of chunks) {
+        //       if (this.chunkContainsContainerEntryModule(chunk, compilation)) {
+        //         realContainers.set(chunk.name, chunk);
+        //       }
+        //     }
+        //     for (const chunk of chunks) {
+        //       if (realContainers.has(chunk.name)) continue;
+        //       this.hoistModulesInChunk(chunk, compilation, realContainers);
+        //     }
+        //
+        //     debugger;
+        //   },
+        // );
       },
     );
   }
@@ -82,11 +95,15 @@ export class HoistContainerReferences implements WebpackPluginInstance {
   ): void {
     const chunkGraph = compilation.chunkGraph;
     const { runtimeChunks } = this.sortNameChunks(chunk, compilation);
-
     const removedModules = new Set();
-    for (const [name, entry] of compilation.entries) {
-      if (realContainers.has(name)) continue;
-      for (const entryDependency of entry.dependencies) {
+    if (
+      chunk.name &&
+      compilation.entries.has(chunk?.name) &&
+      !realContainers.has(chunk.name)
+    ) {
+      const entrypoint = compilation.entries.get(chunk.name);
+      if (!entrypoint) return;
+      for (const entryDependency of entrypoint.dependencies) {
         if (!entryDependency) continue;
         const resolvedModule =
           compilation.moduleGraph.getModule(entryDependency);
@@ -97,7 +114,43 @@ export class HoistContainerReferences implements WebpackPluginInstance {
         if (!context.endsWith('.federation')) continue;
         if (removedModules.has(chunk)) continue;
         chunkGraph.disconnectChunkAndEntryModule(chunk, resolvedModule);
-        entry.dependencies.shift();
+        const getRecursiveConnections = () => {
+          const recursiveConnections: Set<Module> = new Set();
+          const processModule = (inputModule: Module) => {
+            const connections =
+              compilation.moduleGraph.getOutgoingConnectionsByModule(
+                inputModule,
+              );
+            if (connections) {
+              for (const [module, connection] of connections) {
+                if (!module) continue;
+                if (recursiveConnections.has(module)) continue;
+                recursiveConnections.add(module);
+                processModule(module);
+              }
+            }
+          };
+          processModule(resolvedModule);
+          return recursiveConnections;
+        };
+        const allAssociatedModules = getRecursiveConnections();
+
+        for (const mod of allAssociatedModules) {
+          const roots = Array.from(chunkGraph.getChunkRootModules(chunk));
+          if (!chunkGraph.isModuleInChunk(mod, chunk)) continue;
+          // chunkGraph.disconnectChunkAndModule(chunk, mod);
+        }
+
+        allAssociatedModules.add(resolvedModule);
+
+        const runtimes = getRuntime(chunk);
+        for (const runtime of runtimes) {
+          const runtimeChunk = runtimeChunks.get(runtime);
+          if (!runtimeChunk) continue;
+          chunkGraph.attachModules(runtimeChunk, allAssociatedModules);
+        }
+
+        entrypoint.dependencies.shift();
         removedModules.add(chunk);
       }
     }
@@ -124,12 +177,14 @@ export class HoistContainerReferences implements WebpackPluginInstance {
         }
       }
     }
+
+    debugger;
     for (const [name, entrypoint] of namedChunks) {
       if (realContainers.has(name)) continue;
       for (const [runtimeName, container] of realContainers) {
         const containerModules = chunkGraph.getChunkModulesIterable(container);
         for (const cm of containerModules) {
-          chunkGraph.disconnectChunkAndModule(entrypoint, cm);
+          // chunkGraph.disconnectChunkAndModule(entrypoint, cm);
         }
       }
     }
@@ -144,6 +199,7 @@ export class HoistContainerReferences implements WebpackPluginInstance {
   } {
     const runtimeChunks: Map<Chunk['name'], Chunk> = new Map();
     const namedChunks: Map<Chunk['name'], Chunk> = new Map();
+
     for (const c of compilation.chunks) {
       if (c.hasRuntime() && c.name) {
         runtimeChunks.set(c.name, c);
