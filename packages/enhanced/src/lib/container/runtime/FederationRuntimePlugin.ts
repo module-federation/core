a580511051bfe73fee1fb0ed0b6eb246ffca11a2
2heal1 <TwoHeal@163.com>
2024-02-28 15:30:21 +0800

    fix(enhanced): correct FederationRuntimePlugin options

--- packages/enhanced/src/lib/container/runtime/FederationRuntimePlugin.ts
+++ packages/enhanced/src/lib/container/runtime/FederationRuntimePlugin.ts
@@ -40,7 +40,7 @@ class FederationRuntimePlugin {
   bundlerRuntimePath: string;
 
   constructor(options?: moduleFederationPlugin.ModuleFederationPluginOptions) {
-    this.options = { ...options };
+    this.options = options ? { ...options } : undefined;
     this.entryFilePath = '';
     this.bundlerRuntimePath = BundlerRuntimePath;
   }
