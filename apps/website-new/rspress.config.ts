f22f4a297c22fb0c76d548cc57ec77526e265979
zhouxiao.shaw <zhouxiao.shaw@bytedance.com>
2024-02-29 14:51:39 +0800

    chore: init showcase

--- apps/website-new/rspress.config.ts
+++ apps/website-new/rspress.config.ts
@@ -1,19 +1,54 @@
 import * as path from 'path';
 import { defineConfig } from 'rspress/config';
-console.log(
-  'xxxx',
-  require('path').resolve(__dirname) + '/src/components/ShowcaseList/index.tsx',
-);
+
+const getNavbar = (lang: string) => {
+  const cn = lang === 'zh';
+  const prefix = cn ? '' : '/en';
+  const getLink = (str: string) => `${prefix}${str}`;
+  const getText = (cnText: string, enText: string) => (cn ? cnText : enText);
+  return [
+    // {
+    //   text: getText('指南', 'Guide'),
+    //   link: getLink('/guides/get-started/introduction'),
+    //   activeMatch: '/guides/',
+    // },
+    {
+      text: getText('社区', 'Community'),
+      link: getLink('/community/showcase'),
+      activeMatch: '/community/',
+    },
+  ];
+};
+
 export default defineConfig({
   root: path.join(__dirname, 'docs'),
   title: 'Module federation',
   description: 'Module federation',
   icon: '/svg.svg',
+  lang: 'en',
   logo: {
     light: '/module-federation-logo.svg',
     dark: '/module-federation-logo.svg',
   },
   themeConfig: {
+    locales: [
+      // {
+      //   lang: 'zh',
+      //   title: 'Modern.js',
+      //   description:
+      //     'A Progressive React Framework for modern web development.',
+      //   nav: getNavbar('zh'),
+      //   label: '简体中文',
+      // },
+      {
+        lang: 'en',
+        title: 'Module federation',
+        description:
+          'A Progressive React Framework for modern web development.',
+        nav: getNavbar('en'),
+        label: 'English',
+      },
+    ],
     socialLinks: [
       {
         icon: 'github',
