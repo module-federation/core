3aafce3ac200c588046a612fac7a11d19b6799f2
ScriptedAlchemy <zackaryjackson@bytedance.com>
2024-04-13 21:55:39 -0700

    fix(enhanced): patch partial container chunk split

--- packages/enhanced/src/lib/container/ContainerPlugin.ts
+++ packages/enhanced/src/lib/container/ContainerPlugin.ts
@@ -91,7 +91,7 @@ class ContainerPlugin {
             if (typeof cacheGroup.chunks === 'function') {
               const prevChunks = cacheGroup.chunks;
               cacheGroup.chunks = (chunk) => {
-                if (chunk.name && chunk.name === name) {
+                if (chunk.name && (chunk.name === name || chunk.name === name + '_partial')) {
                   return false;
                 }
                 return prevChunks(chunk);
@@ -101,7 +101,7 @@ class ContainerPlugin {
 
             if (cacheGroup.chunks === 'all') {
               cacheGroup.chunks = (chunk) => {
-                if (chunk.name && chunk.name === name) {
+                if (chunk.name && (chunk.name === name || chunk.name === name + '_partial')) {
                   return false;
                 }
                 return true;
@@ -110,7 +110,7 @@ class ContainerPlugin {
             }
             if (cacheGroup.chunks === 'initial') {
               cacheGroup.chunks = (chunk) => {
-                if (chunk.name && chunk.name === name) {
+                if (chunk.name && (chunk.name === name || chunk.name === name + '_partial')) {
                   return false;
                 }
                 return chunk.isOnlyInitial();
