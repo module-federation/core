6a98b5a0852b7c89f8e3cbcf2dc932b0a5de1400
ScriptedAlchemy <zackaryjackson@bytedance.com>
2024-04-14 22:19:13 -0700

    chore: changesets

--- apps/runtime-demo/3005-runtime-host/cypress.config.ts
+++ apps/runtime-demo/3005-runtime-host/cypress.config.ts
@@ -2,8 +2,13 @@ import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
+  projectId: 'satjfn',
   e2e: nxE2EPreset(__filename, { cypressDir: 'cypress' }),
   defaultCommandTimeout: 20000,
   chromeWebSecurity: false,
   experimentalModifyObstructiveThirdPartyCode: true,
+  retries: {
+    runMode: 2,
+    openMode: 1,
+  },
 });
