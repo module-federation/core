9e4a20fe59eb77b60bd12b3a0bbb8d89b39c9b8a
ScriptedAlchemy <zackaryjackson@bytedance.com>
2024-05-06 21:21:31 -0700

    chore: fix gitignore of mf types

--- apps/manifest-demo/3008-webpack-host/@mf-types/js-entry-provider/compiled-types/apps/manifest-demo/3012-rspack-js-entry-provider/src/App.d.ts
+++
@@ -1,2 +0,0 @@
-export default App;
-declare function App(): import("react/jsx-runtime").JSX.Element;
--- apps/manifest-demo/3008-webpack-host/@mf-types/manifest-provider/Component.d.ts
+++
@@ -1,2 +0,0 @@
-export * from './compiled-types/apps/manifest-demo/3011-rspack-manifest-provider/src/App';
-export { default } from './compiled-types/apps/manifest-demo/3011-rspack-manifest-provider/src/App';
