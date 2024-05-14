0113b81d4d04f5dd9ec9ba2b1898da2cbb917cee
Hanric <hanric.zhang@gmail.com>
2024-05-14 10:09:43 +0800

    chore: delete references to used shared to prevent memory leaks (#2347)

--- packages/rspack/src/utils.ts
+++
@@ -1,6 +0,0 @@
-import { SEPARATOR } from '@module-federation/sdk';
-import { utils } from '@module-federation/managers';
-
-export function getIdentifier(name: string): string {
-  return `${name}${SEPARATOR}${utils.getBuildVersion()}`;
-}
