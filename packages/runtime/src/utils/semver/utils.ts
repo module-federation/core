906946cceeb034a74ae9abf58ddb0c793879d946
zhouxiao.shaw <zhouxiao.shaw@bytedance.com>
2024-05-21 19:10:59 +0800

    chore: fix type error

--- packages/runtime/src/utils/semver/utils.ts
+++ packages/runtime/src/utils/semver/utils.ts
@@ -39,7 +39,6 @@ export function pipe<TArgs extends any[], R1, R2, R3, R4, R5, R6, R7>(
   f6: (a: R5) => R6,
   f7: (a: R6) => R7,
 ): (...args: TArgs) => R7;
-// Additional overloads for fewer functions
 // biome-ignore lint/suspicious/noExplicitAny: <explanation>
 export function pipe<TArgs extends any[], R1, R2, R3, R4, R5, R6>(
   f1: (...args: TArgs) => R1,
@@ -49,6 +48,36 @@ export function pipe<TArgs extends any[], R1, R2, R3, R4, R5, R6>(
   f5: (a: R4) => R5,
   f6: (a: R5) => R6,
 ): (...args: TArgs) => R6;
+// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+export function pipe<TArgs extends any[], R1, R2, R3, R4, R5>(
+  f1: (...args: TArgs) => R1,
+  f2: (a: R1) => R2,
+  f3: (a: R2) => R3,
+  f4: (a: R3) => R4,
+  f5: (a: R4) => R5,
+): (...args: TArgs) => R5;
+// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+export function pipe<TArgs extends any[], R1, R2, R3, R4>(
+  f1: (...args: TArgs) => R1,
+  f2: (a: R1) => R2,
+  f3: (a: R2) => R3,
+  f4: (a: R3) => R4,
+): (...args: TArgs) => R4;
+// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+export function pipe<TArgs extends any[], R1, R2, R3>(
+  f1: (...args: TArgs) => R1,
+  f2: (a: R1) => R2,
+  f3: (a: R2) => R3,
+): (...args: TArgs) => R3;
+// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+export function pipe<TArgs extends any[], R1, R2>(
+  f1: (...args: TArgs) => R1,
+  f2: (a: R1) => R2,
+): (...args: TArgs) => R2;
+// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+export function pipe<TArgs extends any[], R1>(
+  f1: (...args: TArgs) => R1,
+): (...args: TArgs) => R1;
 // Continue with additional overloads...
 // This function pipes the output of one function to the input of another, supporting chaining of multiple functions.
 // biome-ignore lint/suspicious/noExplicitAny: <explanation>
