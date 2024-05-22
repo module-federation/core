3eac3b21606faff225a9394a17729059620e0883
ScriptedAlchemy <zackaryjackson@bytedance.com>
2024-05-22 17:14:37 -0400

    refactor: remove initial package ports

--- packages/native-federation-runtime/index.d.ts
+++
@@ -1,3 +0,0 @@
-export * from './lib/init-federation';
-export * from './lib/load-remote-module';
-export * from './lib/model/federation-info';
--- packages/native-federation-runtime/lib/index.mjs
+++
@@ -1,3 +0,0 @@
-export * from './init-federation';
-export * from './load-remote-module';
-export * from './model/federation-info';
