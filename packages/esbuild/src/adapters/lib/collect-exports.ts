131a937e7aefc215b2bfb4802e54028af1025fa5
Zack Jackson <zackary.l.jackson@gmail.com>
2024-06-19 11:26:15 -0700

    feat: external extractor (#2620)
    Co-authored-by: ScriptedAlchemy <zackaryjackson@bytedance.com>

--- packages/esbuild/src/adapters/lib/collect-exports.ts
+++ packages/esbuild/src/adapters/lib/collect-exports.ts
@@ -10,6 +10,7 @@ import {
 import { promisify } from 'util';
 import enhancedResolve from 'enhanced-resolve';
 import fs from 'fs';
+import path from 'path';
 
 export const resolve = promisify(
   enhancedResolve.create({
@@ -17,6 +18,27 @@ export const resolve = promisify(
   }),
 );
 
+export const resolvePackageJson = async (
+  packageName: string,
+  callback: (err: Error | null, result?: string) => void,
+): Promise<void> => {
+  try {
+    const filepath = await resolve(__dirname, packageName);
+    if (typeof filepath !== 'string') {
+      return callback(new Error('Failed to resolve package path'));
+    }
+
+    // Resolve the path to the package.json file
+    const packageJsonPath = path.join(filepath, 'package.json');
+    if (fs.existsSync(packageJsonPath)) {
+      callback(null, packageJsonPath);
+    } else {
+      callback(new Error(`package.json not found for package: ${packageName}`));
+    }
+  } catch (err) {
+    callback(err as Error);
+  }
+};
 export async function getExports(modulePath: string): Promise<string[]> {
   await initEsLexer;
   await initCjsLexer;
