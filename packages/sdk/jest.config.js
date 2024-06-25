178017f289cdd9c761aa48690a59d70c4422720a
ScriptedAlchemy <zackaryjackson@bytedance.com>
2024-06-25 14:56:48 -0700

    chore: fix syntax

--- packages/sdk/jest.config.js
+++ packages/sdk/jest.config.js
@@ -9,8 +9,7 @@ const { exclude: _, ...swcJestConfig } = JSON.parse(
 
 // disable .swcrc look-up by SWC core because we're passing in swcJestConfig ourselves.
 // If we do not disable this, SWC Core will read .swcrc and won't transform our test files due to "exclude"
-swcJestConfig.swcrc ?= false;
-
+swcJestConfig.swcrc ??= false;
 // Uncomment if using global setup/teardown files being transformed via swc
 // https://nx.dev/packages/jest/documents/overview#global-setup/teardown-with-nx-libraries
 // jest needs EsModule Interop to find the default exported setup/teardown functions
