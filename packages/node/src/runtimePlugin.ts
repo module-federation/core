71b088766670a4b8011ec287a0a9ccd133a75edf
ScriptedAlchemy <zackaryjackson@bytedance.com>
2024-06-26 23:28:12 -0700

    fix(node): refactor runtime plugin

--- packages/node/src/runtimePlugin.ts
+++ packages/node/src/runtimePlugin.ts
@@ -39,328 +39,235 @@ type WebpackRequire = {
 declare const __webpack_require__: WebpackRequire;
 declare const __non_webpack_require__: (id: string) => any;
 
+function importNodeModule<T>(name: string): Promise<T> {
+  if (!name) {
+    throw new Error('import specifier is required');
+  }
+  const importModule = new Function('name', `return import(name)`);
+  return importModule(name)
+    .then((res: any) => res.default as T)
+    .catch((error: any) => {
+      console.error(`Error importing module ${name}:`, error);
+      throw error;
+    });
+}
+
 export default function (): FederationRuntimePlugin {
   return {
     name: 'node-federation-plugin',
     beforeInit(args) {
-      //patch webpack chunk loading handlers
+      // Patch webpack chunk loading handlers
       (() => {
-        function resolveFile(rootOutputDir: string, chunkId: string): string {
+        const resolveFile = (
+          rootOutputDir: string,
+          chunkId: string,
+        ): string => {
           const path = __non_webpack_require__('path');
-
-          const filename = path.join(
+          return path.join(
             __dirname,
             rootOutputDir + __webpack_require__.u(chunkId),
           );
+        };
 
-          return filename;
-        }
-
-        function resolveUrl(remoteName: string, chunkName: string): URL | null {
-          const path = __non_webpack_require__('path');
-
+        const resolveUrl = (
+          remoteName: string,
+          chunkName: string,
+        ): URL | null => {
           try {
             return new URL(chunkName, __webpack_require__.p);
-          } catch (error) {
+          } catch {
             const entryUrl =
               returnFromCache(remoteName) ||
               returnFromGlobalInstances(remoteName);
-
-            if (!entryUrl) {
-              return null;
-            }
+            if (!entryUrl) return null;
 
             const url = new URL(entryUrl);
-            const fileToReplace = path.basename(url.pathname);
-            url.pathname = url.pathname.replace(fileToReplace, chunkName);
-
+            const path = __non_webpack_require__('path');
+            url.pathname = url.pathname.replace(
+              path.basename(url.pathname),
+              chunkName,
+            );
             return url;
           }
-        }
+        };
 
-        function returnFromCache(remoteName: string): string | null {
+        const returnFromCache = (remoteName: string): string | null => {
           const globalThisVal = new Function('return globalThis')();
           const federationInstances =
             globalThisVal['__FEDERATION__']['__INSTANCES__'];
-
-          let entryUrl: string | null = null; // Initialize entryUrl to null
-
-          // Using for...of for better readability and direct control
           for (const instance of federationInstances) {
             const moduleContainer = instance.moduleCache.get(remoteName);
-            if (moduleContainer && moduleContainer.remoteInfo) {
-              entryUrl = moduleContainer.remoteInfo.entry; // Assign the found entry URL
-              break; // Exit the loop as soon as a matching entry is found
-            }
+            if (moduleContainer?.remoteInfo)
+              return moduleContainer.remoteInfo.entry;
           }
+          return null;
+        };
 
-          return entryUrl; // Return the found entry URL or null if not found
-        }
-
-        function returnFromGlobalInstances(remoteName: string): string | null {
+        const returnFromGlobalInstances = (
+          remoteName: string,
+        ): string | null => {
           const globalThisVal = new Function('return globalThis')();
           const federationInstances =
             globalThisVal['__FEDERATION__']['__INSTANCES__'];
-
-          let entryUrl: string | null = null; // Declare a variable to store the entry URL when found
-
-          // Iterate over federation instances
           for (const instance of federationInstances) {
-            // Manually iterate over the remotes for each instance
             for (const remote of instance.options.remotes) {
               if (remote.name === remoteName || remote.alias === remoteName) {
                 console.log('Backup remote entry found:', remote.entry);
-                entryUrl = remote.entry; // Set the entry URL
-                break; // Break from the inner loop
+                return remote.entry;
               }
             }
-
-            if (entryUrl) break; // Break from the outer loop if the entry URL has been set
           }
+          return null;
+        };
 
-          return entryUrl; // Return the found entry URL or null if not found
-        }
-
-        function fileSystemRunInContextStrategy(
-          chunkId: string,
-          rootOutputDir: string,
+        const loadFromFs = (
+          filename: string,
           callback: (err: Error | null, chunk: any) => void,
-        ): void {
-          const fs = __non_webpack_require__('fs');
-          const path = __non_webpack_require__('path');
-          const vm = __non_webpack_require__('vm');
-          const filename = resolveFile(rootOutputDir, chunkId);
+        ): void => {
+          const fs = __non_webpack_require__('fs') as typeof import('fs');
+          const path = __non_webpack_require__('path') as typeof import('path');
+          const vm = __non_webpack_require__('vm') as typeof import('vm');
 
           if (fs.existsSync(filename)) {
-            fs.readFile(
-              filename,
-              'utf-8',
-              function (err: Error, content: string) {
-                if (err) {
-                  callback(err, null);
-                  return;
-                }
-                const chunk: any = {};
-                try {
-                  vm.runInThisContext(
-                    '(function(exports, require, __dirname, __filename) {' +
-                      content +
-                      '\n})',
-                    filename,
-                  )(
-                    chunk,
-                    __non_webpack_require__,
-                    path.dirname(filename),
-                    filename,
-                  );
-                  callback(null, chunk);
-                } catch (e) {
-                  console.log("'runInThisContext threw'", e);
-                  callback(e as Error, null);
-                }
-              },
-            );
-          } else {
-            const err = new Error('File ' + filename + ' does not exist');
-            callback(err, null);
-          }
-        }
-
-        function httpEvalStrategy(
-          chunkName: string,
-          remoteName: string,
-          callback: (err: Error | null, chunk: any) => void,
-        ): void {
-          const url = resolveUrl(remoteName, chunkName);
-          if (!url) {
-            const emptyChunk = {
-              modules: {}, // No modules
-              ids: [], // No chunk IDs
-              runtime: null, // No runtime function
-            };
-            return callback(null, emptyChunk);
-          }
-
-          args.origin.loaderHook.lifecycle.fetch
-            .emit(url.href, {})
-            .then(function (res: Response | false | void | Promise<Response>) {
-              if (res instanceof Response) {
-                return res.text();
-              } else if (typeof res === 'string') {
-                return res;
-              } else {
-                throw new Error('Invalid response type:' + res);
-              }
-            })
-            .then(function (data: string) {
-              const chunk: Record<string, unknown> = {};
+            fs.readFile(filename, 'utf-8', (err, content) => {
+              if (err) return callback(err, null);
+              const chunk = {};
               try {
-                const urlDirname = url.pathname
-                  .split('/')
-                  .slice(0, -1)
-                  .join('/');
-                eval(
-                  '(function(exports, require, __dirname, __filename) {' +
-                    data +
-                    '\n})',
-                )(chunk, __non_webpack_require__, urlDirname, chunkName);
-                callback(null, chunk);
-              } catch (e) {
-                callback(e as Error, null);
-              }
-            })
-            .catch((err: Error) => callback(err, null));
-        }
-        function httpVmStrategy(
-          chunkName: string,
-          remoteName: string,
-          callback: (err: Error | null, chunk: any) => void,
-        ): void {
-          const vm = __non_webpack_require__('vm');
-
-          const url = resolveUrl(remoteName, chunkName);
-          if (!url) {
-            const emptyChunk = {
-              modules: {}, // No modules
-              ids: [], // No chunk IDs
-              runtime: null, // No runtime function
-            };
-            return callback(null, emptyChunk);
-          }
-
-          const fetchHook = args.origin.loaderHook.lifecycle.fetch.emit(
-            url.href,
-            {},
-          );
-
-          fetchHook
-            .then((res: any) => res.text())
-            .then((data: string) => {
-              try {
-                const chunk: any = {};
-                const urlDirname = url.pathname
-                  .split('/')
-                  .slice(0, -1)
-                  .join('/');
                 vm.runInThisContext(
+                  `(function(exports, require, __dirname, __filename) {${content}\n})`,
+                  filename,
+                )(
+                  chunk,
+                  __non_webpack_require__,
+                  path.dirname(filename),
+                  filename,
+                );
+                callback(null, chunk);
+              } catch (e) {
+                console.log("'runInThisContext threw'", e);
+                callback(e as Error, null);
+              }
+            });
+          } else {
+            callback(new Error(`File ${filename} does not exist`), null);
+          }
+        };
+
+        const fetchAndRun = (
+          url: URL,
+          chunkName: string,
+          callback: (err: Error | null, chunk: any) => void,
+        ): void => {
+          (typeof fetch === 'undefined'
+            ? importNodeModule<typeof import('node-fetch')>('node-fetch').then(
+                (mod) => mod.default,
+              )
+            : Promise.resolve(fetch)
+          )
+            .then((fetchFunction) => {
+              return args.origin.loaderHook.lifecycle.fetch
+                .emit(url.href, {})
+                .then((res) => {
+                  if (!res || !(res instanceof Response)) {
+                    console.log('No response from hook, falling back to fetch');
+                    return fetchFunction(url.href).then((response) =>
+                      response.text(),
+                    );
+                  }
+                  return res.text();
+                });
+            })
+            .then((data) => {
+              const chunk = {};
+              try {
+                eval(
                   `(function(exports, require, __dirname, __filename) {${data}\n})`,
-                  { filename: chunkName },
-                )(chunk, __non_webpack_require__, urlDirname, chunkName);
+                )(
+                  chunk,
+                  __non_webpack_require__,
+                  url.pathname.split('/').slice(0, -1).join('/'),
+                  chunkName,
+                );
                 callback(null, chunk);
               } catch (e) {
                 callback(e as Error, null);
               }
             })
             .catch((err: Error) => callback(err, null));
-        }
+        };
 
-        function loadChunkStrategy(
-          strategyType: string,
+        const loadChunk = (
+          strategy: string,
           chunkId: string,
           rootOutputDir: string,
-          remotes: any,
           callback: (err: Error | null, chunk: any) => void,
-        ): void {
-          switch (strategyType) {
-            case 'filesystem':
-              return fileSystemRunInContextStrategy(
-                chunkId,
-                rootOutputDir,
-                callback,
-              );
-            case 'http-eval':
-              return httpEvalStrategy(chunkId, rootOutputDir, callback);
-            case 'http-vm':
-              return httpVmStrategy(chunkId, rootOutputDir, callback);
-            default:
-              throw new Error('Invalid strategy type');
+        ): void => {
+          if (strategy === 'filesystem') {
+            return loadFromFs(resolveFile(rootOutputDir, chunkId), callback);
           }
-        }
-        // no baseURI
 
-        // object to store loaded chunks
-        // "0" means "already loaded", Promise means loading
+          const url = resolveUrl(rootOutputDir, chunkId);
+          if (!url)
+            return callback(null, { modules: {}, ids: [], runtime: null });
+
+          fetchAndRun(url, chunkId, callback);
+        };
+
         const installedChunks: { [key: string]: any } = {};
 
-        const installChunk = __webpack_require__.C
-          ? __webpack_require__.C
-          : (chunk: any) => {
-              const moreModules = chunk.modules,
-                chunkIds = chunk.ids,
-                runtime = chunk.runtime;
-              for (const moduleId in moreModules) {
-                if (__webpack_require__.o(moreModules, moduleId)) {
-                  __webpack_require__.m[moduleId] = moreModules[moduleId];
-                }
-              }
-              if (runtime) runtime(__webpack_require__);
-              for (let i = 0; i < chunkIds.length; i++) {
-                if (installedChunks[chunkIds[i]]) {
-                  installedChunks[chunkIds[i]][0]();
-                }
-                installedChunks[chunkIds[i]] = 0;
-              }
-            };
+        const installChunk = (chunk: any): void => {
+          for (const moduleId in chunk.modules) {
+            __webpack_require__.m[moduleId] = chunk.modules[moduleId];
+          }
+          if (chunk.runtime) chunk.runtime(__webpack_require__);
+          for (const chunkId of chunk.ids) {
+            if (installedChunks[chunkId]) installedChunks[chunkId][0]();
+            installedChunks[chunkId] = 0;
+          }
+        };
 
-        // load script equivalent for server side
         __webpack_require__.l = (
           url: string,
           done: (res: any) => void,
           key: string,
           chunkId: string,
-        ) => {
-          if (!key || chunkId) {
+        ): void => {
+          if (!key || chunkId)
             throw new Error(
-              '__webpack_require__.l name is required for ' + url,
+              `__webpack_require__.l name is required for ${url}`,
             );
-          }
           __webpack_require__.federation.runtime
             .loadScriptNode(url, { attrs: { globalName: key } })
-            .then((res: any) => {
-              const federation = __webpack_require__.federation;
-              const enhancedRemote = federation.instance.initRawContainer(
-                key,
-                url,
-                res,
-              );
-              const globalThisVal = new Function('return globalThis')();
-              // use normal global assignment
-              globalThisVal[key] = enhancedRemote;
+            .then((res) => {
+              const enhancedRemote =
+                __webpack_require__.federation.instance.initRawContainer(
+                  key,
+                  url,
+                  res,
+                );
+              new Function('return globalThis')()[key] = enhancedRemote;
               done(enhancedRemote);
             })
-            .catch((error: Error) => {
-              done(error);
-            });
+            .catch(done);
         };
-        // Dynamic filesystem chunk loading for javascript
+
         if (__webpack_require__.f) {
-          const handle = function (chunkId: string, promises: any[]) {
+          const handle = (chunkId: string, promises: any[]): void => {
             let installedChunkData = installedChunks[chunkId];
             if (installedChunkData !== 0) {
-              // 0 means "already installed".
               if (installedChunkData) {
                 promises.push(installedChunkData[2]);
               } else {
                 const matcher = __webpack_require__.federation.chunkMatcher
                   ? __webpack_require__.federation.chunkMatcher(chunkId)
                   : true;
+
                 if (matcher) {
-                  // check if real chunk for handler
-                  const promise = new Promise(function (resolve, reject) {
+                  const promise = new Promise((resolve, reject) => {
                     installedChunkData = installedChunks[chunkId] = [
                       resolve,
                       reject,
                     ];
-
-                    function installChunkCallback(
-                      error: Error | null,
-                      chunk: any,
-                    ) {
-                      if (error) return reject(error);
-                      if (chunk) installChunk(chunk);
-                      resolve(chunk);
-                    }
-
                     const fs =
                       typeof process !== 'undefined'
                         ? __non_webpack_require__('fs')
@@ -374,12 +281,15 @@ export default function (): FederationRuntimePlugin {
                         : false;
 
                     if (fs && fs.existsSync(filename)) {
-                      loadChunkStrategy(
+                      loadChunk(
                         'filesystem',
                         chunkId,
                         __webpack_require__.federation.rootOutputDir || '',
-                        undefined,
-                        installChunkCallback,
+                        (err, chunk) => {
+                          if (err) return reject(err);
+                          if (chunk) installChunk(chunk);
+                          resolve(chunk);
+                        },
                       );
                     } else {
                       const chunkName = __webpack_require__.u(chunkId);
@@ -387,12 +297,15 @@ export default function (): FederationRuntimePlugin {
                         typeof process === 'undefined'
                           ? 'http-eval'
                           : 'http-vm';
-                      loadChunkStrategy(
+                      loadChunk(
                         loadingStrategy,
                         chunkName,
                         __webpack_require__.federation.initOptions.name,
-                        __webpack_require__.federation.initOptions.remotes,
-                        installChunkCallback,
+                        (err, chunk) => {
+                          if (err) return reject(err);
+                          if (chunk) installChunk(chunk);
+                          resolve(chunk);
+                        },
                       );
                     }
                   });
@@ -403,6 +316,7 @@ export default function (): FederationRuntimePlugin {
               }
             }
           };
+
           if (__webpack_require__.f.require) {
             console.warn(
               '\x1b[33m%s\x1b[0m',
@@ -410,11 +324,13 @@ export default function (): FederationRuntimePlugin {
             );
             __webpack_require__.f.require = handle;
           }
+
           if (__webpack_require__.f.readFileVm) {
             __webpack_require__.f.readFileVm = handle;
           }
         }
       })();
+
       return args;
     },
   };
--- packages/sdk/src/node.ts
+++ packages/sdk/src/node.ts
@@ -11,6 +11,33 @@ function importNodeModule<T>(name: string): Promise<T> {
     });
 }
 
+const loadNodeFetch = async (): Promise<typeof fetch> => {
+  const fetchModule =
+    await importNodeModule<typeof import('node-fetch')>('node-fetch');
+  return (fetchModule.default || fetchModule) as unknown as typeof fetch;
+};
+
+const lazyLoaderHookFetch = async (
+  input: RequestInfo | URL,
+  init?: RequestInit,
+): Promise<Response> => {
+  // @ts-ignore
+  const loaderHooks = __webpack_require__.federation.instance.loaderHook;
+
+  const hook = (url: RequestInfo | URL, init: RequestInit) => {
+    return loaderHooks.lifecycle.fetch.emit(url, init);
+  };
+
+  const res = await hook(input, init || {});
+  if (!res || !(res instanceof Response)) {
+    const fetchFunction =
+      typeof fetch === 'undefined' ? await loadNodeFetch() : fetch;
+    return fetchFunction(input, init || {});
+  }
+
+  return res;
+};
+
 export function createScriptNode(
   url: string,
   cb: (error?: Error, scriptContext?: any) => void,
@@ -32,99 +59,78 @@ export function createScriptNode(
     cb(new Error(`Invalid URL: ${e}`));
     return;
   }
-  const getFetch = (): Promise<typeof fetch> => {
-    return new Promise((resolve, reject) => {
-      // @ts-expect-error
-      if (typeof __webpack_require__ !== 'undefined') {
-        try {
-          if (
-            // @ts-expect-error
-            __webpack_require__.federation.instance.loaderHook.lifecycle.fetch
-              .emit
-          ) {
-            return resolve(function (
-              input: RequestInfo | URL,
-              init?: RequestInit,
-            ): Promise<Response> {
-              // @ts-expect-error
-              return __webpack_require__.federation.instance.loaderHook.lifecycle.fetch.emit(
-                input,
-                init || {},
-              );
-            });
-          }
-        } catch (e) {
-          console.warn(
-            'federation.instance.loaderHook.lifecycle.fetch failed:',
-            e,
-          );
-          // fall through
-        }
-      }
 
-      if (typeof fetch === 'undefined') {
-        importNodeModule<typeof import('node-fetch')>('node-fetch')
-          .then((fetchModule) => {
-            const nodeFetch = fetchModule.default || fetchModule;
-            resolve(nodeFetch as unknown as typeof fetch); // Ensure compatibility
-          })
-          .catch(reject);
-      } else {
-        resolve(fetch);
+  const getFetch = async (): Promise<typeof fetch> => {
+    //@ts-ignore
+    if (typeof __webpack_require__ !== 'undefined') {
+      try {
+        //@ts-ignore
+        const loaderHooks = __webpack_require__.federation.instance.loaderHook;
+        if (loaderHooks.lifecycle.fetch) {
+          return lazyLoaderHookFetch;
+        }
+      } catch (e) {
+        console.warn(
+          'federation.instance.loaderHook.lifecycle.fetch failed:',
+          e,
+        );
       }
-    });
+    }
+
+    return typeof fetch === 'undefined' ? loadNodeFetch() : fetch;
   };
 
-  getFetch().then((f) => {
-    f(urlObj.href)
-      .then((res: Response) => res.text())
-      .then(async (data: string) => {
-        const [path, vm]: [typeof import('path'), typeof import('vm')] =
-          await Promise.all([
-            importNodeModule<typeof import('path')>('path'),
-            importNodeModule<typeof import('vm')>('vm'),
-          ]);
-        const scriptContext = { exports: {}, module: { exports: {} } };
-        const urlDirname = urlObj.pathname.split('/').slice(0, -1).join('/');
-        const filename = path.basename(urlObj.pathname);
-        try {
-          const script = new vm.Script(
-            `(function(exports, module, require, __dirname, __filename) {${data}\n})`,
-            filename,
-          );
-          script.runInThisContext()(
-            scriptContext.exports,
-            scriptContext.module,
-            eval('require'),
-            urlDirname,
-            filename,
-          );
-          const exportedInterface: Record<string, any> =
-            scriptContext.module.exports || scriptContext.exports;
-          if (attrs && exportedInterface && attrs['globalName']) {
-            const container =
-              exportedInterface[attrs['globalName']] || exportedInterface;
-            cb(
-              undefined,
-              container as keyof typeof scriptContext.module.exports,
-            );
-            return;
-          }
-          cb(
-            undefined,
-            exportedInterface as keyof typeof scriptContext.module.exports,
-          );
-        } catch (e) {
-          // console.error('Error running script:', e);
-          cb(new Error(`Script execution error: ${e}`));
-        }
-      })
-      .catch((err: Error) => {
-        // console.error('Error fetching script:', err);
-        cb(err);
-      });
-  });
+  const handleScriptFetch = async (f: typeof fetch, urlObj: URL) => {
+    try {
+      const res = await f(urlObj.href);
+      const data = await res.text();
+      const [path, vm] = await Promise.all([
+        importNodeModule<typeof import('path')>('path'),
+        importNodeModule<typeof import('vm')>('vm'),
+      ]);
+      const scriptContext = { exports: {}, module: { exports: {} } };
+      const urlDirname = urlObj.pathname.split('/').slice(0, -1).join('/');
+      const filename = path.basename(urlObj.pathname);
+
+      const script = new vm.Script(
+        `(function(exports, module, require, __dirname, __filename) {${data}\n})`,
+        filename,
+      );
+
+      script.runInThisContext()(
+        scriptContext.exports,
+        scriptContext.module,
+        eval('require'),
+        urlDirname,
+        filename,
+      );
+
+      const exportedInterface: Record<string, any> =
+        scriptContext.module.exports || scriptContext.exports;
+
+      if (attrs && exportedInterface && attrs['globalName']) {
+        const container =
+          exportedInterface[attrs['globalName']] || exportedInterface;
+        cb(undefined, container as keyof typeof scriptContext.module.exports);
+        return;
+      }
+
+      cb(
+        undefined,
+        exportedInterface as keyof typeof scriptContext.module.exports,
+      );
+    } catch (e) {
+      cb(new Error(`Script execution error: ${e}`));
+    }
+  };
+
+  getFetch()
+    .then((f) => handleScriptFetch(f, urlObj))
+    .catch((err) => {
+      cb(err);
+    });
 }
+
 export function loadScriptNode(
   url: string,
   info: {
