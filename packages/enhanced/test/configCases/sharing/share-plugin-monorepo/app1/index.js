e36fd749db46438c4097ab3b54130a6514109157
Robert Donnelly <dev@robdonn.com>
2024-06-28 10:12:13 +0100

    chore: add tests for share plugin recursive search

---
+++ packages/enhanced/test/configCases/sharing/share-plugin-monorepo/app1/index.js
@@ -0,0 +1,60 @@
+it('should provide and consume a normal nested library async', async () => {
+  expect(await import('lib1')).toEqual(
+    expect.objectContaining({
+      default: 'lib1',
+    }),
+  );
+});
+
+it('should provide and consume a normal library async', async () => {
+  expect(await import('lib4')).toEqual(
+    expect.objectContaining({
+      default: 'lib4',
+    }),
+  );
+});
+
+it('should provide and consume a renamed nested library sync', () => {
+  expect(require('lib-two')).toEqual(
+    expect.objectContaining({
+      default: 'lib2',
+    }),
+  );
+});
+
+it('should provide and consume a renamed library sync', () => {
+  expect(require('lib-five')).toEqual(
+    expect.objectContaining({
+      default: 'lib5',
+    }),
+  );
+});
+
+it('should provide and consume a normal nested library async in a separate shareScope', async () => {
+  expect(await import('lib3')).toEqual(
+    expect.objectContaining({
+      default: 'lib3',
+    }),
+  );
+  expect(
+    __webpack_share_scopes__.default && __webpack_share_scopes__.default.lib3,
+  ).toBe(undefined);
+  expect(typeof __webpack_share_scopes__.other.lib3).toBe('object');
+});
+
+it('should provide and consume a relative request async', async () => {
+  expect(await import('./relative1')).toEqual(
+    expect.objectContaining({
+      default: 'rel1',
+    }),
+  );
+});
+
+it('should consume a remapped relative request async', async () => {
+  if (Math.random() < 0) require('store');
+  expect(await import('./relative2')).toEqual(
+    expect.objectContaining({
+      default: 'store',
+    }),
+  );
+});
