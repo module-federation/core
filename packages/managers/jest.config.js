8ea2a1d73b3422ade2241ede94bda070eb61f4b6
ScriptedAlchemy <zackaryjackson@bytedance.com>
2024-06-28 20:54:22 -0700

    chore: revert cors reinjection

--- packages/managers/jest.config.js
+++ packages/managers/jest.config.js
@@ -1,12 +1,25 @@
-// For a detailed explanation regarding each configuration property, visit:
-// https://jestjs.io/docs/en/configuration.html
+/* eslint-disable */
+const { readFileSync } = require('fs');
+
+// Reading the SWC compilation config and remove the "exclude"
+// for the test files to be compiled by SWC
+const { exclude: _, ...swcJestConfig } = JSON.parse(
+  readFileSync(`${__dirname}/.swcrc`, 'utf-8'),
+);
+
+// disable .swcrc look-up by SWC core because we're passing in swcJestConfig ourselves.
+// If we do not disable this, SWC Core will read .swcrc and won't transform our test files due to "exclude"
+swcJestConfig.swcrc ??= false;
+// Uncomment if using global setup/teardown files being transformed via swc
+// https://nx.dev/packages/jest/documents/overview#global-setup/teardown-with-nx-libraries
+// jest needs EsModule Interop to find the default exported setup/teardown functions
+// swcJestConfig.module.noInterop = false;
 
 module.exports = {
   clearMocks: true,
+  cache: false,
   testEnvironment: 'jsdom',
   coveragePathIgnorePatterns: ['__tests__', '/node_modules/'],
-  coverageProvider: 'v8',
-  coverageReporters: ['cobertura', 'clover', 'json', 'lcov', 'text'],
   globals: {
     __DEV__: true,
     __TEST__: true,
@@ -19,7 +32,7 @@ module.exports = {
     '/node_modules/(?!((@byted/garfish-)|(byted-tea-sdk))).+\\.js$',
   ],
   transform: {
-    '^.+\\.(t|j)sx?$': ['@swc/jest'],
+    '^.+\\.(t|j)sx?$': ['@swc/jest', swcJestConfig],
   },
   rootDir: __dirname,
   testMatch: ['<rootDir>__tests__/**/**.spec.[jt]s?(x)'],
