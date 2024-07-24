b5b3933e57ece1cfb3a529331346b6beda4270ae
2heal1 <TwoHeal@163.com>
2024-07-24 19:12:34 +0800

    chore: update docs & remove ueseless code

--- apps/modernjs-ssr/nested-remote/modern.config.ts
+++ apps/modernjs-ssr/nested-remote/modern.config.ts
@@ -1,6 +1,5 @@
 import { appTools, defineConfig } from '@modern-js/app-tools';
 import { moduleFederationPlugin } from '@module-federation/modern-js';
-import { config } from 'process';
 
 // https://modernjs.dev/en/configure/app/usage
 export default defineConfig({
@@ -10,19 +9,11 @@ export default defineConfig({
   runtime: {
     router: true,
   },
-  source: {
-    // enableAsyncEntry:true,
-  },
   server: {
     ssr: {
       mode: 'stream',
     },
     port: 3052,
-  },
-  tools: {
-    rspack: (config) => {
-      // config.resolve?.extensions?.unshift('.server.jsx')
-    },
   },
   plugins: [
     appTools({
