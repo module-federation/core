c6615e36106edd034858564d573342df549eb499
ScriptedAlchemy <zackaryjackson@bytedance.com>
2024-08-28 13:27:37 -0700

    fix(nextjs-mf): types on client plugin apply

--- packages/nextjs-mf/src/plugins/NextFederationPlugin/apply-client-plugins.ts
+++ packages/nextjs-mf/src/plugins/NextFederationPlugin/apply-client-plugins.ts
@@ -6,6 +6,7 @@ import {
 import { ChunkCorrelationPlugin } from '@module-federation/node';
 import InvertedContainerPlugin from '../container/InvertedContainerPlugin';
 import { HoistContainerReferencesPlugin } from '@module-federation/enhanced';
+
 /**
  * Applies client-specific plugins.
  *
@@ -17,13 +18,12 @@ import { HoistContainerReferencesPlugin } from '@module-federation/enhanced';
  * This function applies plugins to the Webpack compiler instance that are specific to the client build of
  * a Next.js application with Module Federation enabled. These plugins include the following:
  *
- * - DelegateModulesPlugin: Delegates modules to the webpack container runtime that can be streamed to other runtimes.
  * - ChunkCorrelationPlugin: Collects metadata on chunks to enable proper module loading across different runtimes.
  * - InvertedContainerPlugin: Adds custom runtime modules to the container runtime to allow a host to expose its
  *   own remote interface at startup.
  *
- * If automatic page stitching is enabled, a loader is added to process the `next/dist/client/page-loader.js`
- * file. If a custom library is specified in the options, an error is thrown. The options.library property is
+ * If automatic page stitching is enabled, a warning is logged indicating that it is disabled in v7.
+ * If a custom library is specified in the options, an error is logged. The options.library property is
  * also set to `{ type: 'window', name: options.name }`.
  */
 export function applyClientPlugins(
@@ -31,45 +31,36 @@ export function applyClientPlugins(
   options: ModuleFederationPluginOptions,
   extraOptions: NextFederationPluginExtraOptions,
 ): void {
-  const { webpack } = compiler;
   const { remotes, name } = options;
 
   if (compiler.options.output.publicPath === '/_next/') {
     compiler.options.output.publicPath = 'auto';
   }
 
-  // If automatic page stitching is enabled, add a new rule to the compiler's module rules
   if (extraOptions.automaticPageStitching) {
     console.warn('[nextjs-mf]', 'automatic page stitching is disabled in v7');
   }
 
-  // If a custom library is set, log an error message
   if (options.library) {
     console.error('[nextjs-mf] you cannot set custom library');
   }
 
-  // Set the library option to be a window object with the name of the module federation plugin
   options.library = {
     type: 'window',
     name,
   };
 
-  // Add a new chunk correlation plugin to the compiler
   new ChunkCorrelationPlugin({
     filename: [
       'static/chunks/federated-stats.json',
       'server/federated-stats.json',
     ],
-    //@ts-ignore
   }).apply(compiler);
-
-  // new HoistContainerReferencesPlugin(options.name + '_partial').apply(compiler);
 
   new InvertedContainerPlugin({
     runtime: 'webpack',
     container: options.name,
     remotes: options.remotes as Record<string, string>,
     debug: extraOptions.debug,
-    //@ts-ignore
   }).apply(compiler);
 }
