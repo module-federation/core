7a4d83050c949a34bd55ceb00021c7b5f8d366c4
ScriptedAlchemy <zackaryjackson@bytedance.com>
2024-08-29 13:51:31 -0700

    fix(enhanced): hoist only initial modules

--- packages/enhanced/src/lib/container/HoistContainerReferencesPlugin.ts
+++ packages/enhanced/src/lib/container/HoistContainerReferencesPlugin.ts
@@ -188,11 +188,6 @@ export class HoistContainerReferences implements WebpackPluginInstance {
             (module) => module instanceof ContainerEntryModule,
           )
         : undefined;
-      // if (runtimeModule && runtimeModule.dependencies.length > 1) {
-      //   runtimeModule = moduleGraph.getModule(runtimeModule.dependencies[1]);
-      // } else {
-      //   runtimeModule = undefined;
-      // }
     }
 
     if (!runtimeModule) {
