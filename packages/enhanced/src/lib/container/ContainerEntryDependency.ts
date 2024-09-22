270c948a60f8983d676437a7d76eeec96b1a30cf
Zack Jackson <25274700+scriptedalchemy@users.noreply.github.com>
2024-09-21 18:31:17 -0700

    chore: lint

--- packages/enhanced/src/lib/container/ContainerEntryDependency.ts
+++ packages/enhanced/src/lib/container/ContainerEntryDependency.ts
@@ -20,7 +20,7 @@ class ContainerEntryDependency extends Dependency {
   public exposes: [string, ExposeOptions][];
   public shareScope: string;
   public injectRuntimeEntry: string;
-/** Additional experimental options for container plugin customization */
+  /** Additional experimental options for container plugin customization */
   public experiments: containerPlugin.ContainerPluginOptions['experiments'];
 
   /**
