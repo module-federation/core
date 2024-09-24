898827a49c9fcaf736d6874878969e2526b34af0
ScriptedAlchemy <zackary.l.jackson@gmail.com>
2024-09-24 13:19:26 -0700

    chore: update rspack version

--- apps/manifest-demo/3011-rspack-manifest-provider/rspack.config.js
+++ apps/manifest-demo/3011-rspack-manifest-provider/rspack.config.js
@@ -54,7 +54,7 @@ module.exports = composePlugins(
     ];
     config.resolve = {
       extensions: ['*', '.js', '.jsx', '.tsx', '.ts'],
-      tsConfigPath: path.resolve(__dirname, 'tsconfig.app.json'),
+      tsConfig: path.resolve(__dirname, 'tsconfig.app.json'),
     };
     // publicPath must be specific url
     config.output.publicPath = 'http://localhost:3011/';
--- apps/react-ts-remote/rspack.config.js
+++ apps/react-ts-remote/rspack.config.js
@@ -69,7 +69,7 @@ module.exports = composePlugins(
     ];
     config.resolve = {
       extensions: ['*', '.js', '.jsx', '.tsx', '.ts'],
-      tsConfigPath: path.resolve(__dirname, 'tsconfig.app.json'),
+      tsConfig: path.resolve(__dirname, 'tsconfig.app.json'),
     };
     // publicPath must be specific url
     config.output.publicPath = 'http://localhost:3004/';
