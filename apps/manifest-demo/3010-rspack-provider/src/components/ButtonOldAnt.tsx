22a3b834dcae0ccd1128f29bd590a066594b19a8
Hanric <hanric.zhang@gmail.com>
2024-10-09 17:08:23 +0800

    fix(data-prefetch): apply DataPrefetchPlugin on demand (#3061)

--- apps/manifest-demo/3010-rspack-provider/src/components/ButtonOldAnt.tsx
+++ apps/manifest-demo/3010-rspack-provider/src/components/ButtonOldAnt.tsx
@@ -1,10 +1,10 @@
 import Button from 'antd/lib/button';
 import { version } from 'antd/package.json';
 import * as stuff from './stuff.module.css';
-console.log(stuff);
 
 export default function ButtonOldAnt() {
   return (
+    // @ts-ignore
     <Button className={stuff['test-remote2'] + ' test-remote2'}>
       Button from antd@{version}
     </Button>
