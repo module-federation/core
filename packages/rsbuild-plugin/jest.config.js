d92086c847a611bcc253f41a144b23ebf6b60df6
nyqykk <1327719263@qq.com>
2024-10-11 17:41:37 +0800

    chore: add config for rsbuild plugin

---
+++ packages/rsbuild-plugin/jest.config.js
@@ -0,0 +1,29 @@
+// For a detailed explanation regarding each configuration property, visit:
+// https://jestjs.io/docs/en/configuration.html
+
+module.exports = {
+  clearMocks: true,
+  cacheDirectory: './jest/cache',
+  coverageDirectory: './jest/coverage',
+  testEnvironment: 'jsdom',
+  coveragePathIgnorePatterns: ['__tests__', '/node_modules/'],
+  coverageProvider: 'v8',
+  coverageReporters: ['cobertura', 'clover', 'json', 'lcov', 'text'],
+  globals: {
+    __DEV__: true,
+    __TEST__: true,
+    __BROWSER__: false,
+    __VERSION__: '"unknow"',
+  },
+  preset: 'ts-jest',
+  transformIgnorePatterns: [
+    // Change MODULE_NAME_HERE to your module that isn't being compiled
+    '/node_modules/(?!((@byted/garfish-)|(byted-tea-sdk))).+\\.js$',
+  ],
+  transform: {
+    '^.+\\.(t|j)sx?$': ['@swc/jest', { swcrc: false }],
+  },
+  rootDir: __dirname,
+  testMatch: ['<rootDir>/__tests__/**/*.spec.[jt]s?(x)'],
+  testPathIgnorePatterns: ['/node_modules/'],
+};
