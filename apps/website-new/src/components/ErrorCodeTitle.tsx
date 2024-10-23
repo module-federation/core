e75f6f99a00bdeeaf26344d3f79427b5b35e18d8
2heal1 <TwoHeal@163.com>
2024-10-23 15:16:54 +0800

    chore: add demo

---
+++ apps/website-new/src/components/ErrorCodeTitle.tsx
@@ -0,0 +1,20 @@
+import { getCustomMDXComponent } from 'rspress/theme';
+import { runtimeCodes } from '@gshared/error-codes';
+
+const ErrorCodeTitle = ({ code }: { code: string }) => {
+  const { h1, ul, li } = getCustomMDXComponent();
+  const Title = h1({ children: runtimeCodes[code] });
+  const Info = ul({
+    children: li({
+      children: ['Error Code: ', <code key={`err-code-${code}`}>{code}</code>],
+    }),
+  });
+  return (
+    <>
+      {Title}
+      {Info}
+    </>
+  );
+};
+
+export default ErrorCodeTitle;
