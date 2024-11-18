757ed463da2955ec15343b40fb3fee2b6f698a82
2heal1 <TwoHeal@163.com>
2024-11-18 17:58:29 +0800

    chore: export getGlobalFederationInstance from helpers

--- packages/runtime/src/helpers.ts
+++ packages/runtime/src/helpers.ts
@@ -1,7 +1,14 @@
 import { helpers } from '@module-federation/runtime-core';
+import { getGlobalFederationInstance } from './utils';
 export type {
   IGlobalUtils,
   IShareUtils,
 } from '@module-federation/runtime-core';
 
-export default helpers;
+export default {
+  global: {
+    ...helpers.global,
+    getGlobalFederationInstance,
+  },
+  share: helpers.share,
+};
