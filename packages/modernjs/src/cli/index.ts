5f67582ac5e428a67ed4fb140c2dfe1d329a5c17
Hanric <hanric.zhang@gmail.com>
2024-12-30 13:44:14 +0800

    chore(modern-js-plugin): add disableSSR option (#3355)

--- packages/modernjs/src/cli/index.ts
+++ packages/modernjs/src/cli/index.ts
@@ -21,6 +21,7 @@ export const moduleFederationPlugin = (
     distOutputDir: '',
     originPluginOptions: userConfig,
     remoteIpStrategy: userConfig?.remoteIpStrategy,
+    userConfig,
   };
   return {
     name: '@modern-js/plugin-module-federation',
