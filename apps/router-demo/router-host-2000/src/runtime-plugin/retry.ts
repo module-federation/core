8945d3df03aad168e7179b475b6d5ab8ed729660
fengdanping <fengdanping@bytedance.com>
2025-02-06 17:55:35 +0800

    chore: fix ci issue

--- apps/router-demo/router-host-2000/src/runtime-plugin/retry.ts
+++ apps/router-demo/router-host-2000/src/runtime-plugin/retry.ts
@@ -4,13 +4,18 @@ const retryPlugin = () =>
   RetryPlugin({
     fetch: {
       url: 'http://localhost:2001/mf-manifest.json',
-      // fallback: () => 'http://localhost:2001/mf-manifest.json',
+      fallback: () => 'http://localhost:2001/mf-manifest.json',
     },
     script: {
       retryTimes: 3,
       retryDelay: 1000,
       moduleName: ['remote1'],
       cb: (resolve, error) => {
+        if (error) {
+          throw new Error(
+            'The request failed three times and has now been abandoned',
+          );
+        }
         return setTimeout(() => {
           resolve(error);
         }, 1000);
