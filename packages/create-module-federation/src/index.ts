63dae78f1727a0311a556c3457ad62fa78bfa29e
2heal1 <TwoHeal@163.com>
2025-03-04 20:04:30 +0800

    feat: add create mf cli

---
+++ packages/create-module-federation/src/index.ts
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+import { create } from './create';
+
+interface Template {
+  template: string;
+  tools?: Record<string, string>;
+}
+
+export const TEMPLATES = [
+  'provider-modern',
+  'consumer-modern',
+  'provider-rsbuild',
+  'consumer-rsbuild',
+  'rslib',
+  'rslib-storybook',
+];
+
+create({
+  name: 'Module Federation',
+  templates: TEMPLATES,
+});
