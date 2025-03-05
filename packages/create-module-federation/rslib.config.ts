7d1b7349573b8702bd86c61a807e6b68b2cf69ad
2heal1 <TwoHeal@163.com>
2025-03-05 16:48:51 +0800

    chore: auto set version

--- packages/create-module-federation/rslib.config.ts
+++ packages/create-module-federation/rslib.config.ts
@@ -1,7 +1,13 @@
 import { pluginPublint } from 'rsbuild-plugin-publint';
 import { defineConfig } from '@rslib/core';
+import pkg from './package.json';
 
 export default defineConfig({
   lib: [{ format: 'esm' }],
   plugins: [pluginPublint()],
+  source: {
+    define: {
+      __VERSION__: JSON.stringify(pkg.version),
+    },
+  },
 });
