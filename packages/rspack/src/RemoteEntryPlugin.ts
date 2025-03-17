c01715a2b9568f8b3a67506d57ddd81876a4cf65
Hanric <hanric.zhang@gmail.com>
2025-03-17 19:27:22 +0800

    fix(rspack): support getPublicPath (#3422)

--- packages/rspack/src/RemoteEntryPlugin.ts
+++ packages/rspack/src/RemoteEntryPlugin.ts
@@ -1,7 +1,27 @@
-import type { Compiler, WebpackPluginInstance } from 'webpack';
+import type { Compiler, RspackPluginInstance } from '@rspack/core';
+// @ts-ignore
 import pBtoa from 'btoa';
 
-export class RemoteEntryPlugin implements WebpackPluginInstance {
+const charMap: Record<string, string> = {
+  '<': '\\u003C',
+  '>': '\\u003E',
+  '/': '\\u002F',
+  '\\': '\\\\',
+  '\b': '\\b',
+  '\f': '\\f',
+  '\n': '\\n',
+  '\r': '\\r',
+  '\t': '\\t',
+  '\0': '\\0',
+  '\u2028': '\\u2028',
+  '\u2029': '\\u2029',
+};
+
+function escapeUnsafeChars(str: string) {
+  return str.replace(/[<>\b\f\n\r\t\0\u2028\u2029\\]/g, (x) => charMap[x]);
+}
+
+export class RemoteEntryPlugin implements RspackPluginInstance {
   readonly name = 'VmokRemoteEntryPlugin';
   private _name: string;
   private _getPublicPath: string;
@@ -13,12 +33,14 @@ export class RemoteEntryPlugin implements WebpackPluginInstance {
 
   apply(compiler: Compiler): void {
     let code;
+    const sanitizedPublicPath = escapeUnsafeChars(this._getPublicPath);
+
     if (!this._getPublicPath.startsWith('function')) {
       code = `${
         compiler.webpack.RuntimeGlobals.publicPath
-      } = new Function(${JSON.stringify(this._getPublicPath)})()`;
+      } = new Function(${JSON.stringify(sanitizedPublicPath)})()`;
     } else {
-      code = `(${this._getPublicPath})()`;
+      code = `(${sanitizedPublicPath}())`;
     }
     const base64Code = pBtoa(code);
     const dataUrl = `data:text/javascript;base64,${base64Code}`;
