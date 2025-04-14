71cfc9b429862d13101b9298a7b6a914cce263db
fengdanping <fengdanping@bytedance.com>
2025-04-14 11:25:45 +0800

    chore: delete useless code

--- packages/bridge/bridge-react/src/index.ts
+++ packages/bridge/bridge-react/src/index.ts
@@ -6,9 +6,3 @@ export type {
   DestroyParams,
   RenderParams,
 } from './types';
-
-// 注意：对于特定React版本的支持，请使用以下导入路径：
-// - React 16/17: import { createBridgeComponent } from '@module-federation/bridge-react/legacy'
-// - React 18: import { createBridgeComponent } from '@module-federation/bridge-react/v18'
-// - React 19: import { createBridgeComponent } from '@module-federation/bridge-react/v19'
-// export * from './legacy';
