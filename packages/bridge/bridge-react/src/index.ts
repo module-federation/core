0eb6d1bad798a29e7e3a74a7df6731ca542aa745
CocooDanielle <dapeen.feng@gmail.com>
2025-04-17 16:51:18 +0800

    refactor(bridge-react): modify the default mode of the bridge to legacy mode to reduce redundant code (#3711)

--- packages/bridge/bridge-react/src/index.ts
+++ packages/bridge/bridge-react/src/index.ts
@@ -1,8 +1,14 @@
+/**
+ * Entry point for React 16/17 (legacy) specific bridge components
+ * This file provides support for React 16 and 17 versions, using the traditional ReactDOM.render API
+ */
+export { createBridgeComponent } from './provider/versions/legacy';
 export { createRemoteComponent } from './remote/create';
-export { createBridgeComponent } from './provider/create';
+export type { CreateRootOptions, Root } from './provider/versions/legacy';
 export type {
   ProviderParams,
-  RenderFnParams,
+  ProviderFnParams,
+  RootType,
   DestroyParams,
   RenderParams,
 } from './types';
