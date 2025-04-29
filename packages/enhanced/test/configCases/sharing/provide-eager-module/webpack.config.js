562221d488ea0aa8565650f2adffe8a00978af34
ScriptedAlchemy <zackary.l.jackson@gmail.com>
2025-04-29 11:23:37 -0700

    chore: restore tests

---
+++ packages/enhanced/test/configCases/sharing/provide-eager-module/webpack.config.js
@@ -0,0 +1,41 @@
+const { ProvideSharedPlugin } = require('../../../../dist/src');
+
+module.exports = {
+  mode: 'development',
+  plugins: [
+    new ProvideSharedPlugin({
+      shareScope: 'eagerOverrideNonEager',
+      provides: {
+        common: {
+          shareKey: 'common',
+          eager: true,
+        },
+      },
+    }),
+    new ProvideSharedPlugin({
+      shareScope: 'nonEagerDontOverrideEager',
+      provides: {
+        uncommon: {
+          shareKey: 'uncommon',
+        },
+      },
+    }),
+    new ProvideSharedPlugin({
+      shareScope: 'newerNonEager',
+      provides: {
+        uncommon: {
+          shareKey: 'uncommon',
+        },
+      },
+    }),
+    new ProvideSharedPlugin({
+      shareScope: 'newerEager',
+      provides: {
+        common: {
+          shareKey: 'common',
+          eager: true,
+        },
+      },
+    }),
+  ],
+};
