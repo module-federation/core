9f784bc218b35e57265c0a47aed382e72362e596
ScriptedAlchemy <zackary.l.jackson@gmail.com>
2025-05-10 20:49:17 -0700

    chore: remove log in test

--- packages/enhanced/test/configCases/sharing/share-multiple-versions-exclude/index.js
+++ packages/enhanced/test/configCases/sharing/share-multiple-versions-exclude/index.js
@@ -10,7 +10,6 @@ it('should only provide shared@2.0.0 due to exclude filter on v1', async () => {
 
 it('should not have v1 in share scope due to exclude filter', async () => {
   const shareScope = __webpack_share_scopes__.default.shared;
-  console.log('share scope', __webpack_share_scopes__.default);
   expect(shareScope['2.0.0']).toBeDefined();
   expect(shareScope['1.0.0']).toBeUndefined();
 });
