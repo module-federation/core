60a019b446ca4ee2ad9b26fab0b2f544b1304835
ScriptedAlchemy <zackary.l.jackson@gmail.com>
2025-05-12 14:47:10 -0700

    test: add consume share plugin compiler tests

--- packages/enhanced/src/lib/container/ModuleFederationPlugin.ts
+++ packages/enhanced/src/lib/container/ModuleFederationPlugin.ts
@@ -215,6 +215,10 @@ class ModuleFederationPlugin implements WebpackPluginInstance {
         new SharePlugin({
           shared: options.shared,
           shareScope: options.shareScope,
+          experiments: {
+            nodeModulesReconstructedLookup:
+              options.experiments?.nodeModulesReconstructedLookup,
+          },
         }).apply(compiler);
       }
     });
