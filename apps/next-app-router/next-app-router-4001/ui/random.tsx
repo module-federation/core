075bf8c10be06d55ae6c91a39a77865bfdda8786
ScriptedAlchemy <zackary.l.jackson@gmail.com>
2025-05-21 14:34:40 -0700

    chore: add rsc directories for loader to handle

---
+++ apps/next-app-router/next-app-router-4001/ui/random.tsx
@@ -0,0 +1,10 @@
+import Button from '#/ui/button';
+
+export default function Random() {
+  return (
+    <div>
+      <p>button container</p>
+      <Button>here is a button</Button>
+    </div>
+  );
+}
