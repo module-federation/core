d31a32659b350cc2811cca0a1dfd645e1b58a29e
Zack Jackson <25274700+ScriptedAlchemy@users.noreply.github.com>
2025-07-17 15:45:02 -0700

    refactor: sink React packages from root to individual packages (#3907)

--- packages/bridge/bridge-react/__tests__/setupTests.ts
+++ packages/bridge/bridge-react/__tests__/setupTests.ts
@@ -1,3 +1,8 @@
 // In vitest, you can use the setupFiles option in your configuration file to import any necessary setup files for your tests.
 // For example, if you want to use testing-library's custom matchers, you can import them in a setup file like this:
 import '@testing-library/jest-dom';
+
+// Fix TextEncoder/TextDecoder not defined in Node.js
+import { TextEncoder, TextDecoder } from 'util';
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder as any;
