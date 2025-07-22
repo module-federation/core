b6a51df3a2ad970045291287108470b327acc8b7
ScriptedAlchemy <zackaryjackson@bytedance.com>
2025-07-21 22:02:51 -0700

    fix: correct runtime-core exports and add bridge-react global types

---
+++ packages/bridge/bridge-react/global.d.ts
@@ -0,0 +1,14 @@
+import { Federation } from '@module-federation/runtime-core';
+import { RemoteEntryExports } from '@module-federation/runtime-core';
+
+declare global {
+  // eslint-disable-next-line no-var
+  var __FEDERATION__: Federation;
+  // eslint-disable-next-line no-var
+  var __VMOK__: Federation;
+  // eslint-disable-next-line no-var
+  var __GLOBAL_LOADING_REMOTE_ENTRY__: Record<
+    string,
+    undefined | Promise<RemoteEntryExports | void>
+  >;
+}
