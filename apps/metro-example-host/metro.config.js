9fb2886fda1972751e2f6e3fb2d2a41acaa84f23
theGrep01 <denis.belotrserkovets@gmail.com>
2025-08-07 16:03:19 +0300

    feat: adds example apps for metro-mf

---
+++ apps/metro-example-host/metro.config.js
@@ -0,0 +1,59 @@
+const path = require('node:path');
+const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+
+const {withModuleFederation} = require('@module-federation/metro');
+
+/**
+ * Metro configuration
+ * https://reactnative.dev/docs/metro
+ *
+ * @type {import('@react-native/metro-config').MetroConfig}
+ */
+
+const config = {
+  resolver: {useWatchman: false},
+  watchFolders: [
+    path.resolve(__dirname, '../../node_modules'),
+    path.resolve(__dirname, '../../packages/core'),
+  ],
+};
+
+module.exports = withModuleFederation(
+  mergeConfig(getDefaultConfig(__dirname), config),
+  {
+    name: 'example-host',
+    remotes: {
+      mini: 'mini@http://localhost:8082/mf-manifest.json',
+      nestedMini: 'nestedMini@http://localhost:8083/mf-manifest.json',
+    },
+    shared: {
+      react: {
+        singleton: true,
+        eager: true,
+        requiredVersion: '19.1.0',
+        version: '19.1.0',
+      },
+      'react-native': {
+        singleton: true,
+        eager: true,
+        requiredVersion: '0.80.0',
+        version: '0.80.0',
+      },
+      lodash: {
+        singleton: false,
+        eager: false,
+        requiredVersion: '4.16.6',
+        version: '4.16.6',
+      },
+    },
+    shareStrategy: 'loaded-first',
+    plugins: [path.resolve(__dirname, './runtime-plugin.ts')],
+  },
+  {
+    flags: {
+      unstable_patchHMRClient: true,
+      unstable_patchInitializeCore: true,
+      unstable_patchRuntimeRequire: true,
+    },
+  },
+);
