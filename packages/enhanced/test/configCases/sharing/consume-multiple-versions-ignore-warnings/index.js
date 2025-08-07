0f424535d5b252adcc1e2e79ece805c2a22f9a39
ScriptedAlchemy <zackaryjackson@bytedance.com>
2025-08-06 21:12:25 -0700

    chore: fix conflicts

--- packages/enhanced/test/configCases/sharing/consume-multiple-versions-ignore-warnings/index.js
+++ packages/enhanced/test/configCases/sharing/consume-multiple-versions-ignore-warnings/index.js
@@ -48,7 +48,7 @@ it('should be able to consume different shared module version depending on conte
     ),
   ).toBe(true);
   expect(() => require('my-module3')).toThrowError(
-    'Invalid loadShareSync function',
+    'loadShareSync failed! The function should not be called unless you set "eager:true"',
   );
   expect(require('my-module4')).toBe('shared@9.9.9');
   // expectWarning();
