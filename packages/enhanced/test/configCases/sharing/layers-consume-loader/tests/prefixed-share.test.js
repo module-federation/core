eb59443fc13164242adbdfc3cb2b9f05d79df24e
ScriptedAlchemy <zackaryjackson@bytedance.com>
2025-08-06 21:12:25 -0700

    chore: fix conflicts

--- packages/enhanced/test/configCases/sharing/layers-consume-loader/tests/prefixed-share.test.js
+++ packages/enhanced/test/configCases/sharing/layers-consume-loader/tests/prefixed-share.test.js
@@ -4,11 +4,11 @@
 it('should consume thing1 from multi-pkg with multi-pkg-layer', async () => {
   const { version, layer } = await import('multi-pkg/thing1');
   expect(version).toBe('2.0.0');
-  expect(layer).toBeUndefined();
+  expect(layer).toBe('multi-pkg-layer');
 });
 
 it('should consume thing2 from multi-pkg with multi-pkg-layer', async () => {
   const { version, layer } = await import('multi-pkg/thing2');
   expect(version).toBe('2.0.0');
-  expect(layer).toBeUndefined();
+  expect(layer).toBe('multi-pkg-layer');
 });
