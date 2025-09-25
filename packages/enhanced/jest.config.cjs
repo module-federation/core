76df80711149311fe6513aa18024e6478a5bb3fc
ScriptedAlchemy <zackaryjackson@bytedance.com>
2025-09-24 23:01:38 -0700

    chore: align managers snapshot with react 19

--- packages/enhanced/jest.config.cjs
+++ packages/enhanced/jest.config.cjs
@@ -1,12 +1,12 @@
 /* eslint-disable */
-const { readFileSync } = require('fs');
-const path = require('path');
-const os = require('os');
-const rimraf = require('rimraf');
+var { readFileSync } = require('fs');
+var path = require('path');
+var os = require('os');
+var rimraf = require('rimraf');
 
 // Reading the SWC compilation config and remove the "exclude"
 // for the test files to be compiled by SWC
-const { exclude: _omit, ...swcJestConfig } = JSON.parse(
+var swcJestConfig = JSON.parse(
   readFileSync(path.join(__dirname, '.swcrc'), 'utf-8'),
 );
 
@@ -15,12 +15,16 @@ rimraf.sync(path.join(__dirname, 'test/js'));
 // Disable .swcrc look-up by SWC core because we're passing in swcJestConfig
 // ourselves. If we do not disable this, SWC Core will read .swcrc and won't
 // transform our test files due to "exclude".
+if (swcJestConfig && typeof swcJestConfig.exclude !== 'undefined') {
+  delete swcJestConfig.exclude;
+}
+
 if (swcJestConfig.swcrc === undefined) {
   swcJestConfig.swcrc = false;
 }
 
 /** @type {import('jest').Config} */
-const config = {
+var config = {
   displayName: 'enhanced',
   preset: '../../jest.preset.js',
   cacheDirectory: path.join(os.tmpdir(), 'enhanced'),
