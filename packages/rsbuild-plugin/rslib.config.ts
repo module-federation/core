671acb32685dbbb56a4504a4057599b5e7c6dbf2
ScriptedAlchemy <zackaryjackson@bytedance.com>
2025-09-24 17:56:50 -0700

    refactor(rsbuild-plugin): switch build to rslib

---
+++ packages/rsbuild-plugin/rslib.config.ts
@@ -0,0 +1,41 @@
+import { defineConfig } from '@rslib/core';
+
+export default defineConfig({
+  lib: [
+    {
+      format: 'esm',
+      syntax: 'es2021',
+      bundle: true,
+      dts: {
+        distPath: './dist',
+      },
+    },
+    {
+      format: 'cjs',
+      syntax: 'es2021',
+      bundle: true,
+      dts: false,
+    },
+  ],
+  source: {
+    entry: {
+      index: './src/cli/index.ts',
+      utils: './src/utils/index.ts',
+      constant: './src/constant.ts',
+    },
+    tsconfigPath: './tsconfig.lib.json',
+  },
+  output: {
+    target: 'node',
+    distPath: {
+      root: './dist',
+    },
+    externals: [/@module-federation\//, 'pnpapi'],
+    copy: [
+      {
+        from: './LICENSE',
+        to: '.',
+      },
+    ],
+  },
+});
