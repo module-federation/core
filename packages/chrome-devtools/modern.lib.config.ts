4d569c8376574ae6f89257f7dc16a03ba14a4cb4
ScriptedAlchemy <zackaryjackson@bytedance.com>
2025-09-30 00:01:36 -0700

    fix(chrome-devtools): isolate react 18 types for builds

--- packages/chrome-devtools/modern.lib.config.ts
+++ packages/chrome-devtools/modern.lib.config.ts
@@ -2,9 +2,22 @@ import { moduleTools, defineConfig } from '@modern-js/module-tools';
 
 export default defineConfig({
   plugins: [moduleTools()],
-  buildPreset: 'npm-component',
-  buildConfig: {
-    input: ['src', '!src/index.tsx'],
-    tsconfig: 'tsconfig.lib.json',
-  },
+  buildPreset: ({ preset }) =>
+    preset['npm-component'].map((config, index) => {
+      const nextConfig = {
+        ...config,
+        input: ['src', '!src/index.tsx'],
+        tsconfig: 'tsconfig.lib.json',
+      };
+
+      if (index === 2 && nextConfig.dts) {
+        nextConfig.dts = {
+          ...nextConfig.dts,
+          tsconfigPath: 'tsconfig.dts.json',
+          enableTscBuild: true,
+        };
+      }
+
+      return nextConfig;
+    }),
 });
