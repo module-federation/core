e89af95072be14f17ec7a47d351c3f516fbc54ef
ScriptedAlchemy <zackaryjackson@bytedance.com>
2025-09-29 19:56:25 -0700

    fix(chrome-devtools): disable DTS; add chrome/react types
    - Disable DTS in modern.lib.config
    
    - Include types: chrome, react, react-dom; add skipLibCheck

--- packages/chrome-devtools/modern.lib.config.ts
+++ packages/chrome-devtools/modern.lib.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
   plugins: [moduleTools()],
   buildPreset: 'npm-component',
   buildConfig: {
+    dts: false,
     input: ['src', '!src/index.tsx'],
     tsconfig: 'tsconfig.lib.json',
   },
