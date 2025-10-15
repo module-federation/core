99c1ccbe26de2f39965c007c87900a09ccda2c29
fengdanping <fengdanping@bytedance.com>
2025-10-15 18:12:27 +0800

    chore: reset unneed changes

--- apps/router-demo/router-host-2000/rsbuild.config.ts
+++ apps/router-demo/router-host-2000/rsbuild.config.ts
@@ -40,8 +40,8 @@ export default defineConfig({
       },
       runtimePlugins: [
         path.join(__dirname, './src/runtime-plugin/shared-strategy.ts'),
-        // path.join(__dirname, './src/runtime-plugin/retry.ts'),
-        // path.join(__dirname, './src/runtime-plugin/fallback.ts'),
+        path.join(__dirname, './src/runtime-plugin/retry.ts'),
+        path.join(__dirname, './src/runtime-plugin/fallback.ts'),
       ],
     }),
   ],
