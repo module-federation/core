c19338647c19a46005d415c9deeee7361182b99a
ScriptedAlchemy <zackaryjackson@bytedance.com>
2025-10-16 18:12:51 -0700

    refactor: remove federation shim in runtime worker

--- apps/runtime-demo/3005-runtime-host/src/worker/loader-worker.js
+++ apps/runtime-demo/3005-runtime-host/src/worker/loader-worker.js
@@ -1,13 +1,10 @@
 /* eslint-env worker */
+/* global __webpack_require__ */
 import { workerMap } from './map.js';
 
 self.onmessage = (event) => {
   const value = event.data && event.data.value;
-  const federation =
-    typeof __webpack_require__ !== 'undefined'
-      ? __webpack_require__.federation || {}
-      : {};
-  const federationKeys = Object.keys(federation);
+  const federationKeys = Object.keys(__webpack_require__.federation);
   self.postMessage({
     answer: workerMap[value] ?? null,
     federationKeys,
