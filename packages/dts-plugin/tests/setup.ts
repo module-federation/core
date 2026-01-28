03da3515a67bc438734eb961cfdde8d5b3c1e91b
ScriptedAlchemy <zackaryjackson@bytedance.com>
2026-01-28 15:55:10 -0800

    fix(dts-plugin): restore vitest global setup export

--- packages/dts-plugin/tests/setup.ts
+++ packages/dts-plugin/tests/setup.ts
@@ -5,15 +5,17 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const TEMP_TS_CONFIG_DIR = resolve(__dirname, '../node_modules/.federation');
-try {
-  rmSync(TEMP_TS_CONFIG_DIR, { recursive: true });
-} catch (err) {
-  // noop
-}
-const TEMP_DIST = resolve(__dirname, '../dist-test');
-try {
-  rmSync(TEMP_DIST, { recursive: true });
-} catch (err) {
-  // noop
+export default function setup() {
+  const TEMP_TS_CONFIG_DIR = resolve(__dirname, '../node_modules/.federation');
+  try {
+    rmSync(TEMP_TS_CONFIG_DIR, { recursive: true });
+  } catch (err) {
+    // noop
+  }
+  const TEMP_DIST = resolve(__dirname, '../dist-test');
+  try {
+    rmSync(TEMP_DIST, { recursive: true });
+  } catch (err) {
+    // noop
+  }
 }
