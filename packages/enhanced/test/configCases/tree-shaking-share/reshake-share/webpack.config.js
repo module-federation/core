b8c98f0bc75a3997d4dba30ac5689e09ed81f73f
ScriptedAlchemy <zackaryjackson@bytedance.com>
2026-01-28 17:12:24 -0800

    chore(enhanced): restore reshake-share fixtures

--- packages/enhanced/test/configCases/tree-shaking-share/reshake-share/webpack.config.js
+++ packages/enhanced/test/configCases/tree-shaking-share/reshake-share/webpack.config.js
@@ -1,46 +1,5 @@
 const { TreeShakingSharedPlugin } = require('../../../../dist/src');
-const fs = require('fs');
 const path = require('path');
-
-const ensureFixture = (pkgName, entryContents) => {
-  const baseDir = path.join(__dirname, 'node_modules', pkgName);
-  fs.mkdirSync(baseDir, { recursive: true });
-  fs.writeFileSync(
-    path.join(baseDir, 'package.json'),
-    JSON.stringify(
-      {
-        name: pkgName,
-        main: './index.js',
-        version: '1.0.0',
-        sideEffects: false,
-      },
-      null,
-      2,
-    ),
-  );
-  fs.writeFileSync(path.join(baseDir, 'index.js'), entryContents);
-};
-
-ensureFixture(
-  'ui-lib-dep',
-  ["export const Message = 'Message';", "export const Spin = 'Spin';", ''].join(
-    '\n',
-  ),
-);
-ensureFixture(
-  'ui-lib',
-  [
-    "import { Message, Spin } from 'ui-lib-dep';",
-    '',
-    "export const Button = 'Button';",
-    "export const List = 'List';",
-    "export const Badge = 'Badge';",
-    '',
-    'export const MessagePro = `${Message}Pro`;',
-    'export const SpinPro = `${Spin}Pro`;',
-    '',
-  ].join('\n'),
-);
 
 module.exports = {
   cache: false,
