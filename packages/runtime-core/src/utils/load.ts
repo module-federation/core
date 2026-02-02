e162e7953ff2653c1fe9439f6844e63e2d2afaed
ScriptedAlchemy <zackaryjackson@bytedance.com>
2026-02-01 18:16:31 -0800

    chore(runtime-core): migrate to rslib bundleless

--- packages/runtime-core/src/utils/load.ts
+++ packages/runtime-core/src/utils/load.ts
@@ -31,10 +31,10 @@ async function loadEsmEntry({
     try {
       if (!remoteEntryExports) {
         if (typeof FEDERATION_ALLOW_NEW_FUNCTION !== 'undefined') {
-          new Function('callbacks', `import("${entry}")${importCallback}`)([
-            resolve,
-            reject,
-          ]);
+          new Function('callbacks', 'entry', `import(entry)${importCallback}`)(
+            [resolve, reject],
+            entry,
+          );
         } else {
           import(/* webpackIgnore: true */ /* @vite-ignore */ entry)
             .then(resolve)
@@ -145,7 +145,7 @@ async function loadEntryScript({
     .then(() => {
       return handleRemoteEntryLoaded(name, globalName, entry);
     })
-    .catch((e) => {
+    .catch((e: unknown) => {
       assert(
         undefined,
         getShortErrorMsg(RUNTIME_008, runtimeDescMap, {
@@ -222,7 +222,7 @@ async function loadEntryNode({
     .then(() => {
       return handleRemoteEntryLoaded(name, globalName, entry);
     })
-    .catch((e) => {
+    .catch((e: unknown) => {
       throw e;
     });
 }
