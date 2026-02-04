ebc8c5fb042d1f10de6d71e331e076c7f77ab0f4
fengdanping <fengdanping@bytedance.com>
2026-02-04 20:59:09 +0800

    chore: upgrade monden-js dependencies to v3.0.0

--- packages/modernjs/src/cli/ssrPlugin.ts
+++ packages/modernjs/src/cli/ssrPlugin.ts
@@ -305,6 +305,7 @@ export const moduleFederationSSRPlugin = (
         });
       }
     });
+    // @ts-ignore
     api.config(() => {
       return {
         builderPlugins: [mfSSRRsbuildPlugin(pluginOptions)],
