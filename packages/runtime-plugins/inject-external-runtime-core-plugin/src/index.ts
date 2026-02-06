8b6835c943910ef4957d78bfdd872c7d6158d90c
ScriptedAlchemy <zackary.l.jackson@gmail.com>
2026-02-06 12:12:43 -0800

    fix(inject-external-runtime-core-plugin): load runtime core from esm entry

--- packages/runtime-plugins/inject-external-runtime-core-plugin/src/index.ts
+++ packages/runtime-plugins/inject-external-runtime-core-plugin/src/index.ts
@@ -1,6 +1,6 @@
-import * as runtimeCore from '@module-federation/runtime/core';
+import * as runtimeCore from '@module-federation/runtime-tools/dist/runtime-core.esm.js';
 
-import type { ModuleFederationRuntimePlugin } from '@module-federation/runtime/core';
+import type { ModuleFederationRuntimePlugin } from '@module-federation/runtime-tools/runtime-core';
 declare global {
   var __VERSION__: string;
   var _FEDERATION_RUNTIME_CORE: typeof runtimeCore;
