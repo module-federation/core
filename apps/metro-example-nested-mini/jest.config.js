cc99a5c377ee10b22df9d3a99a9647106ebb3131
ScriptedAlchemy <zackaryjackson@bytedance.com>
2026-02-25 19:58:45 -0800

    fix(metro-core): revert workflow and jest config changes per review

--- apps/metro-example-nested-mini/jest.config.js
+++ apps/metro-example-nested-mini/jest.config.js
@@ -1,9 +1,3 @@
 module.exports = {
   preset: 'react-native',
-  transformIgnorePatterns: [
-    'node_modules[\\\\/](?!.*(react-native|@react-native)[\\\\/])',
-  ],
-  transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', {configFile: './babel.config.js'}],
-  },
 };
