b4706a24789910551d44ca63c46d7c59d1c4d719
ScriptedAlchemy <zackary.l.jackson@gmail.com>
2026-02-26 19:43:46 -0800

    fix: address ci regressions and export compatibility

--- packages/nextjs-mf/src/index.ts
+++ packages/nextjs-mf/src/index.ts
@@ -1,3 +1,12 @@
 import NextFederationPlugin from './plugins/NextFederationPlugin';
 
+export { NextFederationPlugin };
 export default NextFederationPlugin;
+
+if (
+  typeof module !== 'undefined' &&
+  typeof module.exports !== 'undefined'
+) {
+  module.exports = NextFederationPlugin;
+  module.exports.NextFederationPlugin = NextFederationPlugin;
+}
