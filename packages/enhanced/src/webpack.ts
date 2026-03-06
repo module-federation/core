ce6d2384a428274b8a24860e6cd67f2b121138a3
ScriptedAlchemy <zackaryjackson@bytedance.com>
2026-03-05 17:45:48 -0800

    fix(enhanced): preserve treeshake fixture build ids

--- packages/enhanced/src/webpack.ts
+++ packages/enhanced/src/webpack.ts
@@ -1,13 +1,6 @@
-import * as ModuleFederationPluginModule from './wrapper/ModuleFederationPlugin';
-
-const pickDefault = <T>(mod: T): T extends { default: infer U } ? U : T =>
-  ((mod as { default?: unknown }).default ?? mod) as T extends {
-    default: infer U;
-  }
-    ? U
-    : T;
-
-const ModuleFederationPlugin = pickDefault(ModuleFederationPluginModule);
-const { PLUGIN_NAME } = ModuleFederationPluginModule;
+import {
+  default as ModuleFederationPlugin,
+  PLUGIN_NAME,
+} from './wrapper/ModuleFederationPlugin';
 
 export { ModuleFederationPlugin, PLUGIN_NAME };
