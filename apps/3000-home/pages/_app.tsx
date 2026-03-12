3b86cb5b05a357f09dffff5c45f4e70d129ec717
ScriptedAlchemy <zackary.l.jackson@gmail.com>
2026-03-11 19:13:57 -0700

    test(nextjs-mf): extract pages-router fixtures

--- apps/3000-home/pages/_app.tsx
+++ apps/3000-home/pages/_app.tsx
@@ -1,54 +1,67 @@
 import * as React from 'react';
-import { useState } from 'react';
 import { init } from '@module-federation/runtime';
-console.log('logging init', typeof init);
 import App from 'next/app';
 import { Layout, version, ConfigProvider } from 'antd';
 import { StyleProvider } from '@ant-design/cssinjs';
-
-import Router, { useRouter } from 'next/router';
-const SharedNav = React.lazy(() => import('../components/SharedNav'));
+import { useRouter } from 'next/compat/router';
+import SharedNav from '../components/SharedNav';
 import HostAppMenu from '../components/menu';
+
 function MyApp(props) {
   const { Component, pageProps } = props;
-  const { asPath } = useRouter();
-  const [MenuComponent, setMenuComponent] = useState(() => HostAppMenu);
-  const handleRouteChange = async (url) => {
+  const router = useRouter();
+  const resolvedPath =
+    router?.asPath ||
+    router?.pathname ||
+    (typeof window !== 'undefined'
+      ? `${window.location.pathname}${window.location.search}${window.location.hash}`
+      : '/');
+  const [MenuComponent, setMenuComponent] = React.useState(() => HostAppMenu);
+
+  const handleRouteChange = React.useCallback(async (url: string) => {
     if (url.startsWith('/shop')) {
-      // @ts-ignore
       const RemoteAppMenu = (await import('shop/menu')).default;
       setMenuComponent(() => RemoteAppMenu);
-    } else if (url.startsWith('/checkout')) {
-      // @ts-ignore
+      return;
+    }
+
+    if (url.startsWith('/checkout')) {
       const RemoteAppMenu = (await import('checkout/menu')).default;
       setMenuComponent(() => RemoteAppMenu);
-    } else {
-      setMenuComponent(() => HostAppMenu);
+      return;
     }
-  };
-  // handle first route hit.
-  React.useEffect(() => {
-    handleRouteChange(asPath);
-  }, [asPath]);
 
-  //handle route change
-  React.useEffect(() => {
-    // Step 3: Subscribe on events
-    Router.events.on('routeChangeStart', handleRouteChange);
-    return () => {
-      Router.events.off('routeChangeStart', handleRouteChange);
-    };
+    setMenuComponent(() => HostAppMenu);
   }, []);
+
+  React.useEffect(() => {
+    const initialPath =
+      router?.asPath ||
+      (typeof window !== 'undefined'
+        ? `${window.location.pathname}${window.location.search}${window.location.hash}`
+        : '/');
+    void handleRouteChange(initialPath);
+  }, [handleRouteChange, router?.asPath]);
+
+  React.useEffect(() => {
+    if (!router?.events) {
+      return;
+    }
+
+    router.events.on('routeChangeStart', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeStart', handleRouteChange);
+    };
+  }, [handleRouteChange, router?.events]);
+
   return (
     <StyleProvider layer>
       <ConfigProvider theme={{ hashed: false }}>
         <Layout style={{ minHeight: '100vh' }} prefixCls={'dd'}>
-          <React.Suspense>
-            <SharedNav />
-          </React.Suspense>
+          <SharedNav currentPath={resolvedPath} />
           <Layout>
             <Layout.Sider width={200}>
-              <MenuComponent />
+              <MenuComponent currentPath={resolvedPath} />
             </Layout.Sider>
             <Layout>
               <Layout.Content style={{ background: '#fff', padding: 20 }}>
---
+++ apps/3002-checkout/components/WebpackPng.tsx
@@ -0,0 +1,5 @@
+import png from '../public/webpack.png';
+
+export default function WebpackPng() {
+  return <img className="shop-webpack-png" src={png.src} alt="webpack png" />;
+}
