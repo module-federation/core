7970b5fcfb05d02156eaf6eab67ee1fd1db8130b
2heal1 <TwoHeal@163.com>
2026-05-13 17:58:20 +0800

    chore(devtools): consolidate proxy logic

---
+++ .changeset/basic-proxy-core.md
@@ -0,0 +1,5 @@
+---
+"@module-federation/devtools": patch
+---
+
+Consolidate the Chrome DevTools proxy logic into a bundled proxy asset.
--- packages/chrome-devtools/src/utils/chrome/snapshot-plugin.ts
+++ packages/chrome-devtools/src/utils/chrome/snapshot-plugin.ts
@@ -1,55 +1,7 @@
-import { MODULE_DEVTOOL_IDENTIFIER } from '@module-federation/sdk';
-import runtimeHelpers from '@module-federation/runtime/helpers';
-
-import type { ModuleFederationRuntimePlugin } from '@module-federation/runtime';
-
-import { definePropertyGlobalVal } from '../sdk';
-
-declare global {
-  // eslint-disable-next-line no-var, no-inner-declarations
-  var __INIT_VMOK_CHROME_DEVTOOL_PLUGIN__: boolean | undefined;
-}
-
-const chromeDevtoolsPlugin: () => ModuleFederationRuntimePlugin = function () {
-  type BeforeLoadRemoteSnapshotArgs = Parameters<
-    NonNullable<ModuleFederationRuntimePlugin['beforeLoadRemoteSnapshot']>
-  >[0];
-
-  return {
-    name: 'mf-chrome-devtools-inject-snapshot-plugin',
-    beforeLoadRemoteSnapshot({ options }: BeforeLoadRemoteSnapshotArgs) {
-      const { nativeGlobal } = runtimeHelpers.global;
-
-      if (!options || options.inBrowser) {
-        const realLocalStorage = nativeGlobal.localStorage || localStorage;
-        const debugModuleInfoStr = realLocalStorage.getItem(
-          MODULE_DEVTOOL_IDENTIFIER,
-        );
-        if (
-          debugModuleInfoStr &&
-          !nativeGlobal.__INIT_VMOK_CHROME_DEVTOOL_PLUGIN__
-        ) {
-          const chromeDevtoolSnapshot = JSON.parse(debugModuleInfoStr);
-          if (chromeDevtoolSnapshot) {
-            runtimeHelpers.global.addGlobalSnapshot(chromeDevtoolSnapshot);
-            nativeGlobal.__INIT_VMOK_CHROME_DEVTOOL_PLUGIN__ = true;
-            console.warn(
-              '[Module Federation Devtools]: You are using the chrome devtool to proxy online module',
-            );
-          }
-        }
-      }
-    },
-  };
+const basicProxyCore = require('../../vendor/basic-proxy-core.js') as {
+  registerSnapshotPlugin(globalObject?: typeof globalThis): unknown;
 };
 
-if (!window?.__FEDERATION__) {
-  definePropertyGlobalVal(window, '__FEDERATION__', {});
-  definePropertyGlobalVal(window, '__VMOK__', window.__FEDERATION__);
-}
+basicProxyCore.registerSnapshotPlugin(globalThis);
 
-if (!window?.__FEDERATION__.__GLOBAL_PLUGIN__) {
-  window.__FEDERATION__.__GLOBAL_PLUGIN__ = [];
-}
-
-window.__FEDERATION__.__GLOBAL_PLUGIN__?.push(chromeDevtoolsPlugin());
+export {};
