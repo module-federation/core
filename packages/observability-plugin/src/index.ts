aa751778a4d74663785ba9b5c461cbc2253f324e
2heal1 <TwoHeal@163.com>
2026-06-14 20:07:31 +0800

    feat: sync MF observability to OpenRuntime

--- packages/observability-plugin/src/index.ts
+++ packages/observability-plugin/src/index.ts
@@ -2,3 +2,4 @@ export * from './core';
 export { ObservabilityPlugin } from './browser';
 export { ChromeObservabilityPlugin } from './chrome-devtool';
 export { default } from './browser';
+export type { OpenRuntimeObservabilityOptions } from './openruntime';
