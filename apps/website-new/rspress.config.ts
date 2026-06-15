a4f5b4ad5c34e74d89f0ed5591b58f8a8340ac7b
kongjiacong <kongjiacong@bytedance.com>
2026-06-15 18:15:42 +0800

    docs: update rspress version

--- apps/website-new/rspress.config.ts
+++ apps/website-new/rspress.config.ts
@@ -18,6 +18,11 @@ export default defineConfig({
     light: '/module-federation.svg',
     dark: '/module-federation-logo-white.svg',
   },
+  markdown: {
+    image: {
+      checkDeadImages: false,
+    },
+  },
   themeConfig: {
     locales: [
       {
