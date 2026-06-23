8b671d2b56092d768dfc449d9862873bb1bc535f
zhanghang.heal <zhanghang.heal@bytedance.com>
2026-06-23 11:48:30 +0800

    fix: address clear cache CI failures

--- packages/modernjs/src/cli/configPlugin.ts
+++ packages/modernjs/src/cli/configPlugin.ts
@@ -344,7 +344,6 @@ export function patchBundlerConfig(options: {
 
   const splitChunkConfig = chain.optimization.splitChunks.entries();
   if (!isServer) {
-    // @ts-expect-error type not the same
     autoDeleteSplitChunkCacheGroups(mfConfig, splitChunkConfig);
   }
 
